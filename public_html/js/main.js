36c4bc3fa775015e9a14d1b4de84607c7edd8d7a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-08-24 10:31:52 +0200

    i

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,38 +1,22 @@
-//$("a").click(function () {
-//  var v = $(this).attr("href");
-//  event.preventDefault();
-//  $("section").removeCLass("ive_been_jogging");
-//  $(v).addClass("ive_been_jogging");
-//});
 $( document ).ready(function() {
-    var dt = new Date();
-    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds()
-    $(".rn_time").html(time);
+    document.title = 'コールド | Kalt - Making dope doper.';
 });
-$('.js-tilt').tilt({
-    reset: false
+$("#values article").click(function () {
+  $(this).toggleClass("toggled");
 });
-$(".k_io, .k_io:after").click(function () {
-  // store the scroll pos like this on the object itself and pick it
-  // up if the user wants to : $("this").data( "foo", $(document).scrollTop() );
-  $(".index").toggleClass("open");
-  $("body").toggleClass("k_iopen");
-  $('html, body').animate({
-      scrollTop: $("#upper").offset().top
-  }, 500);
+$(".index-btn").click(function () {
+  $(".index").toggleClass("toggled");
 });
-
-$("#menu article h3").click(function () {
-  $(this).parent().toggleClass("toggled");
+$(".index a").click(function () {
+  $(".index").toggleClass("toggled");
 });
-$("a").click(function () {
-  var pag = $(this).attr("href");
-  $(".active_section").removeClass("active_section");
-  $(pag).addClass("active_section");
-  $(".index").removeClass("open");
-  $("body").removeClass("k_iopen");
-});
-$('.k_io, .companyname').hover(
-       function(){ $("body").addClass('lol') },
-       function(){ $("body").removeClass('lol') }
+$('.index-btn, .companyname').hover(
+       function(){
+         $("body").addClass('lol')
+         document.title = ' ಠ_ಠ ';
+       },
+       function(){
+         $("body").removeClass('lol')
+         document.title = 'コールド | Kalt - Making dope doper.';
+       }
 )
