eaa97e2c2a0b060b528479a786a5fc4f7d13b585
christerwaters <42318403+christerwaters@users.noreply.github.com>
2021-07-12 13:37:13 +0200

    Adding JS powered menu

--- js/base.js
+++ js/base.js
@@ -1,8 +1,73 @@
+let okStyle = [
+  "color: #FF0"
+].join(";");
+
 
 $(".menu button").click(function(){
   $("body").toggleClass("show-menu");
+  console.log("%c Kalt: Toggled menu.", okStyle);
 });
 
 $(".menu ul li a").click(function(){
   $("body").toggleClass("show-menu");
+  console.log("%c Kalt: Toggled menu.", okStyle);
+});
+
+$("code").click(function(){
+  $(this).addClass("show-code");
+});
+
+console.log("")
+console.log("")
+console.log("%c                             kalt.co                                ", okStyle)
+console.log("")
+console.log("")
+console.log("%c                                                                    ", okStyle)
+console.log("%c                                (                                   ", okStyle)
+console.log("%c                    .            )        )                         ", okStyle)
+console.log("%c                              (  (|              .                  ", okStyle)
+console.log("%c                         )   )\/ ( ( (                              ", okStyle)
+console.log("%c                 *  (   ((  /     ))\))  (  )    )                  ", okStyle)
+console.log("%c               (     \   )\(          |  ))( )  (|                  ", okStyle)
+console.log("%c               >)     ))/   |          )/  \((  ) \                 ", okStyle)
+
+
+
+
+
+
+$(function () {
+
+    var data = {
+        menu: [{
+            name: 'Documents',
+            link: 'documents.html' },
+            {
+            name: 'Visual profile',
+            link: 'visual-profile.html' },
+            {
+            name: 'Stylebook',
+            link: 'stylebook.html' },
+            {
+            name: 'Contact',
+            link: 'mailto:christer@kalt.co'
+        }]
+    };
+    var getMenuItem = function (itemData) {
+        var item = $("<li>")
+            .append(
+        $("<a>", {
+            href: itemData.link,
+            html: itemData.name
+        }));
+        return item;
+    };
+
+    var $menu = $(".menu ul");
+    $.each(data.menu, function () {
+        $menu.append(
+            getMenuItem(this)
+        );
+    });
+    $menu.menu();
 });
