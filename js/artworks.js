cf96b50faf1d55011ece6d0e5e53960e9ffd0778
christerwaters <42318403+christerwaters@users.noreply.github.com>
2021-08-29 21:08:43 +0200

    push

--- js/artworks.js
+++ js/artworks.js
@@ -2,33 +2,26 @@
 
 const artworks = [
 		{
-			artid: '2993',
-			title: 'Untitled (History of the Black People)',
-			image: 'https://cdn.kalt.co/media/basquiat.jpeg',
-			year: '1931',
+			artid: '00001',
+			title: 'All Colored Cast (Part II)',
+			image: 'https://cdn.kalt.co/media/00001.jpeg',
+			year: '1982',
+			price: '208 861 200',
+			currency: 'NOK',
 			dimensions: '92 x 140 cm (36.22" x 55.12")',
 			state: 'bid',
 			byline: 'Jean-Michel Basquiat',
-			description: 'In this large work Basquiat recreates the epic of their ancestral arrival to the Americas. This includes references to Egypt and other parts of Africa, and most African American music centers in the southern United States. The paintings are rich in visual and textual references to African history and capture the fascinating subject of Basquiat aesthetics. Art historian Andre Fron proposed that the painting "reject Egypt as an African" in an attempt to neutralize Putremys revisionist position as a pioneer of Western civilization, not his own Africa. Emphasizes a persons identity.'
+			description: 'All Colored Cast (Part II) is one of two canvases that Basquiat created in 1982, whose title is a direct comment on racism in Hollywood. The work also has a number of historical references in regard to black history, such as famous boxer Jersey Joe Walcott, and Haitian revolutionary Toussaint L’Overture.'
 		},
 		{
-			artid: '2991',
-			title: 'Untitled (Memphis)',
+			artid: '00002',
+			title: 'David Hockney - For Louisiana',
 			image: 'https://cdn.kalt.co/media/memphis.jpeg',
-			year: '1931',
+			year: '2021',
+			price: '6 000',
 			state: 'bid',
 			dimensions: '92 x 140 cm (36.22" x 55.12")',
 			byline: 'Memphis group',
-			description: 'In this large work Basquiat recreates the epic of their ancestral arrival to the Americas. This includes references to Egypt and other parts of Africa, and most African American music centers in the southern United States. The paintings are rich in visual and textual references to African history and capture the fascinating subject of Basquiat aesthetics. Art historian Andre Fron proposed that the painting "reject Egypt as an African" in an attempt to neutralize Putremys revisionist position as a pioneer of Western civilization, not his own Africa. Emphasizes a persons identity.'
-		},
-		{
-			artid: '2992',
-			title: 'Large still life with pedestal table',
-			image: 'https://cdn.kalt.co/media/picasso.jpg',
-			year: '1931',
-			state: 'sold',
-			dimensions: '92 x 140 cm (36.22" x 55.12")',
-			byline: 'Pablo Picasso',
 			description: 'In this large work Basquiat recreates the epic of their ancestral arrival to the Americas. This includes references to Egypt and other parts of Africa, and most African American music centers in the southern United States. The paintings are rich in visual and textual references to African history and capture the fascinating subject of Basquiat aesthetics. Art historian Andre Fron proposed that the painting "reject Egypt as an African" in an attempt to neutralize Putremys revisionist position as a pioneer of Western civilization, not his own Africa. Emphasizes a persons identity.'
 		}
 ];
