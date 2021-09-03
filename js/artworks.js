4bee3122684a08cf446528b7676b4756955bc420
christerwaters <42318403+christerwaters@users.noreply.github.com>
2021-09-03 23:37:56 +0200

    ###

--- js/artworks.js
+++ js/artworks.js
@@ -15,12 +15,10 @@ const artworks = [
 			size: '152.5 x 152.5 cm',
 			byline: 'Jean-Michel Basquiat',
 			description: 'All Colored Cast (Part II) is one of two canvases that Basquiat created in 1982, whose title is a direct comment on racism in Hollywood. The work also has a number of historical references in regard to black history, such as famous boxer Jersey Joe Walcott, and Haitian revolutionary Toussaint L’Overture.',
-			exhibited: (
-				'Los Angeles, Gagosian Gallery, Jean-Michel Basquiat: New Paintings, March-April 1983',
-				'Geneva, Musée d\'art moderne et contemporain, Rudiments d\'un musée possible, September 1994-June 1995',
-				'Geneva, Musée d\'art moderne et contemporain, Patchwork in progress 1 to 7, November 1997-December 1999',
-				'Geneva, Musée d\'art moderne et contemporain, Vivement 2002 ! premier épisode, January-March 2000'
-			)
+			exhibited:'<li>Los Angeles, Gagosian Gallery, Jean-Michel Basquiat: New Paintings, March-April 1983</li>
+			<li>Geneva, Musée d\'art moderne et contemporain, Rudiments d\'un musée possible, September 1994-June 1995</li>
+				<li>Geneva, Musée d\'art moderne et contemporain, Patchwork in progress 1 to 7, November 1997-December 1999</li>
+				<li>Geneva, Musée d\'art moderne et contemporain, Vivement 2002 ! premier épisode, January-March 2000</li>'
 		},
 		{
 			artid: '00002',
@@ -35,11 +33,6 @@ const artworks = [
 			dimensions: '92 x 140 cm (36.22" x 55.12")',
 			byline: 'David Hockney',
 			description: 'A pioneer of the 1960s British movement, David Hockney is one of the most celebrated and prolific artists of the 20th and 21st centuries. Endlessly versatile, he has produced acrylic paintings, photo collages, full-scale opera set designs, and digital works created on iPads. Hockney first gained recognition for his semi-abstract paintings of gay love. He moved to California in 1964 and captured his new home in sensual, pastel-hued scenes of uninhibited queer men whose idyllic, sunny backdrops feature swimming pools, palm trees, and light, beautiful homes. Hockney has exhibited at the Museum of Modern Art in New York, the Royal Academy of Arts in London, and the Van Gogh Museum in Amsterdam, among many other institutions.',
-			exhibited: (
-				'Los Angeles, Gagosian Gallery, Jean-Michel Basquiat: New Paintings, March-April 1983',
-				'Geneva, Musée d\'art moderne et contemporain, Rudiments d\'un musée possible, September 1994-June 1995',
-				'Geneva, Musée d\'art moderne et contemporain, Patchwork in progress 1 to 7, November 1997-December 1999',
-				'Geneva, Musée d\'art moderne et contemporain, Vivement 2002 ! premier épisode, January-March 2000'
-			)
+			exhibited:'<li>Los Angeles, Gagosian Gallery, Jean-Michel Basquiat: New Paintings, March-April 1983</li> <li>Geneva, Musée d\'art moderne et contemporain, Rudiments d\'un musée possible, September 1994-June 1995</li> <li>Geneva, Musée d\'art moderne et contemporain, Patchwork in progress 1 to 7, November 1997-December 1999</li> <li>Geneva, Musée d\'art moderne et contemporain, Vivement 2002 ! premier épisode, January-March 2000</li>'
 		}
 ];
