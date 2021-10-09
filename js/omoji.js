608fa171967e12e3899b6da9f849f679dc7ea668
christerwaters <42318403+christerwaters@users.noreply.github.com>
2021-10-09 22:59:38 +0200

    Yes

--- js/omoji.js
+++ js/omoji.js
@@ -1,10 +1,27 @@
 
-// Get all the elements.
-var elements = document.getElementsByClassName("omoji");
-// Iterate each.
-for (var i = 0; i < elements.length; i++) {
-    // Check if the text is found.
-    if (elements[i].innerHTML.indexOf("😀") != -1 )
-        // Replace the contents.
-        elements[i].innerHTML = elements[i].innerHTML.replace('😀', '<o style="background-size:contain;color:transparent;background-repeat:no-repeat;background-image:url(https://cdn.kalt.co/media/emoticon/smile.svg);">😀</o>');
+const css = 'o { background-size:contain;color:transparent;background-repeat:no-repeat;}',
+    	head = document.head || document.getElementsByTagName('head')[0],
+    	style = document.createElement('style');
+
+head.appendChild(style);
+style.type = 'text/css';
+style.appendChild(document.createTextNode(css));
+
+const omojis = [
+	//["😃", "smile.svg"], commented out for the preview page
+	["😄", "smile.svg"],
+	["😁", "smile.svg"],
+	["😀", "smile.svg"],
+	["☺️", "smile.svg"],
+	["😊", "smile.svg"],
+	["🙂", "smile.svg"],
+];
+
+for(var i = 0; i < omojis.length; i++) {
+    const item = omojis[i];
+    for(var j = 0; j < item.length; j++) {
+			const emoji = new RegExp(item[0], "g");
+			const omoji = '<o style="background-image:url(https://cdn.kalt.co/media/emoticon/' + item[1] + ');">' + item[0] + '</o>';
+			document.body.innerHTML = document.body.innerHTML.replace(emoji, omoji);
+    }
 }
