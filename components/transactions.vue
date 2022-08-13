4f3630510b363cc64ab4a8ddb145dc3b32f850d5
christerwaters <42318403+christerwaters@users.noreply.github.com>
2022-08-13 12:36:25 +0200

    Commit

--- components/transactions.vue
+++ components/transactions.vue
@@ -1,77 +1,15 @@
 
 <template>
     <div class="transactions_component">
-          <p> <strong> Transactions </strong> </p>
-          <table>
-            <tr>
-              <th>Amount</th>
-              <th>Account</th>
-              <th>Date</th>
-            </tr>
-            <Transaction v-for="transaction in transactionData" :key="transaction.id" :amount="transaction.amount" :account="transaction.account" :date="transaction.date" />
-          </table>
-          <!--
-          <table>
-            <tr>
-              <th>Amount</th>
-              <th>Account</th>
-              <th>Date</th>
-            </tr>
-            <tr>
-              <td>+ 103.44 €</td>
-              <td>Account name</td>
-              <td>13.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 34.44 €</td>
-              <td>Account name</td>
-              <td>14.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 103.44 €</td>
-              <td>Account name</td>
-              <td>13.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 34.44 €</td>
-              <td>Account name</td>
-              <td>14.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 103.44 €</td>
-              <td>Account name</td>
-              <td>13.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 34.44 €</td>
-              <td>Account name</td>
-              <td>14.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 103.44 €</td>
-              <td>Account name</td>
-              <td>13.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 34.44 €</td>
-              <td>Account name</td>
-              <td>14.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 103.44 €</td>
-              <td>Account name</td>
-              <td>13.02.22</td>
-            </tr>
-            <tr>
-              <td>+ 34.44 €</td>
-              <td>Account name</td>
-              <td>14.02.22</td>
-            </tr>
-            <tr>
-              <td>{{ transactions[1] }} </td>
-            </tr>
-        </table>
-        -->
+      <p> <strong> Transactions {{ transactionData}} </strong> </p>
+      <table>
+        <tr>
+          <th>Amount</th>
+          <th>Account</th>
+          <th>Date</th>
+        </tr>
+        <Transaction v-for="transaction in transactionData.transactions" :key="transaction.id" :amount="transaction.amount" :account="transaction.account" :date="transaction.date" />
+      </table>
     </div>
 </template>
 
@@ -85,10 +23,11 @@ export default{
         transactionData: []
     }),
     async fetch(){
+        let uri = 'http://localhost:9000/getTransactions?id=' + this.$auth.user.sub;
         try {
-            const res = await axios.get('http://localhost:9000/getTransactions');
-            console.log(res.data[1])
-            this.transactionData = res.data[1].transactions
+            const res = await axios.get(uri);
+            this.transactionData = res.data
+            console.log(res.data)
         } catch(err) {
             console.log(err);
         }
