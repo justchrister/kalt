3ab9cf208c3299ba7d793ba3e9024e707a2bec73
christerwaters <42318403+christerwaters@users.noreply.github.com>
2022-11-20 17:34:57 +0100

    Adding favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,21 @@ export default defineNuxtConfig({
   build: {
     transpile: ['chart.js']
   },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Kalt — Undefined',
+      link: [ 
+        { rel: 'icon', type: "image/x-icon", href: '/favicon/favicon.ico' },
+        { rel: 'icon', type: "image/png", sizes: '32x32', href: '/favicon//favicon-32x32.png' } ,
+        { rel: 'icon', type: "image/png", sizes: '16x16', href: '/favicon//favicon-16x16.png' } ,
+        { rel: 'manifest', type: "image/x-icon", href: '/favicon/site.webmanifest' }
+      ],
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Make money, make a difference!' }
+      ],
+    }
+  }
 })
