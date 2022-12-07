625bb337b96585e04693203e32c35fdd882c96ac
christerwaters <42318403+christerwaters@users.noreply.github.com>
2022-12-07 20:05:04 +0100

    transactions page?

--- pages/account/transactions.vue
+++ pages/account/transactions.vue
@@ -26,19 +26,8 @@ onMounted(() => {
     }
   })
 })
-/*
-const { data: transactions } = await useAsyncData('transactions', async () => {
-  const { data } = await client.from('transactions').select('transaction_id, transaction_type, currency, amount, created_at').eq('user_id', user.value.id).order('created_at')
 
-  return data
-})
-*/
-const { pending, data: transactions } = useLazyFetch('/api/transactions/getTransactions')
-watch(transactions, (newTransactions) => {
-  // Because posts starts out null, you will not have access
-  // to its contents immediately, but you can watch it.
-})
-
+const { data: transactions, pending, error, refresh } = await useFetch('http://localhost:3000/api/transactions/getTransactions?user_id='+user.value.id)
 </script>
 <template>
   <div class="PageWrapper">
@@ -46,20 +35,18 @@ watch(transactions, (newTransactions) => {
     <div class="page">
       <div class="section">
         <kaltheader />
-        <div v-if="transactions?.length > 0">
           <table>
             <tr>
-            <th>Amount</th>
-            <th>Account</th>
-            <th>Date</th>
+              <th>Amount</th>
+              <th>Account</th>
+              <th>Date</th>
             </tr>
             <tr v-for="transaction of transactions" :key="transaction.transaction_id">
-            <td>{{ transaction.amount }} {{ transaction.currency }}</td>
-            <td>{{ transaction.transaction_type }}</td>
-            <td>{{ transaction.created_at }}</td>
+              <td>{{ transaction.amount }} {{ transaction.currency }}</td>
+              <td>{{ transaction.transaction_type }}</td>
+              <td>{{ transaction.created_at }}</td>
             </tr>
           </table>
-        </div>
       </div>
     </div>
   </div>
