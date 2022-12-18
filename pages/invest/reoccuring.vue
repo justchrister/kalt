99102403b4baaf535fdca708ebe4739908ccb813
christerwaters <42318403+christerwaters@users.noreply.github.com>
2022-12-18 16:20:07 +0100

    completing the code

--- pages/invest/reoccuring.vue
+++ pages/invest/reoccuring.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-const pagename = 'Set up reoccuring payments';
+const pagename = 'Subscription';
 const title = 'Kalt — ' + pagename;
 const description = ref('My App Description')
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+const day = ref(15)
+const invest_id = ref('')
 
 useHead({
   title,
@@ -15,9 +19,6 @@ definePageMeta({
   middleware: ['auth']
 })
 
-const client = useSupabaseClient()
-const user = useSupabaseUser()
-
 
 onMounted(() => {
   watchEffect(() => {
@@ -26,106 +27,82 @@ onMounted(() => {
     }
   })
 })
+
+const { data: exists, error } = await client
+  .from('cache_invest')
+  .select('invest_id, day')
+  .eq('user_id', user.value.id)
+if(error) console.log(error)
+
+
+if(exists[0]){
+  if(exists[0].invest_id) invest_id.value = exists[0].invest_id
+  if(exists[0].day) day.value = exists[0].day
+} else {
+    navigateTo('/invest')
+}
+
+// if we cannot find the investment in the cache, we need to revert the user back to the first page
+
+
+async function updateCache() {
+  try {
+    const { data, error } = await client
+      .from('cache_invest')
+      .upsert({
+        invest_id: invest_id.value, 
+        reoccuring: true,
+        day: day.value,
+        user_id: user.value.id
+      })
+      .select()
+  } catch (error) {
+    console.log(error)
+  } finally {
+    navigateTo('/invest/payment')
+  }
+}
 </script>
 <template>
   <div class="PageWrapper">
     <Kaltmenu :pageTitle='pagename' />
     <div class="page">
       <div class="section">
-          
-        <p> Reoccuring investment <a href="">?</a></p>
-        <p> Which day to auto-invest?</p>
-        <div>
-            <input id="one" type="radio" value="1" v-model="days" name="days"/>
-            <label for="one">1</label>
-            
-            <input id="two" type="radio" value="2" v-model="days" name="days"/>
-            <label for="two">2</label>
-            <input id="three" type="radio" value="3" v-model="days" name="days"/>
-            <label for="three">3</label>
-            <input id="four" type="radio" value="4" v-model="days" name="days"/>
-            <label for="four">4</label>
-            <input id="five" type="radio" value="5" v-model="days" name="days"/>
-            <label for="five">5</label>
-            <input id="six" type="radio" value="6" v-model="days" name="days"/>
-            <label for="six">6</label>
-                <input id="seven" type="radio" value="7" v-model="days" name="days"/>
-            <label for="seven">7</label>
-                <input id="eight" type="radio" value="8" v-model="days" name="days" />
-            <label for="eight">8</label>
-                <input id="nine" type="radio" value="9" v-model="days" name="days" />
-            <label for="nine">9</label>
-            <input id="ten" type="radio" value="10" v-model="days" name="days" />
-
-            <label for="ten">10</label>
-            <input id="eleven" type="radio" value="11" v-model="days" name="days" />
-
-            <label for="eleven">11</label>
-            <input id="twelve" type="radio" value="12" v-model="days" name="days" />
-
-            <label for="twelve">12</label>
-            <input id="thirteen" type="radio" value="13" v-model="days" name="days" />
-
-            <label for="thirteen">13</label>
-            <input id="fourteen" type="radio" value="14" v-model="days" name="days" />
-
-            <label for="fourteen">14</label>
-            <input id="fifteen" type="radio" value="15" v-model="days" name="days" />
-
-            <label for="fifteen">15</label>
-            <input id="sixteen" type="radio" value="16" v-model="days" name="days" />
-
-            <label for="sixteen">16</label>
-            <input id="seventeen" type="radio" value="17" v-model="days" name="days" />
-
-            <label for="seventeen">17</label>
-            <input id="eighteen" type="radio" value="18" v-model="days" name="days" />
-
-            <label for="eighteen">18</label>
-            <input id="nineteen" type="radio" value="19" v-model="days" name="days" />
-
-            <label for="nineteen">19</label>
-
-            <input id="twenty" type="radio" value="20" v-model="days" name="days" />
-            <label for="twenty">20</label>
-
-            <input id="twentyone" type="radio" value="21" v-model="days" name="days" />
-            <label for="twentyone">21</label>
-
-            <input id="twentytwo" type="radio" value="22" v-model="days" name="days" />
-            <label for="twentytwo">22</label>
-
-            <input id="twentythree" type="radio" value="23" v-model="days" name="days" />
-            <label for="twentythree">23</label>
-
-            <input id="twentyfour" type="radio" value="24" v-model="days" name="days" />
-            <label for="twentyfour">24</label>
-
-            <input id="twentyfive" type="radio" value="25" v-model="days" name="days" />
-            <label for="twentyfive">25</label>
-
-            <input id="twentysix" type="radio" value="26" v-model="days" name="days" />
-            <label for="twentysix">26</label>
-
-            <input id="twentyseven" type="radio" value="27" v-model="days" name="days" />
-            <label for="twentyseven">27</label>
-
-            <input id="twentyeight" type="radio" value="28" v-model="days" name="days" />
-            <label for="twentyeight">28</label>
-
-            <input id="twentynine" type="radio" value="29" v-model="days" name="days" />
-            <label for="twentynine">29</label>
-
-            <input id="thirty" type="radio" value="30" v-model="days" name="days" />
-            <label for="thirty">30</label>
-
-            <input id="thirtyone" type="radio" value="31" v-model="days" name="days" />
-            <label for="thirtyone">31</label>
-            
-        </div>
-          <a href="/invest/complete">
-            <button>finish →</button>
-          </a>
+        <form @submit.prevent="updateCache">
+        <p> Choose the of the month you would like to invest:</p>
+            <input id="a" type="radio" value="1"  v-model="day" name="day"/><label for="a">1 </label>
+            <input id="b" type="radio" value="2"  v-model="day" name="day"/><label for="b">2 </label>
+            <input id="c" type="radio" value="3"  v-model="day" name="day"/><label for="c">3 </label>
+            <input id="d" type="radio" value="4"  v-model="day" name="day"/><label for="d">4 </label>
+            <input id="e" type="radio" value="5"  v-model="day" name="day"/><label for="e">5 </label>
+            <input id="f" type="radio" value="6"  v-model="day" name="day"/><label for="f">6 </label>
+            <input id="g" type="radio" value="7"  v-model="day" name="day"/><label for="g">7 </label>
+            <input id="h" type="radio" value="8"  v-model="day" name="day"/><label for="h">8 </label>
+            <input id="i" type="radio" value="9"  v-model="day" name="day"/><label for="i">9 </label>
+            <input id="j" type="radio" value="10" v-model="day" name="day"/><label for="j">10</label>
+            <input id="k" type="radio" value="11" v-model="day" name="day"/><label for="k">11</label>
+            <input id="l" type="radio" value="12" v-model="day" name="day"/><label for="l">12</label>
+            <input id="m" type="radio" value="13" v-model="day" name="day"/><label for="m">13</label>
+            <input id="n" type="radio" value="14" v-model="day" name="day"/><label for="n">14</label>
+            <input id="o" type="radio" value="15" v-model="day" name="day"/><label for="o">15</label>
+            <input id="p" type="radio" value="16" v-model="day" name="day"/><label for="p">16</label>
+            <input id="q" type="radio" value="17" v-model="day" name="day"/><label for="q">17</label>
+            <input id="r" type="radio" value="18" v-model="day" name="day"/><label for="r">18</label>
+            <input id="s" type="radio" value="19" v-model="day" name="day"/><label for="s">19</label>
+            <input id="t" type="radio" value="20" v-model="day" name="day"/><label for="t">20</label>
+            <input id="u" type="radio" value="21" v-model="day" name="day"/><label for="u">21</label>
+            <input id="v" type="radio" value="22" v-model="day" name="day"/><label for="v">22</label>
+            <input id="w" type="radio" value="23" v-model="day" name="day"/><label for="w">23</label>
+            <input id="x" type="radio" value="24" v-model="day" name="day"/><label for="x">24</label>
+            <input id="y" type="radio" value="25" v-model="day" name="day"/><label for="y">25</label>
+            <input id="z" type="radio" value="26" v-model="day" name="day"/><label for="z">26</label>
+            <input id="A" type="radio" value="27" v-model="day" name="day"/><label for="A">27</label>
+            <input id="B" type="radio" value="28" v-model="day" name="day"/><label for="B">28</label>
+            <input id="C" type="radio" value="29" v-model="day" name="day"/><label for="C">29</label>
+            <input id="D" type="radio" value="30" v-model="day" name="day"/><label for="D">30</label>
+            <input id="E" type="radio" value="31" v-model="day" name="day"/><label for="E">31</label>
+            <input type="submit" value="payment →">
+        </form>
       </div>
     </div>
   </div>
@@ -133,19 +110,22 @@ onMounted(() => {
 <style scoped>
     label{
         display:inline-block;
-        margin:5px;
-        width:8%;
+        margin:0 10px 10px 0;
+        width:11%;
         text-align:center;
+        border:1px dashed gray;
+        border-radius:4px;
 
     }
     label:hover{
-        background:blue;
+      border:1px solid black;
     }
     input[type="radio"] {
     display: none;
     }      
 
     input[type="radio"]:checked+label {
-    background-color: #76cf9f;
+      border:1px solid black;
+        font-weight:500;
     }
 </style>
