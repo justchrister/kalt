33df3fdda39f0f04aeeb27e600356413666dfdaa
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-01-23 20:09:50 +0100

    created oklog usable

--- composables/oklog.ts
+++ composables/oklog.ts
@@ -1,20 +1,9 @@
 export const oklog = (type, text) => {
-  // good
-  let label
-  if (type==='success'){
-    //logger.info(text)
-    label = ' \x1b[42m SUCCESS \x1b[0m '
-  }
-  // almost bad
-  if (type==='warn'){
-    label = ' \x1b[43m WARNING \x1b[0m '
-    //logger.warn(text)
-  }
-  // really bad
-  if (type==='error'){
-    label = ' \x1b[41m ERROR \x1b[0m   '
-    //logger.error(text);
-  }
+  // https://talyian.github.io/ansicolors/
+  let label = '\x1b[34m● \x1b[0m';
+  if (type==='success') label = '\x1b[32m● \x1b[0m'
+  if (type==='warn')    label = '\x1b[43m● \x1b[0m'
+  if (type==='error')   label = '\x1b[33m● \x1b[0m'
   console.log(label + text)
   return
 };
