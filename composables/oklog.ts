e49ca0e3a14c8a913025d7bb7ac296b5862b00a4
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-01-23 22:14:08 +0100

    Fixed colors

--- composables/oklog.ts
+++ composables/oklog.ts
@@ -2,8 +2,8 @@ export const oklog = (type, text) => {
   // https://talyian.github.io/ansicolors/
   let label = '\x1b[34m● \x1b[0m';
   if (type==='success') label = '\x1b[32m● \x1b[0m'
-  if (type==='warn')    label = '\x1b[43m● \x1b[0m'
-  if (type==='error')   label = '\x1b[33m● \x1b[0m'
+  if (type==='warn')    label = '\x1b[33m● \x1b[0m'
+  if (type==='error')   label = '\x1b[31m● \x1b[0m'
   console.log(label + text)
   return
 };
