d9390e64fc02439b895535fe74fa3fcdadb17c09
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-02-25 00:21:11 +0100

    Added auth middleware, adjusted spacing from top

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -3,34 +3,32 @@
     <navbar :pageTitle="pagename" />
     <div class="page">
       <div class="section">
-        <div class="block">
         <tabs />
-          <form @submit.prevent="updateProfile">
-            <div class="col-2 gutter-right">
-              <input-first-name :initial="data.first_name" :user_id="user.id"/>
-            </div>
-            <div class="col-2 gutter-left">
-              <input-last-name :initial="data.last_name" :user_id="user.id"/>
-            </div>
-            <div class="col-3 gutter-right">
-              <select-country :initial="data.country" :user_id="user.id"/>
-            </div>
-            <div class="col-3 gutter-horizontal">
-              <input-city :initial="data.city" :user_id="user.id"/>
-            </div>
-            <div class="col-3 gutter-left">
-              <input-postal-code :initial="data.postal_code" :user_id="user.id"/>
-            </div>
-            <input-address-line :initial="data.address_line" :user_id="user.id"/>
-            <input-birthdate :initial="data.birthdate" :user_id="user.id"/>
-            <div class="col-2 gutter-right">
-              <select-preferred-currency :initial="data.preferred_currency" :user_id="user.id"/>
-            </div>
-            <div class="col-2 gutter-left">
-              <select-preferred-language :initial="data.preferred_language" :user_id="user.id"/>
-            </div>
-          </form>
-        </div>
+        <form @submit.prevent="updateProfile">
+          <div class="col-2 gutter-right">
+            <input-first-name :initial="data.first_name" :user_id="user.id"/>
+          </div>
+          <div class="col-2 gutter-left">
+            <input-last-name :initial="data.last_name" :user_id="user.id"/>
+          </div>
+          <div class="col-3 gutter-right">
+            <select-country :initial="data.country" :user_id="user.id"/>
+          </div>
+          <div class="col-3 gutter-horizontal">
+            <input-city :initial="data.city" :user_id="user.id"/>
+          </div>
+          <div class="col-3 gutter-left">
+            <input-postal-code :initial="data.postal_code" :user_id="user.id"/>
+          </div>
+          <input-address-line :initial="data.address_line" :user_id="user.id"/>
+          <input-birthdate :initial="data.birthdate" :user_id="user.id"/>
+          <div class="col-2 gutter-right">
+            <select-preferred-currency :initial="data.preferred_currency" :user_id="user.id"/>
+          </div>
+          <div class="col-2 gutter-left">
+            <select-preferred-language :initial="data.preferred_language" :user_id="user.id"/>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -50,6 +48,9 @@
   const title = 'Kalt — ' + pagename;
   useHead({
     title,
+  });
+  definePageMeta({
+    middleware: 'auth'
   });
 
   const { data } = await supabase
