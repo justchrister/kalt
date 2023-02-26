aebc542f967c04591c0eecfc0e706b3264e0686f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-02-26 11:03:34 +0100

    Adding grid to bottom navbar

--- components/navbarBottom.vue
+++ components/navbarBottom.vue
@@ -2,22 +2,26 @@
 <div class="page bottom-navbar-wrapper">
     <div class="bottom-navbar">
         <h4>Account:</h4>
-        <nuxt-link to="/portfolio" > Portfolio </nuxt-link>
-        <nuxt-link to="/account" > Account </nuxt-link>
-        <nuxt-link to="/account/notifications" > Notifications </nuxt-link>
-        <nuxt-link to="/account/transactions" > Transactions </nuxt-link>
-        <nuxt-link to="/account/manage-cards" > Cards </nuxt-link>
-        <nuxt-link to="/account/manage-subscriptions" > Subscription </nuxt-link>
-        <nuxt-link to="/auth/" > sign up </nuxt-link>
-        <nuxt-link to="/auth/" > sign in </nuxt-link>
-        <nuxt-link to="/auth/sign-out" > sign out 👋 </nuxt-link>
+        <div class="grid-col-3">
+          <nuxt-link to="/portfolio" > Portfolio </nuxt-link>
+          <nuxt-link to="/account" > Account </nuxt-link>
+          <nuxt-link to="/account/notifications" > Notifications </nuxt-link>
+          <nuxt-link to="/account/transactions" > Transactions </nuxt-link>
+          <nuxt-link to="/account/manage-cards" > Cards </nuxt-link>
+          <nuxt-link to="/account/manage-subscriptions" > Subscription </nuxt-link>
+          <nuxt-link to="/auth/" > sign up </nuxt-link>
+          <nuxt-link to="/auth/" > sign in </nuxt-link>
+          <nuxt-link to="/auth/sign-out" > sign out 👋 </nuxt-link>
+        </div>
         <h4>Content:</h4>
-        <nuxt-link to="/about" > about us </nuxt-link>
-        <nuxt-link to="/about/values" > our values </nuxt-link>
-        <nuxt-link to="/about/mission" > our mission </nuxt-link>
-        <nuxt-link to="/questions" > Q&A </nuxt-link>
-        <nuxt-link to="/questions/how-does-it-work" > how it works </nuxt-link>
-        <nuxt-link to="/questions/how-do-you-make-money" > how we make money </nuxt-link>
+        <div class="grid-col-3">
+          <nuxt-link to="/about" > about us </nuxt-link>
+          <nuxt-link to="/about/values" > our values </nuxt-link>
+          <nuxt-link to="/about/mission" > our mission </nuxt-link>
+          <nuxt-link to="/questions" > Q&A </nuxt-link>
+          <nuxt-link to="/questions/how-does-it-work" > how it works </nuxt-link>
+          <nuxt-link to="/questions/how-do-you-make-money" > how we make money </nuxt-link>
+        </div>
         <div class="insignia">Make money, make a difference.</div>
     </div>
   </div>
