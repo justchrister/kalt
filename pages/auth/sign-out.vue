9ca1dc87d28e3b4a179edf6d1fbfdc3e1e552065
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-02-28 21:27:39 +0100

    just removing the forwarding, to see what that does

--- pages/auth/sign-out.vue
+++ pages/auth/sign-out.vue
@@ -3,9 +3,5 @@
 </template>
 <script lang="ts" setup>
   const supabase = useSupabaseClient()
-  const router = useRouter()
   const { error } = await supabase.auth.signOut()
-  if(!error){
-    router.go(-1)
-  }
 </script>
