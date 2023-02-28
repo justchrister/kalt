43875c3587bfafedead1468e0822481dc09ae1ff
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-03-01 00:32:56 +0100

    KALT-123

--- components/navbarBottom.vue
+++ components/navbarBottom.vue
@@ -9,18 +9,18 @@
           <nuxt-link to="/account/transactions" > Transactions </nuxt-link>
           <nuxt-link to="/account/cards" > Cards </nuxt-link>
           <nuxt-link to="/subscription" > Subscription </nuxt-link>
-          <nuxt-link to="/auth/" > sign up </nuxt-link>
-          <nuxt-link to="/auth/" > sign in </nuxt-link>
-          <nuxt-link to="/auth/sign-out" > sign out 👋 </nuxt-link>
+          <nuxt-link to="/auth/" > Sign up </nuxt-link>
+          <nuxt-link to="/auth/" > Sign in </nuxt-link>
+          <nuxt-link to="/auth/sign-out" > Sign out 👋 </nuxt-link>
         </div>
         <h4>Content:</h4>
         <div class="grid-col-3">
-          <nuxt-link to="/about" > about us </nuxt-link>
-          <nuxt-link to="/about/values" > our values </nuxt-link>
-          <nuxt-link to="/about/mission" > our mission </nuxt-link>
+          <nuxt-link to="/about" > About us </nuxt-link>
+          <nuxt-link to="/about/values" > Our values </nuxt-link>
+          <nuxt-link to="/about/mission" > Our mission </nuxt-link>
           <nuxt-link to="/questions" > Q&A </nuxt-link>
-          <nuxt-link to="/questions/how-does-it-work" > how it works </nuxt-link>
-          <nuxt-link to="/questions/how-do-you-make-money" > how we make money </nuxt-link>
+          <nuxt-link to="/questions/how-does-it-work" > How it works </nuxt-link>
+          <nuxt-link to="/questions/how-do-you-make-money" > How we make money </nuxt-link>
         </div>
         <div class="insignia">Make money, make a difference.</div>
     </div>
