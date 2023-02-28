419e723ab0861f3be868654176331d8bf27b6a4e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-02-28 07:26:33 +0100

    Added scroll to pills

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -10,31 +10,64 @@
             <chart-portfolio :days="days" />
           </div>
         </div>
-        <nav class="pills filters">
-          <button @click="setDays(7)" class="pill">
-            7 days
-          </button>
-          <button @click="setDays(30)" class="pill">
-            30 days
-          </button>
-          <button @click="setDays(181)" class="pill">
-            6 months
-          </button>
-          <button @click="setDays(365)" class="pill">
-            max
-          </button>
+        <nav class="component pills filters">
+          <div class="overflow-wrap">
+            <ul>
+              <li>
+                <button @click="setDays(1)" class="element pill">
+                  live
+                </button>
+              </li>
+              <li>
+                <button @click="setDays(7)" class="element pill">
+                  7 days
+                </button>
+              </li>
+              <li>
+                <button @click="setDays(90)" class="element pill">
+                  3 months
+                </button>
+              </li>
+              <li>
+                <button @click="setDays(180)" class="element pill">
+                  6 months
+                </button>
+              </li>
+              <li>
+                <button @click="setDays(365)" class="element pill">
+                  1 year
+                </button>
+              </li>
+              <li>
+                <button @click="setDays(1095)" class="element pill">
+                  3 years
+                </button>
+              </li>
+              <li>
+                <button @click="setDays(1825)" class="element pill">
+                  5 years
+                </button>
+              </li>
+              <li>
+                <!-- should be days since first investment-->
+                <button @click="setDays(1825)" class="element pill">
+                  max
+                </button>
+              </li>
+            </ul>
+          </div>
         </nav>
         <div class="block center-text">
           <cta showDivest="true"/>
         </div>
         <div class="block">
           <h3>Our impact in numbers <omoji emoji="😮"/></h3>
-          <p> Your investments has helped decrease the emissions in the worls as much as </p>
+          <p> Your investments has decreased your carbon footprint by 90 tonnes of Co2, in other words:  </p>
           <div class="impacts">
             <impact type="car" title="Driving 1 000 km in a Fiat Panda" />
             <impact type="house" title="Construction of a 200 m2 house" />
             <impact type="plane" title="Flying 1.4 times to Paris from Tokyo" />
-            <impact type="computer" title="Building and selling 10 computers" />
+            <impact type="computer" title="Producing, shipping and eating 9 000 avocadoes" />
           </div>
         </div>
       </div>
