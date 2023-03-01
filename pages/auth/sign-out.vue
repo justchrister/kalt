34fa43f643f1ef876a461b86fb5a5b478839068c
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-03-01 21:20:26 +0100

    KALT-129,KALT-114

--- pages/auth/sign-out.vue
+++ pages/auth/sign-out.vue
@@ -1,9 +1,20 @@
 <template>
-<h1> signing you out </h1>
+  <main>
+    <navbar :pageTitle="pagename" />
+    <div class='page'>
+      <div class="section">
+        <div class="block">        
+          <Image :images="['/media/images/dall-e/404_2.png']" /> 
+        </div>
+        <div class="block">
+          <h1>See you soon!</h1>
+          <p>Press the menu button, or go back to the previous page.</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 <script lang="ts" setup>
-  const supabase = useSupabaseClient()
   const client = useSupabaseAuthClient()
   const { error } = await client.auth.signOut()
-  if(!error) navigateTo('/')
 </script>
