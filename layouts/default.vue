4ab56ca8f149daa0f5e73b44321be992677fe4b7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-03-05 16:20:41 +0100

    Added jobs console.log

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,3 +4,18 @@
     <navbar-bottom />
   </div>
 </template>
+<script setup lang="ts">
+console.log("  ")
+console.log("\x1b[34m                                                 \x1b[32m  88               88                      ")
+console.log("\x1b[34m88                      88    ,d            d8\" \x1b[32m                    88                      ")
+console.log("\x1b[34m88   ,d8   ,adPPYYba,   88  MM88MMM       ,8P'   \x1b[32m  88   ,adPPYba,   88,dPPYba,   ,adPPYba,  ")
+console.log("\x1b[34m88 ,a8\"    \"\"     `Y8   88    88         d8\" \x1b[32m      88  a8\"     \"8a  88P'    \"8a  I8[    \"\"  ")
+console.log("\x1b[34m8888[      ,adPPPPP88   88    88       ,8P'    \x1b[32m    88  8b       d8  88       d8   `\"Y8ba,   ")
+console.log("\x1b[34m88`\"Yba,   88,    ,88   88    88,     d8\"  \x1b[32m        88  \"8a,   ,a8\"  88b,   ,a8\"  aa    ]8I  ")
+console.log("\x1b[34m88   `Y8a  `\"8bbdP\"Y8   88    \"Y888  8P' \x1b[32m   88,   ,88    `\"YbbdP\"'   8Y\"Ybbd8\"'   `\"YbbdP\"'  ")
+console.log("\x1b[34m                                           \x1b[32m  \"\"bbb\"\"                                       ")
+console.log("\x1b[0m")
+console.log("Apply for a job today at \x1b[34mhttps://ka.lt/jobs\x1b[0m, and check out \x1b[34mhttps://github.com/justchrister\x1b[0m")
+
+
+</script>
