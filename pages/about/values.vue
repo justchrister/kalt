a5c907d5596552cdb6954aff08a1e87cc4c81a84
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-03-19 19:11:58 +0100

    Update values.vue

--- pages/about/values.vue
+++ pages/about/values.vue
@@ -1,16 +1,42 @@
 <template>
   <main class="values">
     <navbar :pageTitle="pagename" />
-    <div class="headline">
-      <h1>Our Values —</h1>
+    <div class="intro">
+      <div class="section">
+        <div class="block">
+          <h3>Our values and goals</h3>
+          <p>
+            All humans are our stakeholders, we try to find a slightly better solution to each challenge we find. Trying to make a slightly better world for everyone at each turn.
+          </p>
+        </div>
+      </div>
     </div>
     <div class='page'>
       <div class="section">
         <div class="block">
-          <h1>Build a new society </h1>
-
+          <h4>Create meaningful impact</h4>
+          <p>
+            Impact and wealth creation are not in opposition, they should work together, where wealth creation creates more impact and vice versa.
+          </p>
         </div>
-        <Cta />
+        <div class="block">
+          <h4>Aim for clarity</h4>
+          <p>
+            Everyone should understand what they are investing in, the risks, and how its contributing to the wealth creation.
+          </p>
+        </div>
+        <div class="block">
+          <h4>Simple first, then powerful</h4>
+          <p>
+            Focus on simplicity in functionality before adding a bunch of shiny features that might not accomplish much. Skip the nonesense and jargon.
+          </p>
+        </div>
+        <div class="block">
+          <h4>Build for people</h4>
+          <p>
+            Both our customer facing solution, such as this website, but also the infrastructure we are creating globally, needs to be built for the people actually living there.
+          </p>
+        </div>
       </div>
     </div>
   </main>
@@ -26,3 +52,8 @@
     }]
   });
 </script>
+<style scoped>
+.intro{
+  background: #0CF574;
+}
+</style>
