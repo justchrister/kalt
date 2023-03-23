a63a6b385119f34da8b8f095296a99d831289929
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-03-23 20:39:06 +0100

    Minors

--- composables/messaging.ts
+++ composables/messaging.ts
@@ -3,48 +3,25 @@
 const timestamp = async () => {
   return Date.now()
 }
-const uuid = () =>{
-  const hex = Math.floor(Math.random() * 0xfffffffffffff).toString(16).padStart(12, '0');
-  const uuid = `${hex.substr(0, 8)}-${hex.substr(8, 4)}-4${hex.substr(12, 3)}-${Math.floor(Math.random() * 0xfff).toString(16).padStart(3, '0')}-${Math.floor(Math.random() * 0xfffffffffffff).toString(16).padStart(12, '0')}`;
-  return uuid;
-}
-const httpPost = async (api, json) => {
-  fetch("https://kalt.requestcatcher.com/", {
-    method: "POST",
-    body: JSON.stringify(json),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  });
-  return
-}
 
-export const publishOrder = (from, user, quantity, currency) => {
+export const publishTransaction = (
+  me: string,
+  user: string,
+  quantity: number,
+  currency: string,
+  transactionType: transactionType
+) => {
   let message = {
-    "from": from,
+    "from": me,
     "timestamp": timestamp(),
-    "messageId": uuid(),
+    "messageId": null, // auto generated
     "message": {
       "user_id": user,
       "quantity": quantity,
-      "currency": currency
+      "currency": currency,
+      "transaction_type": transactionType
     }
   }
-  httpPost("/api/adapter/orderCreated", message)
+  console.log(message)
   return message
-};
-
-export const publishTransaction = (from, user, quantity, currency) => {
-  let message = {
-    "from": from,
-    "timestamp": timestamp(),
-    "messageId": uuid(),
-    "message": {
-      "user_id": user,
-      "quantity": quantity,
-      "currency": currency
-    }
-  }
-  httpPost("/api/adapter/orderCreated", message)
-  return message
-};
+}
--- types/index.ts
+++ types/index.ts
@@ -3,17 +3,23 @@
 export { };
 
 declare global {
-  type goExchangeOrder = [boolean, string, number]; 
 
-  interface MyFancyInterface {
+  // types
+  type uuid = string;
+
+  interface Transaction {
+    id: uuid;
+    user_id: uuid;
+    type: number;
     
   }
+
   // makes code related to exchange orders more readable
   const enum exchangeOrderTypes {
     buyOrder = 0,
     sellOrder = 1
   }
-  const enum transactionTypes {
+  const enum transactionType {
     deposit = 0,
     withdrawal = 1,
     dividend = 2,
