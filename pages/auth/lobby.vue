773800f4fd852d884426554519b0eaadbfba6c68
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-02 01:40:46 +0200

    margin

--- pages/auth/lobby.vue
+++ pages/auth/lobby.vue
@@ -5,7 +5,7 @@
       <frame-image src="/media/images/audun-alvestad/waiting-room.png" />
       <frame-image src="/media/images/apollo-celebration.jpg" />
     </frame-wrap>
-    <block>
+    <block margin="half">
       <h1> One small step for you, a big step for mankind. </h1>
       <p> You are just one step away from investing in the future of fossil-free energy. Check your email for our invitation. </p>
       <!--
