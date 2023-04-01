c1f5698bfa1d23754e2ba3b088feb1abe29672c7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-02 00:06:12 +0200

    Create account.vue

---
+++ components/account.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="account">
+    {{name}}
+    {{amount}}
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true
+    },
+    uuid: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+  .account{
+    border:$border;
+    padding:$clamp $clamp-2;
+  }
+</style>
