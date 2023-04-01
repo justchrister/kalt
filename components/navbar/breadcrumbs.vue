52b2b916f391c06af58f28a14c8954c13d08dfb7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-02 00:06:22 +0200

    breadcrumbs

--- components/navbar/breadcrumbs.vue
+++ components/navbar/breadcrumbs.vue
@@ -1,170 +1,25 @@
 <template>
-    <header>
-        <nav class="menu">
-            <ul id="menu_items">
-                <li class="logomark" v-if="signedIn" v-on:click="toggleMenuOff">
-                    <nuxt-link to="/portfolio">
-                    {{ pageTitle }}
-                    </nuxt-link>
-                </li>
-                <li class="logomark" v-else v-on:click="toggleMenuOff">
-                    <nuxt-link to="/">
-                    {{ pageTitle }}
-                    </nuxt-link>
-                </li>
-                <li>
-                    <span>Kalt — </span>
-                    <nuxt-link to="/about" v-on:click="toggleMenu"> About </nuxt-link>
-                </li>
-                <li>
-                    <span>Kalt — </span>
-                    <nuxt-link to="/questions/how-does-it-work" v-on:click="toggleMenu"> How it works </nuxt-link>
-                </li>
-                <li v-if="signedIn">
-                    <span>Kalt — </span>
-                    <nuxt-link to="/portfolio" v-on:click="toggleMenu"> Portfolio </nuxt-link>
-                </li>
-                <li v-if="signedIn">
-                    <span>Kalt — </span>
-                    <nuxt-link to="/profile" v-on:click="toggleMenu"> Account </nuxt-link>
-                </li>
-                <li v-if="!signedIn">
-                    <span>Kalt — </span>
-                    <nuxt-link to="/auth/sign-up" v-on:click="toggleMenu"> Sign up </nuxt-link>
-                </li>
-                <li v-if="!signedIn">
-                    <span>Kalt — </span>
-                    <nuxt-link to="/auth" v-on:click="toggleMenu"> Sign in </nuxt-link>
-                </li>
-                <li v-if="signedIn">
-                    <span>Kalt — </span>
-                    <a href="/auth/sign-out" v-on:click="toggleMenu"> Sign out </a>
-                </li>
-            </ul>
-        </nav>
-        
-        <button class="menu-toggle" v-on:click="toggleMenu">menu</button>
-        <nuxt-link to="/portfolio" v-on:click="toggleMenuOff">
-            <div class="my-account-button"></div>
-        </nuxt-link>
-    </header>
+  <block margin="none">
+    <nuxt-link :to="parentUrl"> {{parent}}</nuxt-link> > {{route.meta.pagename}}
+  </block>
 </template>
-
-<script setup>
-    let signedIn = false
-    const supabase = useSupabaseClient()
-    const user = useSupabaseUser()
-    if(user.value) signedIn = true
-    const toggleMenu = async () => { 
-        document.getElementsByTagName("body")[0].classList.toggle("show-menu");
+<script setup lang="ts">
+  const route = useRoute()
+  const props = defineProps({
+    parent: {
+      type: String,
+      required: true
     }
-    const toggleMenuOff = async () => { 
-        document.getElementsByTagName("body")[0].classList.remove("show-menu");
-    }
-
-    const props = defineProps({
-        pageTitle: {
-            type: String,
-            required: false
-        }
-    })
+  })
+  const parentUrl = "/"+props.parent.toLowerCase()
+  console.log(parentUrl)
 </script>
+
 <style scoped lang="scss">
-.menu ul{
-  position: fixed;
-  z-index: 3;
-  margin: 0;
-  height: $clamp-4;
-  overflow:hidden;
-  padding: $big-clamp-half 0 0 $big-clamp-half;
-  li.logomark,
-  li.logomark span{
-    opacity:1;
-    pointer-events : all;
-  }
-  li {
-    opacity:0;
-    pointer-events : none;
-    padding: 0;
-    margin-bottom:0;
-    a{
-      text-decoration: none;
-      &:hover{
-        text-decoration:underline;
-      }
-    }
-    span{
-      font-weight:bold;
-      opacity:0;
-    }
-    &:before{
-      display:none;
+  a{
+    text-decoration:none;
+    &:hover{
+      text-decoration:underline;
     }
   }
-}
-
-header .menu-toggle,
-header .menu ul li {
-  font-family: $sans-serif;
-  font-size:$clamp-2;
-  font-weight:normal;
-  line-height: 150%;
-  font-display: optional;
-}
-button.menu-toggle {
-  padding: $big-clamp-half;
-  right: $big-clamp;
-  position: fixed;
-  top: $border-width;
-  width: auto;
-  color: $dark;
-  border: 0;
-  &:hover,
-  &:hover:before{
-    text-decoration: underline;
-  }
-  &:before{
-    content:"close ";
-    white-space: pre;
-    display:inline-block;
-    opacity:0;
-  }
-}
-.my-account-button{
-  width:$clamp-2;
-  height:$clamp-2;
-  display:block;
-  position:fixed;
-  top:$border-width;
-  right:$border-width;
-  background-image: url("/omoji/happy.svg");
-  background-size:contain;
-  background-position:center;
-  box-sizing: border-box;
-  margin: $big-clamp-half;
-  margin-top:clamp($unit-min*2.5, $unit*2.5, $unit-max*2.5);
-  &:hover{
-    background-image: url("/omoji/super-happy.svg");
-  }
-}
-</style>
-<style lang="scss">
-.show-menu{
-  .bottom-navbar,
-  .main{
-    opacity:0;
-    pointer-events : none;
-  }
-  .menu ul{
-    overflow: visible;
-  }
-  .menu ul li{
-    opacity:1;
-    pointer-events : all;
-  }
-  header button.menu-toggle:before{
-    opacity:1;
-  }
-}
-
 </style>
