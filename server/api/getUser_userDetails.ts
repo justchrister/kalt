0d3ef0d461bd0e7f3aba362b04e7bb6df18a661f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-07 13:19:34 +0200

    boom

--- server/api/getUser_userDetails.ts
+++ server/api/getUser_userDetails.ts
@@ -26,19 +26,19 @@ export default defineEventHandler( async (event) => {
   let json = {
 
   }
-  if(message.user_id)     json.user_id =    message.user_id
-  if(message.first_name)  json.first_name = message.first_name
-  if(message.last_name)   json.last_name =  message.last_name
-  if(message.country)   json.country =  message.country
-  if(message.city)   json.city =  message.city
-  if(message.postal_code)   json.postal_code =  message.postal_code
-  if(message.birthdate)   json.birthdate =  message.birthdate
-  if(message.address_line_1)   json.address_line_1 =  message.address_line_1
-  if(message.address_line_2)   json.address_line_2 =  message.address_line_2
+  if(message.address_line_1) json.address_line_1 = message.address_line_1
+  if(message.address_line_2) json.address_line_2 = message.address_line_2
+  if(message.postal_code) json.postal_code = message.postal_code
+  if(message.first_name) json.first_name = message.first_name
+  if(message.birthdate) json.birthdate =  message.birthdate
+  if(message.last_name) json.last_name = message.last_name
+  if(message.country) json.country = message.country
+  if(message.city) json.city = message.city
 
   const { data, error } = await supabase
     .from(serviceKebab)
     .upsert(json)
+    .eq('user_id', message.user_id)
     .select()
 
   if(data) return data
---
+++ server/api/getUser_userPreferences.ts
@@ -0,0 +1,34 @@
+import { ok } from '~/composables/ok'
+import { messaging } from '~/composables/messaging'
+
+import { serverSupabaseServiceRole } from '#supabase/server'
+
+export default defineEventHandler( async (event) => {
+  const supabase = serverSupabaseServiceRole(event)
+  const topic = 'userDetails'
+  const topicKebab = ok.camelToKebab(topic)
+  const service = 'getUser'
+  const serviceKebab = ok.camelToKebab(topic)
+  const query = getQuery(event)
+  const body = await readBody(event)
+  if(body.record.message_read) return 'message already read'
+  
+  const message = await messaging.getEntity(supabase, topicSub, body.record.message_entity_id)
+  const readMessage = await messaging.read(supabase, topicSub, service, body.record.message_id)
+
+  let json = {}
+  if(message.auto_invest) json.auto_invest = message.auto_invest
+  if(message.email_marketing) json.email_marketing = message.email_marketing
+  if(message.color_schema) json.color_schema = message.color_schema
+  if(message.language) json.language = message.language
+  if(message.currency) json.currency = message.currency
+
+  const { data, error } = await supabase
+    .from(serviceKebab)
+    .upsert(json)
+    .eq('user_id', message.user_id)
+    .select()
+
+  if(data) return data
+  if(error) return error
+});
