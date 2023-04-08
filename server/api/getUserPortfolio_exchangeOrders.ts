d5b959f494ccfbc3b06006bbbccc20670e2dda84
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-08 10:13:02 +0200

    Update getUserPortfolio_exchangeOrders.ts

--- server/api/getUserPortfolio_exchangeOrders.ts
+++ server/api/getUserPortfolio_exchangeOrders.ts
@@ -1,25 +1,67 @@
 import { ok } from '~/composables/ok'
+import { messaging } from '~/composables/messaging'
 import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler( async (event) => {
-  const supabase = serverSupabaseServiceRole(event)
-  const query = getQuery(event)
-  const body = await readBody(event)
-  if(body.record.message_read) return 'message already read'
+  const supabase = serverSupabaseServiceRole(event);
+  const query = getQuery(event);
+  const topic = 'exchangeOrders';
+  const service = 'getUserPortfolio';
+  const body = await readBody(event);
+  if(body.record.message_read) return 'message already read';
   
-  const message = messaging.getEntity(
+  const message = await messaging.getEntity(
     supabase,
-    'userPreferences',
-    'getUserPortfolio',
+    topic,
     body.record.message_entity_id
-  )
-  if(!message.preferred_currency) return 'different field'
+  );
 
   const readMessage = await messaging.read(
     supabase,
-    body.record.message_id)
+    topic,
+    service,
+    body.record.message_id
+  );
+  const date = new Date(message.message_created);
+  // Create a new date object at the start of the day
+  const dateFrom = date;
+  dateFrom.setHours(0, 0, 0, 0);
 
-  if(data) return data
-  if(error) return error
+  // Create a new date object at the start of the next day
+  const dateTo = new Date(dateFrom);
+  dateTo.setDate(dateTo.getDate() + 1);
 
+  const dateFromISOString = dateFrom.toISOString();
+  const dateToISOString = dateTo.toISOString();
+  
+  const { data: dailyOrders, error: dailyOrdersError } = await supabase
+    .from('exchange_orders')
+    .select()
+    .eq('user_id', message.user_id)
+    .eq('order_status', 'fulfilled')
+    .eq('ticker', message.ticker)
+    .gte('message_created', dateFromISOString)
+    .lt('message_created', dateToISOString)
+  
+  const json = {
+    'user_id': message.user_id,
+    'date': date,
+    'ticker': message.ticker,
+    'quantity_change': 0
+  };
+
+  for (let i = 0; i < dailyOrders.length; i++) {
+    const order = dailyOrders[i];
+    json.quantity_change += order.quantity;
+  };
+
+  const { data, error} = await supabase
+    .from('get_user_portfolio')
+    .upsert(json)
+    .eq('user_id', message.user_id)
+    .eq('date', date)
+    .eq('ticker', message.ticker);
+  
+  if(data) return data;
+  if(error) return error;
 });
