0f28f9eacf6f9cd9053d45669162c51c305c6d1a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-10 17:19:30 +0200

    Update _userPreferences.ts

--- server/api/getUserPortfolio/_userPreferences.ts
+++ server/api/getUserPortfolio/_userPreferences.ts
@@ -6,18 +6,18 @@ export default defineEventHandler( async (event) => {
   const query = getQuery(event)
   const service = 'getUserPortfolio';
   const topic = 'exchangeOrders';
+  const ticker = 'gi.ddf';
   const body = await readBody(event)
   if(body.record.message_read) return 'message already read'
   
-  const message = messaging.getEntity(
-    topic,
-    service,
-    body.record.message_entity_id
-  )
+  const message = await messaging.getEntity(supabase, topic, body.record.message_entity_id);
+  const readMessage = await messaging.read(supabase, topic, service, body.record.message_id);
+
   const { data, error } = await supabase
     .from('get_user_portfolio')
     .update({ 'currency_value': message.currency })
-    .eq('user_id', message.user_id);
+    .eq('user_id', message.user_id)
+    .eq('ticker', ticker);
 
   if(data) return data
   if(error) return error
