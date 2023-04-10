8d1dfe563de10d03a4418266228ab15e2d845633
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-10 18:13:44 +0200

    Update _userPreferences.ts

--- server/api/getUserPortfolio/_userPreferences.ts
+++ server/api/getUserPortfolio/_userPreferences.ts
@@ -1,22 +1,25 @@
 import { ok } from '~/composables/ok'
+import { messaging } from '~/composables/messaging'
 import { serverSupabaseServiceRole } from '#supabase/server'
 // this one should get the new currency and replace that in the value_currency column
 export default defineEventHandler( async (event) => {
   const supabase = serverSupabaseServiceRole(event)
   const query = getQuery(event)
   const service = 'getUserPortfolio';
-  const topic = 'exchangeOrders';
+  const topic = 'userPreferences';
   const ticker = 'gi.ddf';
   const body = await readBody(event)
   if(body.record.message_read) return 'message already read'
   
   const message = await messaging.getEntity(supabase, topic, body.record.message_entity_id);
   const readMessage = await messaging.read(supabase, topic, service, body.record.message_id);
+
   const { data, error } = await supabase
     .from('get_user_portfolio')
-    .update({ 'currency_value': message.currency })
+    .update({ 'value_currency': message.currency })
     .eq('user_id', message.user_id)
-    .eq('ticker', ticker);
+    .eq('ticker', ticker)
+    .select();
 
   if(data) return data
   if(error) return error
