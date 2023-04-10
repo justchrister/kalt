432e0287d8523758099aa6e9f020810b963baf14
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-10 22:59:38 +0200

    Update addQuantityToday.ts

--- server/api/getUserPortfolio/addQuantityToday.ts
+++ server/api/getUserPortfolio/addQuantityToday.ts
@@ -1,57 +1,39 @@
+import { ok } from '~/composables/ok';
 import { serverSupabaseServiceRole } from '#supabase/server';
 
-
-const updatePortfolioQuantityToday = async (userId, date, ticker, quantity_today, supabase) => {
-  await supabase
-    .from('get_user_portfolio')
-    .update({
-      'quantity_today': quantity_today
-    })
-    .eq('user_id', userId)
-    .eq('date', date)
-    .eq('ticker', ticker);
-};
-
-const updateUserPortfolioQuantityToday = async (userId, ticker, supabase) => {
-  const portfolio = await getUserPortfolio(userId, supabase);
-  let quantity_today = 0;
-
-  for (const portfolioItem of portfolio) {
-    const { quantity_change, date } = portfolioItem;
-    quantity_today += quantity_change;
-    await updatePortfolioQuantityToday(userId, date, ticker, quantity_today, supabase);
-  }
-};
-
-const getUserPortfolio = async (userId, supabase) => {
-  const { data } = await supabase
-    .from('get_user_portfolio')
-    .select()
-    .eq("user_id", userId)
-    .order('date', { ascending: true });
-
-  return data;
-};
-
-const getUsers = async (supabase) => {
-  const { data } = await supabase
-    .from('get_user')
-    .select('user_id');
-
-  return data;
-};
-
 export default defineEventHandler(async (event) => {
-  const supabase = serverSupabaseServiceRole(event);
+  const supabase = serverSupabaseServiceRole(event)
+  const query = getQuery(event)
+  const service = 'getUserPortfolio';
+  const topic = 'userPreferences';
   const ticker = 'gi.ddf';
+  const body = await readBody(event)
 
-  const users = await getUsers(supabase);
+  const getDays = async (user) => {
+    const { data, error } = await supabase
+      .from('get_user_portfolio')
+      .select('user_id, quantity_change, date, ticker')
+      .eq('user_id', user)
+      .order('date', { ascending: true })
+    if (data) return data
+  };
 
-  for (const { user_id } of users) {
-    console.log(user_id);
-    console.log(ticker);
-    await updateUserPortfolioQuantityToday(user_id, ticker, supabase);
+  const json = []
+  let quantity_today = 0;
+  const days = await getDays(body.record.user_id);
+  for (let i = 0; i < days.length; i++) {
+    const current = days[i];
+    quantity_today += current.quantity_change
+    json.push({
+      "user_id": current.user_id,
+      "ticker": current.ticker,
+      "date": current.date,
+      "quantity_today": quantity_today,
+    })
   }
-
-  return 'Quantity today has been calculated and updated';
+  const { data, error } = await supabase
+    .from('get_user_portfolio')
+    .update(json)
+    .eq('user_id', user)
+    .eq('ticker', ticker);
 });
