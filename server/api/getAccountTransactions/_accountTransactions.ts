54f827ae9fc0b582478295eb1e60d0fcdb45d609
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-10 12:07:22 +0200

    Update _accountTransactions.ts

--- server/api/getAccountTransactions/_accountTransactions.ts
+++ server/api/getAccountTransactions/_accountTransactions.ts
@@ -4,8 +4,8 @@ import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler( async (event) => {
   const supabase = serverSupabaseServiceRole(event);
-  const service = 'getAccountBalance';
-  const topic = 'userPreferences';
+  const service = 'getAccountTransactions';
+  const topic = 'accountTransactions';
   const serviceKebab = ok.camelToKebab(service);
   const topicKebab = ok.camelToKebab(topic);
   const query = getQuery(event);
@@ -18,39 +18,39 @@ export default defineEventHandler( async (event) => {
     body.record.message_entity_id)
 
   await messaging.read( supabase, topic, service, body.record.message_id)
-  
-  let json = {
-    "user_id": message.user_id,
-    "amount": null,
-    "currency": message.currency
-  }
 
-  const getTransactions = async () => {
-      const { data, error } = await supabase
+  const getDistinctTransactions = async () => {
+    const { data, error } = await supabase
       .from('account_transactions')
-      .select()
-      .or('transaction_status.eq.payment_accepted,transaction_status.eq.withdrawal_accepted')
+      .select('message_entity_id')
       .eq('user_id', message.user_id);
-    return data
-  }
-  const transactions = await getTransactions();
 
+    const distinctMessageEntityIds = [...new Set(data.map(transaction => transaction.message_entity_id))];
+    return distinctMessageEntityIds
+  }
+  const transactions = await getDistinctTransactions();
+  const inserted = []
   for (let i = 0; i < transactions.length; i++) {
-    const transaction = transactions[i];
-    const amountConverted = await messaging.convertCurrency(
+    const transaction = await messaging.getEntity(
       supabase,
-      transaction.amount, 
-      transaction.currency,
-      message.currency
-    );
-    json.amount += parseFloat(amountConverted);
+      topic,
+      transactions[i])
+    console.log(transaction)
+    const { data, error} = await supabase
+      .from(serviceKebab)
+      .upsert({
+        "user_id": transaction.user_id,
+        "amount": transaction.amount,
+        "currency": transaction.currency,
+        "date": transaction.message_created,
+        "type": transaction.transaction_type,
+        "sub_type": transaction.transaction_sub_type
+      })
+      .select()
+      .limit(1)
+      .single()
+    if(error) ok.log('error', 'could not insert', error)
+    inserted.push(data)
   }
-
-  const { data, error } = await supabase
-    .from('get_account_balance')
-    .upsert(json)
-    .select()
-  
-  if(data) return data
-  if(error) return error
+  return inserted
 });
