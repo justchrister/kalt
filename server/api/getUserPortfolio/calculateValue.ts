6511952bbf2da70f3fe0215f6d49ddf0d926d983
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-11 19:12:15 +0200

    Update calculateValue.ts

--- server/api/getUserPortfolio/calculateValue.ts
+++ server/api/getUserPortfolio/calculateValue.ts
@@ -5,9 +5,10 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   if(body.record.quantity_today===body.old_record.quantity_today) return "No change in quantity_today"
   const assetPrice = await messaging.getAssetPrice(supabase, body.record.value_currency, body.record.ticker)
+  const valueCalculated = body.record.quantity_today/assetPrice;
   const { data, error } = await supabase
     .from('get_user_portfolio')
-    .update({"value": body.record.quantity_today/assetPrice})
+    .update({"value": valueCalculated})
     .eq('date', body.record.date)
     .eq('ticker', body.record.ticker)
     .eq('user_id', body.record.user_id)
