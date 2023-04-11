dab846b76221bbefacb61e44abe48255be2fd8aa
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-11 19:09:10 +0200

    Update calculateValue.ts

--- server/api/getUserPortfolio/calculateValue.ts
+++ server/api/getUserPortfolio/calculateValue.ts
@@ -1,60 +1,16 @@
-import { ok } from '~/composables/ok'
 import { messaging } from '~/composables/messaging'
 import { serverSupabaseServiceRole } from '#supabase/server'
-
-const updateUserPortfolio = async (userId, preferredCurrency, ticker, supabase) => {
-  const portfolio = await getUserPortfolio(userId, supabase);
-
-  for (const portfolioItem of portfolio) {
-    const { quantity_change, date } = portfolioItem;
-    
-    const assetPrice = await messaging.getAssetPrice(supabase, preferredCurrency, ticker);
-    const value = quantity_today / assetPrice;
-    
-    await updatePortfolioValue(userId, date, ticker, value, preferredCurrency, supabase);
-  }
-};
-
-const getUserPortfolio = async (userId, supabase) => {
-  const { data } = await supabase
-    .from('get_user_portfolio')
-    .select()
-    .eq("user_id", userId);
-
-  return data;
-};
-
-const getPreferredCurrencies = async (supabase) => {
-  const { data } = await supabase
-    .from('get_user')
-    .select('user_id, currency');
-
-  return data;
-};
-
-const updatePortfolioValue = async (userId, date, ticker, value, preferredCurrency, supabase) => {
-  const { data } = await supabase
-    .from('get_user_portfolio')
-    .update({
-      'value': value,
-      'value_currency': preferredCurrency
-    })
-    .eq('user_id', userId)
-    .eq('date', date)
-    .eq('ticker', ticker)
-    .select();
-};
-
 export default defineEventHandler(async (event) => {
-  return 'im fucked rebuild me'
   const supabase = serverSupabaseServiceRole(event);
-  const ticker = 'gi.ddf';
-
-  const userPreferredCurrencies = await getPreferredCurrencies(supabase);
-
-  for (const { user_id, currency } of userPreferredCurrencies) {
-    await updateUserPortfolio(user_id, currency, ticker, supabase);
-  }
-
-  return 'Values have been calculated and updated';
+  const body = await readBody(event);
+  if(body.record.quantity_today===body.old_record.quantity_today) return "No change in quantity_today"
+  const assetPrice = await messaging.getAssetPrice(supabase, body.record.value_currency, body.record.ticker)
+  const { data, error } = await supabase
+    .from('get_user_portfolio')
+    .update({"value": body.record.quantity_today/assetPrice})
+    .eq('date', body.record.date)
+    .eq('ticker', body.record.ticker)
+    .eq('user_id', body.record.user_id)
+    .select()
+  return data
 });
