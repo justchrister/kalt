e5d34b65f3184a16a39bcfedd1c7f13aba8c26b3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-13 20:44:29 +0200

    Fixed

--- server/api/getUserPortfolio/calculateValue.ts
+++ server/api/getUserPortfolio/calculateValue.ts
@@ -6,13 +6,24 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   if(body.record.value!==body.old_record.value) return "No change in quantity_today"
-
-  const assetPrice = await messaging.getAssetPrice(supabase, body.record.value_currency, body.record.ticker)
-
+  const getUserCurrency = async () => {
+    const { data, error } = await supabase
+      .from('get_user')
+      .select('currency')
+      .eq('user_id', body.record.user_id)
+      .limit(1)
+      .single();
+    return data.currency;
+  };
+  const currency = await getUserCurrency();
+  const assetPrice = await messaging.getAssetPrice(supabase, currency, body.record.ticker)
   const valueCalculated = body.record.quantity_today/assetPrice;
   const { data, error } = await supabase
     .from('get_user_portfolio')
-    .update({"value": valueCalculated})
+    .update({
+      'value': valueCalculated,
+      'value_currency': currency
+    })
     .eq('date', body.record.date)
     .eq('ticker', body.record.ticker)
     .eq('user_id', body.record.user_id)
