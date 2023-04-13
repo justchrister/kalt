5aa683c07534b3796607fe99ddab27e34fa109df
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-13 20:30:37 +0200

    Changing it slightly

--- server/api/getUserPortfolio/calculateValue.ts
+++ server/api/getUserPortfolio/calculateValue.ts
@@ -1,10 +1,14 @@
+//@ts-nocheck
 import { messaging } from '~/composables/messaging'
 import { serverSupabaseServiceRole } from '#supabase/server'
 export default defineEventHandler(async (event) => {
   const supabase = serverSupabaseServiceRole(event);
   const body = await readBody(event);
-  if(body.record.quantity_today===body.old_record.quantity_today) return "No change in quantity_today"
+
+  if(body.record.value!==body.old_record.value) return "No change in quantity_today"
+
   const assetPrice = await messaging.getAssetPrice(supabase, body.record.value_currency, body.record.ticker)
+
   const valueCalculated = body.record.quantity_today/assetPrice;
   const { data, error } = await supabase
     .from('get_user_portfolio')
