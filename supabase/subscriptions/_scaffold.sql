2775283e708fb0eb9abaf7a2805a8b83d4f6aebc
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-13 22:44:48 +0200

    kebab_case → camelCase

--- supabase/subscriptions/_scaffold.sql
+++ supabase/subscriptions/_scaffold.sql
@@ -4,31 +4,31 @@
 -- topic   <<topic_name>>
 
 --- create the table, with default values
-CREATE TABLE <<serviceName>>_<<topicName>> (
-    message_id          uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    message_entity_id   uuid        NOT NULL  DEFAULT uuid_generate_v4(),
-    message_created     timestamptz NOT NULL  DEFAULT (now() at time zone 'utc'),
-    message_sender      text        NOT NULL,
-    message_read        boolean     NOT NULL  DEFAULT FALSE
+CREATE TABLE "<<serviceName>>_<<topicName>>" (
+    "messageId"           uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "messageEntityId"     uuid        NOT NULL  DEFAULT uuid_generate_v4(),
+    "messageCreated"      timestamptz NOT NULL  DEFAULT (now() at time zone 'utc'),
+    "messageSender"       text        NOT NULL,
+    "messageRead"         boolean     NOT NULL  DEFAULT FALSE
 );
 
 --- add row level security
-ALTER TABLE <<serviceName>>_<<topicName>> ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "<<serviceName>>_<<topicName>>" ENABLE ROW LEVEL SECURITY;
 
 --- Standard descriptions
-comment on column <<serviceName>>_<<topicName>>.message_id 
+comment on column "<<serviceName>>_<<topicName>>"."messageId" 
 is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
 
-comment on column <<serviceName>>_<<topicName>>.message_entity_id 
-is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
+comment on column "<<serviceName>>_<<topicName>>"."messageEntityId" 
+is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different "messageId"s, usually a 1:1 relation to an order_id or similar';
 
-comment on column <<serviceName>>_<<topicName>>.message_created 
+comment on column "<<serviceName>>_<<topicName>>"."messageCreated" 
 is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
 
-comment on column <<serviceName>>_<<topicName>>.message_sender 
+comment on column "<<serviceName>>_<<topicName>>"."messageSender" 
 is 'where the message originates, usually set in the application.';
 
-comment on column <<serviceName>>_<<topicName>>.message_read 
+comment on column "<<serviceName>>_<<topicName>>".message_read 
 is 'the only field that is updated in this table, when the service reads it, it changes the bool to true.';
 
 
@@ -36,14 +36,14 @@ is 'the only field that is updated in this table, when the service reads it, it 
 CREATE OR REPLACE FUNCTION <<serviceName>>_<<topicName>>()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO <<serviceName>>_<<topicName>> (message_id, message_entity_id, message_sender, message_created)
-  VALUES (NEW.message_id, NEW.message_entity_id, NEW.message_sender, NEW.message_created);
+  INSERT INTO "<<serviceName>>_<<topicName>>" ("messageId", "messageEntityId", "messageSender", "messageCreated")
+  VALUES (NEW."messageId", NEW."messageEntityId", NEW."messageSender", NEW."messageCreated");
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create the trigger on the topic table and event
-CREATE TRIGGER <<serviceName>>_<<topicName>>
-AFTER INSERT ON <<topic_name>>
+CREATE TRIGGER "<<serviceName>>_<<topicName>>"
+AFTER INSERT ON "<<topic_name>>"
 FOR EACH ROW
 EXECUTE FUNCTION <<serviceName>>_<<topicName>>();
