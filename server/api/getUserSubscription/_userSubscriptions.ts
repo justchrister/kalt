e20ef1bed622431b878dd52fe3159905376e8549
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-14 00:05:14 +0200

    Adding endpoint for user subscription topic

---
+++ server/api/getUserSubscription/_userSubscriptions.ts
@@ -0,0 +1,31 @@
+import { ok } from '~/composables/ok';
+import { messaging } from '~/composables/messaging';
+import { serverSupabaseServiceRole } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+  const supabase = serverSupabaseServiceRole(event);
+  const service = 'getUserSubscription';
+  const serviceKebab = ok.camelToKebab(service);
+  const topic = 'userSubscriptions';
+  const body = await readBody(event);
+  
+  if (body.record.message_read) return 'message already read';
+
+  const message = await messaging.getEntity(
+    supabase,
+    topic,
+    body.record.message_entity_id
+  );
+
+  await messaging.read(supabase, topic, service, body.record.message_id);
+
+  const cleanMessage = await messaging.cleanMessage(message);
+
+  const { data, error } = await supabase
+    .from(serviceKebab)
+    .upsert(cleanMessage)
+    .select();
+  
+  if(data) return data;
+  if(error) return error;
+});
