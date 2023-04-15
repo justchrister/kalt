6efb8b6ebd9ba47facf180c505248b992ec4892f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-15 16:09:46 +0200

    Adding a wrapper and some user feedback

--- components/card/add.vue
+++ components/card/add.vue
@@ -1,32 +1,35 @@
 <template>
   <div>
     <form @submit.prevent="addCard">
-      <div :class="checkBrand(card_number)"></div>
-      <input 
-        type="text" 
-        v-model="card_number" 
-        minlength="19"
-        v-maska data-maska="#### #### #### ####"
-        placeholder="•••• •••• •••• 4242"/>
-      <input 
-        type="text" 
-        v-model="expiry_month" 
-        v-maska data-maska="##"
-        placeholder="12" />
-      <input 
-        type="text" 
-        v-model="expiry_year" 
-        v-maska data-maska="##"
-        placeholder="24" />
-      <input 
-        type="text" 
-        v-model="cvc" 
-        v-maska data-maska="###"
-        placeholder="333" />
+      <div class="card">
+        <div :class="checkBrand(card_number)"></div>
+        <input 
+          type="text" 
+          v-model="card_number" 
+          minlength="19"
+          v-maska data-maska="#### #### #### ####"
+          placeholder="•••• •••• •••• 4242"/>
+        <input 
+          type="text" 
+          v-model="expiry_month" 
+          v-maska data-maska="##"
+          placeholder="12" />
+        <input 
+          type="text" 
+          v-model="expiry_year" 
+          v-maska data-maska="##"
+          placeholder="24" />
+        <input 
+          type="text" 
+          v-model="cvc" 
+          v-maska data-maska="###"
+          placeholder="333" />
+        </div>
+      <button>
+        add card <loading-icon v-if="loading" />
+      </button>
     </form>
-    <button @click="addCard()">
-      add card
-    </button>
+    <notification-fixed :type="notification.type" :message="notification.message" v-if="notification.message"/>
   </div>
 </template>
 <script setup lang="ts">
@@ -36,17 +39,41 @@
   const expiry_month = ref();
   const expiry_year = ref();
   const cvc = ref('');
+  const loading = ref(false);
+  const notification = ref({
+    type: null, 
+    message: null
+  });
 
   const addCard = async () => {
     const last_four_digits = card_number.value.slice(-4)
-    console.log(ok.toInt(card_number.value))
+    const cardNumberInt = ok.toInt(card_number.value);
+    loading.value = true;
+
+    if(16>cardNumberInt.toString().length){
+      notification.value={
+        type: 'error',
+        message: 'Payment card is too short'
+      };
+      ok.log('error', 'card number is too short');
+      return
+    }
+
+    if(12<expiry_month.value){
+      notification.value={
+        type: 'error',
+        message: 'Expiry month is set to '+expiry_month.value+' and it should be between 1 and 12 😅'
+      }
+      ok.log('warn', 'Expiry month is set to ' + expiry_month.value + ' and it should be between 1 and 12 😅')
+      return
+    }
     const { data, error } = await supabase
       .from('payment_cards')
       .insert({
         'message_sender': 'component/card/add.vue',
         'user_id': user.value.id,
         'last_four_digits': last_four_digits,
-        'card_number': ok.toInt(card_number.value),
+        'card_number': cardNumberInt,
         'expiry_month': expiry_month.value,
         'expiry_year': expiry_year.value,
         'cvc': cvc.value,
@@ -68,7 +95,7 @@
   }
 </script>
 <style scoped lang="scss">
-form{
+.card{
   display: grid; 
   grid-template-columns: 
     $clamp-2 
