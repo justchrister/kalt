199755c3ac4c680b92498213e10fbf928048b9b1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-15 13:42:07 +0200

    Inline loading

---
+++ components/loadingIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <span></span>
+</template>
+<style scoped lang="scss">
+  span{
+    background-image: url('/loading.gif');
+    background-size:contain;
+    background-repeat:no-repeat;
+    width:$clamp;
+    height:$clamp;
+    display:inline-block;
+  }
+</style>
