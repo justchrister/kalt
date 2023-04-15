d35fe0dce0d7c3c4fa81609f05d29e151038d301
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-16 01:42:08 +0200

    Update focused.vue

--- layouts/focused.vue
+++ layouts/focused.vue
@@ -15,7 +15,7 @@
 </script>
 <style scoped lang="scss">
 .layout.focused .back-button{
-  padding: $big-clamp-half 0 0 $big-clamp-half;
+  padding: $clamp-2 0 0 $clamp-2;
   font-family: $sans-serif;
   font-size:$clamp-2;
   font-weight:300;
