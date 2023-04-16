8b21eb65a6e12b99b0b75253cd5ffe97a48d6f84
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-16 20:06:11 +0200

    Create _linkedBankAccounts.ts

---
+++ server/api/getLinkedBankAccounts/_linkedBankAccounts.ts
@@ -0,0 +1,23 @@
+import { ok } from '~/composables/ok'
+import { messaging } from '~/composables/messaging'
+import { serverSupabaseServiceRole } from '#supabase/server'
+
+export default defineEventHandler( async (event) => {
+  const supabase = serverSupabaseServiceRole(event);
+  const service = 'getLinkedBankAccounts';
+  const topic = 'linkedBankAccounts';
+  const serviceKebab = ok.camelToKebab(service);
+  const topicKebab = ok.camelToKebab(topic);
+  const query = getQuery(event);
+  const body = await readBody(event);
+  if(body.record.message_read) return 'message already read';
+
+  const message = await messaging.getEntity(supabase, topic, body.record.message_entity_id);
+  await messaging.read( supabase, topic, service, body.record.message_id)
+  const cleanedMessage = await messaging.cleanMessage(message)
+
+  const { data, error } = await supabase
+    .from(serviceKebab)
+    .upsert(cleanedMessage)
+
+});
