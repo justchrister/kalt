f44781a994e5b039a7af3d8bc30cb1b851d464b6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-16 16:02:22 +0200

    Update index.vue

--- pages/accounts/index.vue
+++ pages/accounts/index.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
-      
+    <intro title="Accounts" text="Make money, make a difference." />
+    <block margin="half">
+      <account-details-card />
+      <info-box type="success" text="With automatic investment set to 100% and automatic transfers to Kalt, you can set up a low-cost alternative to subscriptions ✨" />
+    </block>
+    <block>
+      <linked-bank-account-card />
+    </block>
+
+    <transaction-list limit="10"/>
   </main>
 </template>
 <script lang="ts" setup>
@@ -18,5 +27,5 @@
   
 </script>
 <style scoped lang="scss">
-  
+  main{padding-top:0;}
 </style>
