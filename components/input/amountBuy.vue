8e9b02c76595455d95984deb7aedc46152de0569
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-16 14:08:59 +0200

    + buy flow

---
+++ components/input/amountBuy.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="input-wrap">
+    <label for="amount"> 
+      Select amount: 
+    </label>
+    <div class="input-group">
+      <input
+        v-maska:[options]
+        data-maska="0.99"
+        data-maska-tokens="0:\d:multiple|9:\d:optional"
+        type="text"
+        placeholder="Amount"
+        id="amount"
+        class="amount"
+        v-model="amount"
+        @input="updatePaymentAmount"
+      />
+      <div class="currency">{{ currency }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const state = ref('loading')
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+  const props = defineProps({
+    uuid: {
+      type: String,
+      required: true
+    }
+  })
+  const getPreferredCurrency = async () => {
+    const { data, error } = await supabase
+      .from('get_user')
+      .select('currency')
+      .eq('user_id', user.value.id)
+      .limit(1)
+      .single()
+    return data.currency
+  }
+
+  const currency = await getPreferredCurrency();
+
+  const updatePaymentAmount = async () => { 
+    if(amount.value){
+      const { error } = await supabase
+        .from('account_transactions')
+        .insert({
+          message_entity_id: props.uuid,
+          message_sender: 'components/input/amountBuy.vue',
+          user_id: user.value.id,
+          amount: ok.toInt(amount.value),
+          currency: currency,
+          transaction_type: 'deposit',
+          transaction_sub_type: 'card',
+          transaction_status: 'incomplete',
+          auto_invest: 1,
+      })
+      if(error) ok.log('error', 'could not update amount', error)
+      if(!error) ok.log('success', 'updated amount')
+    }
+  }
+
+  const options = {
+    preProcess: val => val.replace(/[$,]/g, ''),
+    postProcess: val => {
+      if (!val) return ''
+      const sub = 3 - (val.includes('.') ? val.length - val.indexOf('.') : 0)
+      return Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: currency
+      }).format(val)
+        .slice(0, sub ? -sub : undefined)
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .input-group{
+    border:$border;
+    display: grid;
+    grid-template-rows: 1fr;
+    gap: 0% 0%;
+    grid-auto-flow: row;
+    grid-template-columns: 6fr 1fr;
+  }
+  .currency{
+    height:$clamp-4;
+    line-height:$clamp-4;
+    text-align:center;
+  }
+  input{
+    border:none;
+    border-right:$border;
+  }
+</style>
--- pages/portfolio/buy.vue
+++ pages/portfolio/buy.vue
@@ -4,14 +4,15 @@
     <block>
       <h3>Let's invest!  <omoji emoji="☀️" /></h3>
       <form @submit.prevent="completeTransaction">
-        <input-amount-invest :uuid="uuid"/>
-        <default-card />
-        <button @click="completeTransaction"> buy </button>
+        <input-amount-buy :uuid="uuid"/>
+        <card-default />
+        <button> invest <loading-icon v-if="loading" /> </button>
       </form>
     </block>
   </main>
 </template>
 <script setup lang="ts">
+  const loading = ref(false)
   definePageMeta({
     pagename: 'Invest',
     layout: 'focused',
@@ -29,14 +30,24 @@
   const uuid = ok.uuid();
 
   const completeTransaction = async () => {
+    loading.value = true
     const { data, error } = await supabase
       .from('account_transactions')
       .insert({
-        'message_entity_id': uuid,
-        'user_id': user.value.id,
-        'transaction_type': 0
+          message_entity_id: uuid,
+          message_sender: 'pages/portfolio/buy.vue',
+          user_id: user.value.id,
+          transaction_sub_type: 'card',
+          transaction_status: 'payment_awaiting',
+          auto_invest: 1
       })
-
+    if(error){
+      ok.log('error', 'could not create transaction', error)
+      loading.value = false
+    } else {
+      ok.log('success', 'transaction created')
+      loading.value = false;
+    };
   }
 
 </script>
