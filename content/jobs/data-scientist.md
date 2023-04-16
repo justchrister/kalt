79da3c3280af80a921e2d6d6f8ded197d6691431
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-16 13:10:01 +0200

    Initial jobs

---
+++ content/jobs/data-scientist.md
@@ -0,0 +1,2 @@
+
+# Data scientist
---
+++ pages/jobs/[...slug].vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="candidate">
+    <block>
+      <ContentDoc>
+        <template #not-found>
+          <not-found />
+        </template>
+      </ContentDoc>
+    </block>
+  </main>
+</template>
+<script setup lang="ts">
+  definePageMeta({
+    pagename: 'Candidate'
+  })
+  useHead({
+    title: 'Candidate',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+</script>
+<style lang="scss">
+main.notification h1{
+  font-family:$sans-serif;
+}
+</style>
