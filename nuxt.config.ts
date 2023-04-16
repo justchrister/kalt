5e59efd1868ffb47466690af286d03be4ad65f80
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-16 20:10:30 +0200

    -preloads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,10 +31,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: "image/png", sizes: '32x32', href: '/icon/icon-32.png' } ,
         { rel: 'icon', type: "image/png", sizes: '180x180', href: '/icon/icon-180.png' } ,
         { rel: 'icon', type: "image/svg+xml", href: '/icon/icon.svg' } ,
-        { rel: 'manifest', type: "image/x-icon", href: '/manifest.webmanifest' },
-        { rel: 'preload', as: 'font', href: "/fonts/display.woff2", crossorigin: true},
-        { rel: 'preload', as: 'font', href: "/fonts/body.woff2", crossorigin: true },
-        { rel: 'preload', as: 'font', href: "/fonts/body-bold.woff2", crossorigin: true}
+        { rel: 'manifest', type: "image/x-icon", href: '/manifest.webmanifest' }
       ],
       meta: [
         { name: 'description', content: 'Make money, make a difference!' },
