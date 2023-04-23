c4d59dfc229245803bc27365dbba6b4a8c468433
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-23 20:15:40 +0200

    Adding color scheme

---
+++ components/select/colorScheme.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li class="dark">
+      
+    </li>
+    <li class="light">
+      
+    </li>
+    <li class="crazy">
+      
+    </li>
+  </ul>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+  ul{
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap:$clamp;
+  }
+  li{
+    border:$border;
+    display:block;
+    height:$clamp-7;
+    background-size: cover;
+    background-position: center top;
+    background-repeat: no-repeat;
+  }
+  .dark{
+    background-image: url('/darkmode.svg');
+  }
+  .light{
+    background-image: url('/lightmode.svg');
+  }
+  .crazy{
+    background-image: url('/FsoMtuBXsAI_Bx7.jpeg');
+    background-position: center;
+  }
+</style>
