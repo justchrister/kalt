1e099e9325e0059ead55eeda9b0bdf4b4d2f5e97
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-26 00:04:28 +0200

    idk

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <block>
+      <img src="/error.jpg" alt="" style="max-width:100%"/>
+      <br/>
+      <br/>
+      <h1 class="sans-serif">Requested page did not exist</h1>
+      <nuxt-link to="/">Go to homepage</nuxt-link>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'error',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'error',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
