0062f2dc53114f370d2af7724d58f34ffde5bb00
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-04-30 13:49:28 +0200

    Create back.vue

---
+++ components/navbar/back.vue
@@ -0,0 +1,17 @@
+<template>
+  <span @click="navigateTo(to)"><omoji emoji="←"/> back</span>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    to: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+  span:hover{
+    cursor:pointer;
+    text-decoration: underline;
+  }
+</style>
