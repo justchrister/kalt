bab98a886cf8e9d0cde6d9953d0fe00eb071369e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-05-01 21:12:50 +0200

    Moved toggle + fixed naming

---
+++ components/toggle/subscription.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="input-wrap">
+    <toggle text="Subscription enabled" :on="isOn" @click="toggleSubscription()"/>
+  </div>
+</template>
+<script setup>
+  const supabase = useSupabaseClient()
+  const user = useSupabaseUser()
+  const isOn = ref()
+  const { data, error } = await supabase
+    .from('get_user_subscription')
+    .select()
+    .limit(1)
+    .single()
+  if(data) isOn.value = data.active;
+  ok.log('success', 'Got user preference: ', data.terms_of_service)
+  const toggleValue = async () => {
+    if(isOn.value) return false
+    else return true
+  }
+  const toggleSubscription = async () => {
+    const toggledValue = await toggleValue()
+    isOn.value = toggledValue
+    message.post(supabase, 'user_subscriptions',{
+      "messageSender": "components/toggle/subscription.vue",
+      "userId": user.value.id,
+      "active": isOn.value
+    })
+  }
+</script>
--- pages/subscription/index.vue
+++ pages/subscription/index.vue
@@ -2,12 +2,15 @@
 <template>
   <main>
     <navbar-tabs />
+    <block margin="none">
+      <info-box type="success" text="With automatic investment set to 100% and automatic transfers to Kalt, you can set up a low-cost alternative to subscriptions ✨" />
+    </block>
     <block margin="half">
-      <input-amount-subscription :amount="data.amount"/>
+      <input-amount-subscription :amount="subscription.amount"/>
     </block>
     <block margin="half">
       <label>Select which days you want to auto-invest</label>
-      <calendar-subscription :days="data.days_of_month"/>
+      <calendar-subscription :days="subscription.days_of_month"/>
     </block>
     <block margin="half">
       <label>Select card you want to withdraw:</label>
@@ -15,7 +18,7 @@
       <info-box type="info" text="changing default card will change card being charged" />
     </block>
     <block margin="half">
-      <toggle text="Subscription enabled " @click="toggleSubscription()" :on="enabled"/>
+      <toggle-subscription />
     </block>
   </main>
 </template>
@@ -29,25 +32,23 @@
   useHead({
     title: 'Subscription'
   })
-
-  const enabled = ref(false)
-
-  const { data, error } = await supabase
-    .from('get_user_subscription')
-    .select()
-    .eq('user_id', user.value.id)
-    .limit(1)
-    .single()
-  const toggleSubscription = async () => {
+  const getUserSubscription = async () => {
+    
     const { data, error } = await supabase
-      .from('user_subscriptions')
-      .update({
-        message_sender: 'pages/subscription/index.vue',
-        enabled: false
-      })
+      .from('get_user_subscription')
       .select()
+      .eq('user_id', user.value.id)
+      .limit(1)
       .single()
-    enabled.value=data.enabled
+    if(data) {
+      ok.log('success', 'Got user subscription: ', data)
+      return data
+    }
+    if(error) {
+      ok.log('error', 'Failed to get user subscription: ', error)
+      return error
+    }
   }
+  const subscription = await getUserSubscription();
 
 </script>
