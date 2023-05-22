a11b8dbaf0a736193a3d12c4836d932dfa82cabb
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-05-22 17:31:32 +0200

    Update video.vue

--- components/frame/video.vue
+++ components/frame/video.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-  <video autoplay muted loop>
+  <video autoplay muted loop playsinline>
     <source :src="src" type="video/mp4">
     Your browser does not support the video tag.
   </video>
