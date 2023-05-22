70655ab4898aa24cd522854c3aef47fbeee7f23b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-05-22 17:54:30 +0200

    news

---
+++ content/news/feedback.md
@@ -0,0 +1,1 @@
+# We are looking for feedback
---
+++ pages/news/[...slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="news-article">
+    <block>
+      <ContentDoc>
+        <template #not-found>
+          <not-found />
+        </template>
+      </ContentDoc>
+    </block>
+  </main>
+</template>
+<script setup>
+  definePageMeta({
+    pagename: 'News'
+  })
+  useHead({
+    title: 'News'
+  })
+</script>
+<style lang="scss">
+
+</style>
