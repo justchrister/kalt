467cfc3806ca81ba576ec365bcf6ec61cc5a8e32
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-05-28 20:58:50 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -5,4 +5,8 @@
 </template>
 <script setup>
   console.log("✨ https://ka.lt/jobs ✨")
+
+  const colorMode = useColorMode()
+  const getUser = useCookie('user');
+  if(getUser.value.color_scheme) colorMode.preference = getUser.value.color_scheme;
 </script>
