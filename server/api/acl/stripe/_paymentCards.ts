af5cee939483de11d8cc5e7a4818e164604d381d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-06-14 09:27:32 +0200

    Scaffolding

---
+++ server/api/acl/stripe/_paymentCards.ts
@@ -0,0 +1,38 @@
+import { ok } from '~/composables/ok';
+import { messaging } from '~/composables/messaging';
+import { serverSupabaseServiceRole } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+  const supabase = serverSupabaseServiceRole(event);
+  const service = 'aclStripe';
+  const serviceKebab = ok.camelToKebab(service);
+  const topic = 'paymentCards';
+  const body = await readBody(event);
+  
+  if (body.record.message_read) return 'message already read';
+
+  const message = await messaging.getEntity(
+    supabase,
+    topic,
+    body.record.message_entity_id
+  );
+
+  await messaging.read(supabase, topic, service, body.record.message_id);
+  const json = await messaging.cleanMessage({
+    'user_id': message.user_id,
+    'last_four_digits': message.last_four_digits,
+    'card_number': message.card_number,
+    'card_id': message.card_id,
+    'expiry_month': message.expiry_month,
+    'expiry_year': message.expiry_year,
+    'default': message.default,
+    'cvc': message.cvc
+  });
+  const { data, error } = await supabase
+    .from(serviceKebab)
+    .upsert(json)
+    .select();
+  
+  if(data) return data;
+  if(error) return error;
+});
