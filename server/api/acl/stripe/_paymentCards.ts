b38fa6e0e9a4806a40a18bc6475fc843d20da0d2
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-01 17:26:09 +0200

    + cards work, attaching does not

--- server/api/acl/stripe/_paymentCards.ts
+++ server/api/acl/stripe/_paymentCards.ts
@@ -2,14 +2,12 @@ import { ok } from '~/composables/ok';
 import { messaging } from '~/composables/messaging';
 import { serverSupabaseServiceRole } from '#supabase/server';
 import Stripe from 'stripe';
-
 export default defineEventHandler(async (event) => {
   const supabase = serverSupabaseServiceRole(event);
   const service = 'getPaymentCards';
   const topic = 'paymentCards';
   const body = await readBody(event);
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // your stripe key here
-  
   if (body.record.message_read) return 'message already read';
 
   const message = await messaging.getEntity(
@@ -17,7 +15,6 @@ export default defineEventHandler(async (event) => {
     topic,
     body.record.message_entity_id
   );
-
   await messaging.read(supabase, topic, service, body.record.message_id);
   const json = await messaging.cleanMessage({
     'user_id': message.user_id,
@@ -26,9 +23,9 @@ export default defineEventHandler(async (event) => {
     'card_id': message.card_id,
     'expiry_month': message.expiry_month,
     'expiry_year': message.expiry_year,
-    'default': message.default,
     'cvc': message.cvc
   });
+  ok.log('','card details', json);
   // do api call to Stripe
   const addCard = async () => {
     
@@ -40,10 +37,23 @@ export default defineEventHandler(async (event) => {
         exp_year: json.expiry_year,
         cvc: json.cvc
       },
+      metadata: {
+        card_id: json.card_id  // your unique card id here
+      }
     });
     return paymentMethod;
   }
-
-  
-
+  const attachCard = async (customerId, cardId) => {
+    const card = await stripe.paymentMethods.attach(
+      cardId,
+      {
+        customer: customerId
+      }
+    );
+    ok.log('success', 'attached card', card);
+    return card
+  }  
+  const createdCard = await addCard();
+  const attachedCard = await attachCard(createdCustomer.id, createdCard.id);
+  return attachedCard;
 });
