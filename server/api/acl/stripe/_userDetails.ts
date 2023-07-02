331881147a6a324ce65c811276d1566b877f9231
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-02 17:56:14 +0200

    Update _userDetails.ts

--- server/api/acl/stripe/_userDetails.ts
+++ server/api/acl/stripe/_userDetails.ts
@@ -5,6 +5,7 @@ import Stripe from 'stripe';
 export default defineEventHandler(async (event) => {
   const supabase = serverSupabaseServiceRole(event);
   const topic = 'userDetails';
+  const service = 'aclStripe';
   const body = await readBody(event);
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // your stripe key here
   if (body.record.message_read) return 'message already read';
@@ -15,15 +16,32 @@ export default defineEventHandler(async (event) => {
     body.record.message_entity_id
   );
   await messaging.read(supabase, topic, service, body.record.message_id);  
+
+    
+  const checkIfUserExists = async () => {
+    const { data, error } = await supabase
+      .from('acl_stripe_user_ids')
+      .select()
+      .eq('user_id', message.user_id)
+      .limit(1)
+      .single()
+    if(data) return true
+    else return false
+  }
+  const userExists = await checkIfUserExists();
+  if(userExists) return "user already exists"
   ok.log('','user id: '+message.user_id);
 
   const createUser = async () => {
     const user = await stripe.customers.create({
       email: message.email,
-      name: message.first_name+' '+message.last_name
+      name: message.first_name+' '+message.last_name,
+      metadata: { 
+        user_id: message.user_id
+      }
     });
+    ok.log('succes', 'created user:', user)
     if (user) return user;
-    ok.log('succes', 'created user: '+user)
   };
   const assignStripeId = async (userId, stripeUserId) => {
     const { data, error } = await supabase
@@ -36,10 +54,7 @@ export default defineEventHandler(async (event) => {
     ok.log('success', 'assigned stripe id: '+data)
     return data
   }
-  // do api call to Stripe
   const createdUser = await createUser();
-
-  // post new message containing user id in stripe
-  // add to a table that is user ids (acl_stripe_user_ids)
   if (createdUser) await assignStripeId(message.user_id, createdUser.id);
+  return "successfully assigned internal user_id with stripe user_id"
 });
