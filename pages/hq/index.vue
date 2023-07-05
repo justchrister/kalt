ed24a782f81881dfc47661614f022c3c85b8aa43
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-05 19:54:26 +0200

    Create index.vue

---
+++ pages/hq/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+      <block>
+        <nuxt-link to="hq/sell-order">
+          Create sell order
+        </nuxt-link>
+      </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'HQ',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'HQ',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
