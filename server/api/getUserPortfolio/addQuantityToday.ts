fe62abc39f064fd3c98ed08a95091903d06a1477
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-05 18:41:14 +0200

    Update addQuantityToday.ts

--- server/api/getUserPortfolio/addQuantityToday.ts
+++ server/api/getUserPortfolio/addQuantityToday.ts
@@ -5,6 +5,14 @@ export default defineEventHandler(async (event) => {
   const supabase = serverSupabaseServiceRole(event)
   const body = await readBody(event)
 
+  const getUsers = async () => {
+    const { data, error } = await supabase
+      .from('get_user')
+      .select('user_id')
+    if(data) ok.log('success', 'got all users: ', data)
+    if(error) ok.log('error', 'could not get all users: ', error)
+    return data
+  };
   const getDays = async (user) => {
     const { data, error } = await supabase
       .from('get_user_portfolio')
@@ -13,22 +21,32 @@ export default defineEventHandler(async (event) => {
       .order('date', { ascending: true })
     if (data) return data
   };
-  let quantity_today = 0;
-  const days = await getDays(body.record.user_id);
-  for (let i = 0; i < days.length; i++) {
-    const current = days[i];
-    quantity_today += current.quantity_change
-    const { data, error } = await supabase
-      .from('get_user_portfolio')
-      .update({
-        "quantity_today": quantity_today
-      })
-      .eq('user_id', current.user_id)
-      .eq('date', current.date)
-      .eq('ticker', current.ticker)
-      .select();
-      if (data) ok.log('success', 'updated date', data)
-      if (error) ok.log('error', 'updated date', error)
+  const addValueToday = async (days) => {
+    let quantity_today = 0;
+    for (let i = 0; i < days.length; i++) {
+      const current = days[i];
+      quantity_today += current.quantity_change
+      const { data, error } = await supabase
+        .from('get_user_portfolio')
+        .update({ "quantity_today": quantity_today })
+        .eq('user_id', current.user_id)
+        .eq('date', current.date)
+        .eq('ticker', current.ticker)
+        .select();
+        if (data) ok.log('success', 'updated date', data)
+        if (error) ok.log('error', 'updated date', error)
+    }
+  }
+  if(body){
+    const days = await getDays(body.record.user_id);
+    await addValueToday(days);
+  }
+  if(!body){
+    const users = await getUsers();
+    for (let i = 0; i < users.length; i++) {
+      const days = await getDays(users[i].user_id);
+      await addValueToday(days);
+    }
   }
   return 'processed'
 });
