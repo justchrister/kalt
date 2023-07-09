c95464a99dfb213c51397279a692ef19d15cdc23
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-09 03:07:39 +0200

    untested, probably works

---
+++ server/api/payRevenue/_exchangeOrders.ts
@@ -0,0 +1,48 @@
+import { ok } from '~/composables/ok'
+import { messaging } from '~/composables/messaging'
+import { serverSupabaseServiceRole } from '#supabase/server'
+
+export default defineEventHandler( async (event) => {
+  const supabase = serverSupabaseServiceRole(event);
+  const query = getQuery(event);
+  const topic = 'exchangeOrders';
+  const service = 'getUserPortfolio';
+  const body = await readBody(event);
+  if(body.record.message_read) return 'message already read';
+  
+  const message = await messaging.getEntity(supabase, topic, body.record.message_entity_id);
+
+  const readMessage = await messaging.read(supabase, topic, service, body.record.message_id);
+  const getPortfolio = async () => {
+    const { data, error } = await supabase
+      .from('exchange_orders')
+      .select()
+      .eq('user_id', message.user_id)
+      .eq('order_status', 'fulfilled')
+    return data
+  }
+  const sumShares = async (portfolio) => {
+    let totalShares = 0;
+    for (let i = 0; i < portfolio.length; i++) {
+      const order = portfolio[i];
+      totalShares += order.quantity;
+    };
+    return totalShares
+  }
+
+  const updateShares = async (shares) => {
+    const { data, error } = await supabase
+      .from('pay_revenue')
+      .insert({
+        user_id: message.user_id,
+        quantity: shares,
+      })
+      .select()
+    return data
+  }
+  const portfolio = await getPortfolio();
+  const totalShares = await sumShares(portfolio);
+  const updatedShares = await updateShares(totalShares);
+  
+  return updatedShares;
+});
