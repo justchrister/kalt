ffce48429f2b5d7aea917e0224bd3a3bd4798c50
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-10 20:36:36 +0200

    Create _accountTransactions.ts

---
+++ server/api/payments/_accountTransactions.ts
@@ -0,0 +1,81 @@
+import { ok } from '~/composables/ok'
+import { messaging } from '~/composables/messaging'
+import { serverSupabaseServiceRole } from '#supabase/server'
+
+export default defineEventHandler( async (event) => {
+  const supabase = serverSupabaseServiceRole(event);
+  const service = 'payments';
+  const topic = 'accountTransactions';
+  const serviceKebab = ok.camelToKebab(service);
+  const topicKebab = ok.camelToKebab(topic);
+  const query = getQuery(event);
+  const body = await readBody(event);
+  if(body.record.message_read) return 'message already read';
+
+  const message = await messaging.getEntity(supabase, topic, body.record.message_entity_id);
+  await messaging.read( supabase, topic, service, body.record.message_id)
+  if(message.transaction_type != 'deposit') return 'wrong transaction type'
+  if(message.transaction_sub_type != 'card') return 'wrong transaction sub type'
+  
+  const deletePayment = async () => {
+    const { error } = await supabase
+      .from('payments_pending')
+      .delete()
+      .eq('message_id', message.transaction_id)
+    if(error) return false
+    else return true;
+  }
+  const updateTransaction = async () => {
+    let json = message;
+    json.transaction_status = 'payment_awaiting';
+    json.message_id = ok.uuid();
+    json.message_sender = 'server/api/payments/_accountTransactions.ts';
+    const { data, error } = await supabase
+      .from('account_transactions')
+      .insert(json)
+      .select()
+    return data
+  }
+  const paymentExists = async (id) => {
+    const { data, error } = await supabase
+      .from('payments')
+      .select()
+      .eq('transaction_id', id)
+      .limit(1)
+      .single()
+    if(error) {
+      ok.log('success', 'could not find payment in table')
+      return false
+    }
+    if(data){
+      ok.log('success', 'found payment in table ', data)
+      return true
+    }
+  }
+  const existingPayment = await paymentExists(message.message_entity_id);
+  if(existingPayment) return 'payment already exists'
+
+  if(message.transaction_status='payment_awaiting'){
+    const { data, error } = await supabase
+      .from('payments')
+      .insert({
+        user_id: message.user_id,
+        amount: message.amount,
+        currency: message.currency,
+        transaction_id: message.message_entity_id
+      })
+      .select()
+    if(error) return error;
+    if(data) {
+      const updatedAccountTransaction = await updateTransaction();
+      return data
+    };
+  }
+  
+  if(message.transaction_status='payment_declined' || 'payment_approved' ) {
+    const deletedPayment = await deletePayment(message.message_entity_id);
+    if(deletedPayment===true) return 'payment'+message.message_entity_id+' deleted'
+    if(deletedPayment===false) return 'payment'+message.message_entity_id+' did not exist, it might have been deleted previously'
+  }
+  return 'ggg'
+});
