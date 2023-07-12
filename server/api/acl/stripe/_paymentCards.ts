a7ad8782b2bc122c6f22bf5a03422356eea713fd
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-12 21:34:00 +0200

    + default thing

--- server/api/acl/stripe/_paymentCards.ts
+++ server/api/acl/stripe/_paymentCards.ts
@@ -10,12 +10,9 @@ export default defineEventHandler(async (event) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // your stripe key here
   if (body.record.message_read) return 'message already read';
 
-  const message = await messaging.getEntity(
-    supabase,
-    topic,
-    body.record.message_entity_id
-  );
+  const message = await messaging.getEntity(supabase, topic, body.record.message_entity_id);
   await messaging.read(supabase, topic, service, body.record.message_id);
+
   const json = await messaging.cleanMessage({
     'user_id': message.user_id,
     'last_four_digits': message.last_four_digits,
@@ -25,8 +22,7 @@ export default defineEventHandler(async (event) => {
     'expiry_year': message.expiry_year,
     'cvc': message.cvc
   });
-  
-    
+
   const checkIfUserExists = async () => {
     const { data, error } = await supabase
       .from('acl_stripe_user_ids')
@@ -63,10 +59,40 @@ export default defineEventHandler(async (event) => {
     ok.log('success', 'attached card', card);
     return card
   }
+  const makeCardDefault = async (customerId, cardId) => {
+    const customer = await stripe.customers.update(
+      customerId,
+      {invoice_settings: {default_payment_method: cardId}}
+    );
+    ok.log('success', 'updated customer', customer);
+    return customer
+  }
+  const checkIfCardExists = async () => {
+    const { data, error } = await supabase
+      .from('acl_stripe_card_ids')
+      .select()
+      .eq('card_id', message.card_id)
+      .limit(1)
+      .single()
+    if(data) return data.stripe_card_id
+    else return false
+  }
+
   const userExists = await checkIfUserExists();
+  const cardExists = await checkIfCardExists();
+
+  if(!userExists) return 'user does not exist';
+  if(userExists && cardExists && message.default){
+    const defaultCard = await makeCardDefault(userExists.stripe_user_id, cardExists.stripe_card_id);
+    return defaultCard;
+  }
   if(userExists){
     const createdCard = await addCard();
     const attachedCard = await attachCard(userExists.stripe_user_id, createdCard.id);
+    if(message.default) {
+      const defaultCard = await makeCardDefault(userExists.stripe_user_id, createdCard.id);
+    }
     return attachedCard;
   }
-});
+  return 'idk this if shit is getting complicated. Needs to be refactored';
+}); 
