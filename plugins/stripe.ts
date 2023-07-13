0bb3494572dc1b7c109da5e1b98042a4075153eb
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-13 12:18:28 +0200

    Delete stripe.ts

--- plugins/stripe.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js/pure'
-
-export default defineNuxtPlugin(nuxtApp => {
-  // now available on `nuxtApp.$injected`
-  const lol = async () => {
-  return 'works!'
-  }
-  nuxtApp.provide('stripe', () => lol)
-})
