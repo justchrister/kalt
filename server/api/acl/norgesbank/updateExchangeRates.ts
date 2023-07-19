3524a95d85f77a2db95e9e0b090f9095a0e28d41
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-19 15:44:01 +0200

    Create updateExchangeRates.ts

---
+++ server/api/acl/norgesbank/updateExchangeRates.ts
@@ -0,0 +1,84 @@
+import { ok } from '~/composables/ok'
+import { serverSupabaseServiceRole } from '#supabase/server'
+
+export default defineEventHandler( async (event) => {
+  const supabase = serverSupabaseServiceRole(event)
+  
+  const getCurrencies = async () => {
+    let currencies = []
+    const { data, error } = await supabase
+      .from('currencies')
+      .select()
+      .eq('enabled', true)
+    if(error) {
+      ok.log('error', 'could not get currencies', error.message )
+    }
+    if(data) {
+      for (let i = 0; i < data.length; i++) {
+        const currency = data[i];
+        currencies.push(currency.iso)
+      }
+    }
+
+    ok.log('', 'got the currencies', data)
+    return currencies
+  }
+
+  const createCurrencyPairs = (currencies) => {
+    let pairs = [];
+  
+    for (let i = 0; i < currencies.length; i++) {
+      for (let j = 0; j < currencies.length; j++) {
+        pairs.push({
+          from: currencies[i], 
+          to: currencies[j], 
+          value: null
+        });
+      }
+    }
+    ok.log('', 'made them into pairs ', pairs)
+  
+    return pairs;
+  }
+  const getRate = async (iso) => {
+    if(iso=='NOK') return 1;
+    const { data, error} = await ok.fetch('get', 'https://data.norges-bank.no/api/data/EXR/B.'+iso+'.NOK.SP?format=sdmx-json&startPeriod=2022-07-19&endPeriod=2023-07-19&locale=no')
+    if(error) {
+      ok.log('error', 'could not get rate for '+iso, error.message)
+      return null
+    }
+    if(data){
+      return data.data.dataSets[0].series["0:0:0:0"].observations["0"][0]
+    }
+  }
+  const updateRate = async (pair) => {
+    const fromRate = await getRate(pair.from);
+    const toRate = await getRate(pair.to);
+    const json = {
+      'from': pair.from,
+      'to': pair.to,
+      'value': fromRate/toRate
+    };
+    if(pair.to==pair.from) return;
+    if(!fromRate) return;
+    if(!toRate) return;
+    const { data, error } = await supabase
+      .from('exchange_rates')
+      .upsert(json)
+      .select()
+    if(data){
+      ok.log('', 'data: ', data)
+    }
+    if(error){
+      ok.log('', 'error: ', error)
+    }
+  }
+  const currencies = await getCurrencies()
+  const currencyPairs = await createCurrencyPairs(currencies)
+  for (let i = 0; i < currencyPairs.length; i++) {
+    ok.log('', 'im here 1')
+    const pair = currencyPairs[i];
+    await updateRate(pair)
+  }
+  return currencyPairs
+});
