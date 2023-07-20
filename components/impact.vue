7e720f4f4565862cdbb542e66486de57f9ed9be6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-21 01:58:20 +0200

    works but calculations are wrong

--- components/impact.vue
+++ components/impact.vue
@@ -1,20 +1,75 @@
 <template>
   <div class="impact">
     <div class="image" :id="props.type"></div>
-    <p class="title">{{props.title}}</p>
+    <p class="title">{{title}}</p>
   </div>
 </template>
 <script setup>
-const props = defineProps({
-  title: {
-    type: String,
-    required: true
-  },
-  type: {
-    type: String,
-    required: true
+  const supabase = useSupabaseClient()
+  const user = useSupabaseUser()
+  const props = defineProps({
+    type: {
+      type: String,
+      required: true
+    }
+  })
+  const getImpactEquivalent = async (productGWP,euros) => {
+    const KWh = euros*0.07; // 1 EUR = 0.07 KWh
+    const Co2 = 0.527;    // KWh equivalent CO2 emissions
+    const avoidedEmissions=KWh*Co2;
+    const equivalent = avoidedEmissions/productGWP
+    return equivalent
   }
-})
+  const getUserPortfolio = async () => {
+    const { data, error } = await supabase
+      .from('get_user_portfolio')
+      .select()
+      .eq('user_id', user.value.id)
+      .limit(1)
+      .single()
+    if(data){
+      return {
+        'value': data.value,
+        'currency': data.value_currency
+      }
+    }
+  }
+  
+  const getExchangeRate = async (from) => {
+    if(from === 'EUR') return 1
+    const { data, error } = await supabase
+      .from('exchange_rates')
+      .select()
+      .eq('from', )
+      .eq('to', 'EUR')
+      .limit(1)
+      .single()
+    if(data){
+      return data.rate
+    }
+  }
+  const title = ref('missing type?')
+  const portfolio = await getUserPortfolio()
+  const exchangeRate = await getExchangeRate(portfolio.currency)
+  const eur = portfolio.value*exchangeRate||1;
+
+  if(props.type === 'fiat') {
+    const equivalent = await getImpactEquivalent(0.00012, eur)
+    title.value = 'Driving '+equivalent+' km in a Fiat Panda'
+  }
+  
+  if(props.type === 'house') {
+    const equivalent = await getImpactEquivalent(0.6, eur)
+    title.value = 'Construction of a '+equivalent+' m2 house'
+  }
+  if(props.type === 'plane') {
+    const equivalent = await getImpactEquivalent(0.78, eur)
+    title.value = 'Flying '+equivalent+' times to Paris from Tokyo'
+  }
+  if(props.type == 'avocado') {
+    const equivalent = await getImpactEquivalent(0.0005, eur)
+    title.value = 'Producing and eating '+equivalent+' avocadoes'
+  }
 </script>
 <style scoped lang="scss">
 .impacts{
@@ -25,7 +80,7 @@ const props = defineProps({
   gap: 2% 2%;
   grid-auto-flow: row;
 }
-.impacts .impact{
+.impact{
   padding:$clamp 0;
   border-top:$border;
   display: grid;
@@ -36,7 +91,7 @@ const props = defineProps({
   font-size:120%;
 }
 
-.impacts .impact .image{
+.impact .image{
   height: $clamp-2-5;
   width: $clamp-2-5;
   border-radius:50%;
@@ -45,16 +100,16 @@ const props = defineProps({
   background-size:cover;
   background-image:url('/orbs/grain.png');
 }
-.impacts .impact .image#pink{
+.impact .image#fiat{
   background-color:$pink-40;
 }
-.impacts .impact .image#blue{
+.impact .image#house{
   background-color:$blue-40;
 }
-.impacts .impact .image#red{
+.impact .image#plane{
   background-color:$red-40;
 }
-.impacts .impact .image#green{
+.impact .image#avocado{
   background-color:$green-40;
 }
 </style>
