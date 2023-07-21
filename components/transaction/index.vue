97afd293b4fd7c22f6ff4e6892d14e49374e7e6d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-21 16:34:13 +0200

    dark mode

--- components/transaction/index.vue
+++ components/transaction/index.vue
@@ -3,7 +3,7 @@
     <div class="transaction-type" :id="props.type">
     </div>
     <div class="amount">
-      {{ok.prettyCurrency(props.amount, props.currency)}}
+      {{ok.prettyCurrency(props.amount, currency)}}
     </div>
     <div class="date">
       {{ok.prettyDate(props.date)}}
@@ -28,31 +28,42 @@
       required: true
     },
     currency:{
-      type: String, 
+      type: String,
       required: true
     }
-  })
-
+  }) 
+  const currency = props.currency || 'EUR';
 
 </script>
 <style scoped lang="scss">
-.transaction {
-  display: grid; 
-  grid-template-columns: $clamp-2 12fr 2fr 2fr; 
-  gap: $clamp;
-  border-bottom:$border;
-}
-.transaction-type{
-  background-size:$clamp-1;
-  background-repeat:no-repeat;
-  background-position:center;
-  width:$clamp-2;
-  height:$clamp-2;
-}
-#withdraw{
-  background-image:url('/omoji/withdraw.svg');
-}
-#deposit{
-  background-image:url('/omoji/deposit.svg');
-}
+  .transaction {
+    display: grid; 
+    grid-template-columns: $clamp-2 12fr 2fr 2fr; 
+    gap: $clamp;
+    border-bottom:$border;
+  }
+  .transaction-type{
+    background-size:$clamp-1;
+    background-repeat:no-repeat;
+    background-position:center;
+    width:$clamp-2;
+    height:$clamp-2;
+  }
+  #withdraw{
+    background-image:url('/omoji/withdraw.svg');
+  }
+  #deposit{
+    background-image:url('/omoji/deposit.svg');
+  }
+  .dark-mode{
+    .transaction{
+      border-color:$light;
+    }
+    #withdraw{
+      background-image:url('/omoji/light/withdraw.svg');
+    }
+    #deposit{
+      background-image:url('/omoji/light/deposit.svg');
+    }
+  }
 </style>
