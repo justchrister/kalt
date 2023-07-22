5bcefbc12c630f257a3a58cae28f7ee7552c9119
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-22 14:42:19 +0200

    Create color-mode.ts

---
+++ middleware/color-mode.ts
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware(() => {
+  const user = useSupabaseUser()
+
+  const colorMode = useColorMode()
+  if (!user.value) {
+    colorMode.preference = 'light-mode';
+  }
+})
