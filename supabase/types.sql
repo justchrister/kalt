f52fb14762270db10291f5185ca7af7b1bb254e3
christer <42318403+justchrister@users.noreply.github.com>
2023-07-27 10:36:22 +0200

    Update types.sql

--- supabase/types.sql
+++ supabase/types.sql
@@ -1,26 +1,3 @@
-create type public.transaction_statuses as enum (
-    'incomplete',
-    'payment_awaiting',
-    'payment_declined',
-    'payment_accepted',
-    'withdrawal_awaiting',
-    'withdrawal_processing',
-    'withdrawal_accepted',
-    'payment_processing'
-);
-create type public.transaction_types as enum (
-    'deposit',
-    'withdraw'
-);
-create type public.transaction_sub_types as enum (
-    'card',
-    'wire_transfer',
-    'dividend',
-    'subscription',
-    'auto_invested',
-    'new_shares',
-    'auto_withdraw'
-);
 create type public.card_statuses as enum (
     'incomplete',
     'active',
@@ -34,19 +11,19 @@ create type public.order_types as enum (
     'buy',
     'sell'
 );
-create type public.order_statuses as enum (
+create type public."order_statuses" as enum (
     'open',
     'fulfilled',
     'cancelled',
     'split'
 );
-create type public.payments_pending_statuses as enum (
+create type public."paymentsPending_statuses" as enum (
     'pending',
     'processing',
     'failed',
     'complete'
 );
-create type public.payment_providers as enum (
+create type public."payments_paymentProviders" as enum (
     'stripe'
 );
 create type public.continents as enum (
