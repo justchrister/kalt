a0bc0c28b632ffecb631dd7b363a9f41954b23a5
christer <42318403+justchrister@users.noreply.github.com>
2023-07-27 10:38:41 +0200

    Update accountTransactions.sql

--- supabase/topics/accountTransactions.sql
+++ supabase/topics/accountTransactions.sql
@@ -1,40 +1,39 @@
 -- create the topic 
 
 --- create the table, with default values
-CREATE TABLE account_transactions (
+CREATE TABLE "topic_accountTransactions" (
 -- meta information used for processing
-    message_id            uuid                      NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    message_entity_id     uuid                      NOT NULL  DEFAULT uuid_generate_v4(),
-    message_created       timestamptz               NOT NULL  DEFAULT (now() at time zone 'utc'),
-    message_sender        text                      NOT NULL,
---- 
-    user_id               uuid                      NOT NULL,
-    amount                numeric,
-    currency              CHAR(3)                                                                REFERENCES currencies(iso),
-    transaction_type      transaction_types,
-    transaction_sub_type  transaction_sub_types,
-    transaction_status    transaction_statuses      NOT NULL,
-    auto_invest           DECIMAL(5, 4) 
-                          CHECK (auto_invest >= 0 
-                          AND auto_invest <= 1)     NOT NULL
+    "id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "sender"      text                            NOT NULL,
+    --- 
+    "userId"      uuid                            NOT NULL,
+    "amount"      numeric,
+    "currency"    CHAR(3)                                         REFERENCES sys_currencies(iso),
+    "type"        "accountTransactions_types",
+    "subType"     "accountTransactions_subTypes",
+    "status"      "accountTransactions_statuses"  NOT NULL,
+    "autoInvest"  DECIMAL(5, 4) 
+                  CHECK ("autoInvest" >= 0 
+                  AND "autoInvest" <= 1)          NOT NULL
 );
 
 --- add row level security
-ALTER TABLE account_transactions ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "topic_accountTransactions" ENABLE ROW LEVEL SECURITY;
 
 --- Standard descriptions
-comment on column user_details.message_id 
+comment on column "topic_accountTransactions".id 
 is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
 
-comment on column user_details.message_entity_id 
+comment on column "topic_accountTransactions".entity 
 is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
 
-comment on column user_details.message_created 
+comment on column "topic_accountTransactions".sent 
 is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
 
-comment on column user_details.message_sender 
+comment on column "topic_accountTransactions".sender
 is 'where the message originates, usually set in the application.';
-
 
 DO $$
 BEGIN
@@ -42,13 +41,13 @@ BEGIN
     SELECT 1
     FROM pg_policies
     WHERE schemaname = 'public'
-      AND tablename = 'account_transactions'
+      AND tablename = 'topic_accountTransactions'
       AND policyname = 'SELF — Insert'
   ) THEN
-    CREATE POLICY "SELF — Insert" ON public.account_transactions
+    CREATE POLICY "SELF — Insert" ON public."topic_accountTransactions"
       AS PERMISSIVE FOR INSERT
       TO authenticated
-      WITH CHECK (auth.uid() = user_id);
+      WITH CHECK (auth.uid() = (content->>'userId')::uuid);
   END IF;
 END
 $$;
