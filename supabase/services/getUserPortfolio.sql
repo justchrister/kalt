1bd6265cbddc9f63179d212d67d5c3e55f90138a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 15:08:51 +0200

    Update getUserPortfolio.sql

--- supabase/services/getUserPortfolio.sql
+++ supabase/services/getUserPortfolio.sql
@@ -30,7 +30,7 @@ BEGIN
     CREATE POLICY "SELF — Select" ON public.get_user_portfolio
       AS PERMISSIVE FOR SELECT
       TO authenticated
-      USING (auth.uid() = user_id);
+      USING (auth.uid() = "userId");
   END IF;
 END
 $$;
