8b72bbd1e68a0d11f434262d11b2e2d8f4b35a00
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 15:09:53 +0200

    Update userSubscriptions.sql

--- supabase/topics/userSubscriptions.sql
+++ supabase/topics/userSubscriptions.sql
@@ -32,7 +32,7 @@ BEGIN
     CREATE POLICY "SELF — Insert" ON public."topic_userSubscriptions"
       AS PERMISSIVE FOR INSERT
       TO authenticated
-      WITH CHECK (auth.uid() = user_id);
+      WITH CHECK (auth.uid() = "userId");
   END IF;
 END
 $$;
@@ -49,7 +49,7 @@ BEGIN
     CREATE POLICY "SELF — Select" ON public."topic_userSubscriptions"
       AS PERMISSIVE FOR SELECT
       TO authenticated
-      USING (auth.uid() = user_id);
+      USING (auth.uid() = "userId");
   END IF;
 END
 $$;
