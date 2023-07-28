957374f2c9b1eb1e41a50a7ac66fc2f6742e2865
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 15:27:18 +0200

    new structure

--- supabase/sub/accountTransactions/autoInvest.sql
+++ supabase/sub/accountTransactions/autoInvest.sql
@@ -1,10 +1,10 @@
 -- subscribe to topic, by simply renaming all instances of <<topic name>__<service name>
 -- version 6.4.23
--- service acl_stripe_create_card
--- topic   payment_cards
+-- service auto_invest
+-- topic   account_transactions
 
 --- create the table, with default values
-CREATE TABLE acl_stripe__payment_cards (
+CREATE TABLE auto_invest__account_transactions (
     message_id          uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     message_entity_id   uuid        NOT NULL  DEFAULT uuid_generate_v4(),
     message_created     timestamptz NOT NULL  DEFAULT (now() at time zone 'utc'),
@@ -13,37 +13,37 @@ CREATE TABLE acl_stripe__payment_cards (
 );
 
 --- add row level security
-ALTER TABLE acl_stripe__payment_cards ENABLE ROW LEVEL SECURITY;
+ALTER TABLE auto_invest__account_transactions ENABLE ROW LEVEL SECURITY;
 
 --- Standard descriptions
-comment on column acl_stripe__payment_cards.message_id 
+comment on column auto_invest__account_transactions.message_id 
 is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
 
-comment on column acl_stripe__payment_cards.message_entity_id 
+comment on column auto_invest__account_transactions.message_entity_id 
 is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
 
-comment on column acl_stripe__payment_cards.message_created 
+comment on column auto_invest__account_transactions.message_created 
 is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
 
-comment on column acl_stripe__payment_cards.message_sender 
+comment on column auto_invest__account_transactions.message_sender 
 is 'where the message originates, usually set in the application.';
 
-comment on column acl_stripe__payment_cards.message_read 
+comment on column auto_invest__account_transactions.message_read 
 is 'the only field that is updated in this table, when the service reads it, it changes the bool to true.';
 
 
--- Create the trigger function on the payment_cards
-CREATE OR REPLACE FUNCTION acl_stripe__payment_cards()
+-- Create the trigger function on the account_transactions
+CREATE OR REPLACE FUNCTION auto_invest__account_transactions()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO acl_stripe__payment_cards (message_id, message_entity_id, message_sender, message_created)
+  INSERT INTO auto_invest__account_transactions (message_id, message_entity_id, message_sender, message_created)
   VALUES (NEW.message_id, NEW.message_entity_id, NEW.message_sender, NEW.message_created);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create the trigger on the topic table and event
-CREATE TRIGGER acl_stripe__payment_cards
-AFTER INSERT ON payment_cards
+CREATE TRIGGER auto_invest__account_transactions
+AFTER INSERT ON account_transactions
 FOR EACH ROW
-EXECUTE FUNCTION acl_stripe__payment_cards();
+EXECUTE FUNCTION auto_invest__account_transactions();
--- supabase/sub/paymentCards/acl_stripe.sql
+++ supabase/sub/paymentCards/acl_stripe.sql
@@ -1,10 +1,10 @@
 -- subscribe to topic, by simply renaming all instances of <<topic name>__<service name>
 -- version 6.4.23
--- service get_user_portfolio
--- topic   user_signed_in
+-- service acl_stripe_create_card
+-- topic   payment_cards
 
 --- create the table, with default values
-CREATE TABLE get_user_portfolio__user_signed_in (
+CREATE TABLE sub paymentCards (
     message_id          uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     message_entity_id   uuid        NOT NULL  DEFAULT uuid_generate_v4(),
     message_created     timestamptz NOT NULL  DEFAULT (now() at time zone 'utc'),
@@ -13,37 +13,37 @@ CREATE TABLE get_user_portfolio__user_signed_in (
 );
 
 --- add row level security
-ALTER TABLE get_user_portfolio__user_signed_in ENABLE ROW LEVEL SECURITY;
+ALTER TABLE acl_stripe__payment_cards ENABLE ROW LEVEL SECURITY;
 
 --- Standard descriptions
-comment on column get_user_portfolio__user_signed_in.message_id 
+comment on column acl_stripe__payment_cards.message_id 
 is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
 
-comment on column get_user_portfolio__user_signed_in.message_entity_id 
+comment on column acl_stripe__payment_cards.message_entity_id 
 is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
 
-comment on column get_user_portfolio__user_signed_in.message_created 
+comment on column acl_stripe__payment_cards.message_created 
 is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
 
-comment on column get_user_portfolio__user_signed_in.message_sender 
+comment on column acl_stripe__payment_cards.message_sender 
 is 'where the message originates, usually set in the application.';
 
-comment on column get_user_portfolio__user_signed_in.message_read 
+comment on column acl_stripe__payment_cards.message_read 
 is 'the only field that is updated in this table, when the service reads it, it changes the bool to true.';
 
 
--- Create the trigger function on the user_signed_in
-CREATE OR REPLACE FUNCTION get_user_portfolio__user_signed_in()
+-- Create the trigger function on the payment_cards
+CREATE OR REPLACE FUNCTION acl_stripe__payment_cards()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO get_user_portfolio__user_signed_in (message_id, message_entity_id, message_sender, message_created)
+  INSERT INTO acl_stripe__payment_cards (message_id, message_entity_id, message_sender, message_created)
   VALUES (NEW.message_id, NEW.message_entity_id, NEW.message_sender, NEW.message_created);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create the trigger on the topic table and event
-CREATE TRIGGER get_user_portfolio__user_signed_in
-AFTER INSERT ON user_signed_in
+CREATE TRIGGER acl_stripe__payment_cards
+AFTER INSERT ON payment_cards
 FOR EACH ROW
-EXECUTE FUNCTION get_user_portfolio__user_signed_in();
+EXECUTE FUNCTION acl_stripe__payment_cards();
