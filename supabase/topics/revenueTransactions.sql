42cb2bce9f12f8a8a3c316304cc55879073ff446
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 14:27:34 +0200

    prefixing w/message_* + removing column comments

--- supabase/topics/revenueTransactions.sql
+++ supabase/topics/revenueTransactions.sql
@@ -1,32 +1,19 @@
 -- create a topic, by simply renaming all instances of <<topic_name>
 -- version 6.4.23
--- topic   revenue_transactions
+-- topic   "topic_revenueTransactions"
 
 --- create the table, with default values
-CREATE TABLE revenue_transactions (
+CREATE TABLE "topic_revenueTransactions" (
 -- meta information used for processing
-    "id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
-    "sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
-    "sender"      text                            NOT NULL,
+    "message_id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "message_entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "message_sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "message_sender"      text                            NOT NULL,
     -- 
-    ticker              tickers     NOT NULL, 
-    amount              numeric,
-    currency            text
+    "ticker"              tickers     NOT NULL, 
+    "amount"              numeric,
+    "currency"            text
 );
 
 --- add row level security
-ALTER TABLE revenue_transactions ENABLE ROW LEVEL SECURITY;
-
---- Standard descriptions
-comment on column revenue_transactions.message_id 
-is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
-
-comment on column revenue_transactions.message_entity_id 
-is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
-
-comment on column revenue_transactions.message_created 
-is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
-
-comment on column revenue_transactions.message_sender 
-is 'where the message originates, usually set in the application.';
+ALTER TABLE "topic_revenueTransactions" ENABLE ROW LEVEL SECURITY;
