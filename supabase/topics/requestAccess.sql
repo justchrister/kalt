8e38fb5f4b1beb3a20140dd314d82a48fdc7fcea
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 14:27:30 +0200

    prefixing w/message_* + removing column comments

--- supabase/topics/requestAccess.sql
+++ supabase/topics/requestAccess.sql
@@ -1,33 +1,20 @@
 -- create a topic, by simply renaming all instances of <<topic_name>
 -- version 6.4.23
--- topic   request_access
+-- topic   "topic_requestAccess"
 
 --- create the table, with default values
-CREATE TABLE request_access (
+CREATE TABLE "topic_requestAccess" (
 -- meta information used for processing
-    "id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
-    "sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
-    "sender"      text                            NOT NULL,
+    "message_id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "message_entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "message_sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "message_sender"      text                            NOT NULL,
 -- 
-    email               text                        NOT NULL,
-    first_name          text,
-    last_name           text,
-    country             text
+    "email"               text                            NOT NULL,
+    "firstName"           text,
+    "lastName"            text,
+    "country"             text
 );
 
 --- add row level security
-ALTER TABLE request_access DISABLE ROW LEVEL SECURITY;
-
---- Standard descriptions
-comment on column request_access.message_id 
-is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
-
-comment on column request_access.message_entity_id 
-is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
-
-comment on column request_access.message_created 
-is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
-
-comment on column request_access.message_sender 
-is 'where the message originates, usually set in the application.';
+ALTER TABLE "topic_requestAccess" DISABLE ROW LEVEL SECURITY;
