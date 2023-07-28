571a092bed6e1a63a693105f51f5e327f2e8ab5d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 15:08:40 +0200

    Update languages.sql

--- supabase/system/languages.sql
+++ supabase/system/languages.sql
@@ -1,14 +1,17 @@
-
-
-create table
-  public."sys_languages" (
+create table public."sys_languages" (
     iso           text        not null,
     name          text,
-    available     boolean                 default false,
+    available     boolean                 default false
+);
 
 
 --- add row level security
 ALTER TABLE "sys_languages" ENABLE ROW LEVEL SECURITY;
+
+insert into public."sys_languages" (iso, name, available) values 
+  ('NOR', 'Norwegian', true),
+  ('ENG', 'English', true),
+  ('GER', 'German', true);
 
 CREATE POLICY "AUTH — Select"
 ON public."sys_languages"
@@ -17,8 +20,3 @@ TO authenticated
 USING (
   true
 );
-
-insert into public."sys_languages" (iso, name, available) values 
-  ('NOR', 'Norwegian', true),
-  ('ENG', 'English', true),
-  ('GER', 'German', true);
