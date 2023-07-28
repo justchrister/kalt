37187e8f6915b218f6e17f996a94ed97c018e1a1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 14:26:42 +0200

    prefixing w/message_* + removing column comments

--- supabase/topics/exchangeRates.sql
+++ supabase/topics/exchangeRates.sql
@@ -5,10 +5,10 @@
 --- create the table, with default values
 CREATE TABLE "topic_exchangeRates" (
 -- meta information used for processing
-    "id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
-    "sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
-    "sender"      text                            NOT NULL,
+    "message_id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "message_entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "message_sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "message_sender"      text                            NOT NULL,
 --
     "from"                CHAR(3)         NOT NULL,
     "to"                  CHAR(3)         NOT NULL,
@@ -17,16 +17,3 @@ CREATE TABLE "topic_exchangeRates" (
 
 --- add row level security
 ALTER TABLE "topic_exchangeRates" ENABLE ROW LEVEL SECURITY;
-
---- Standard descriptions
-comment on column "topic_exchangeRates"."id" 
-is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
-
-comment on column "topic_exchangeRates"."entity" 
-is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
-
-comment on column "topic_exchangeRates"."sent" 
-is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
-
-comment on column "topic_exchangeRates"."sender" 
-is 'where the message originates, usually set in the application.';
