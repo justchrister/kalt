25e475f4edcba3c0215eac4dc55efa2d228aa469
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 15:09:03 +0200

    Update getUser.sql

--- supabase/services/getUser.sql
+++ supabase/services/getUser.sql
@@ -40,7 +40,7 @@ BEGIN
     CREATE POLICY "SELF — Select" ON public.get_user
       AS PERMISSIVE FOR SELECT
       TO authenticated
-      USING (auth.uid() = user_id);
+      USING (auth.uid() = "userId");
   END IF;
 END
 $$;
