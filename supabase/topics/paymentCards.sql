7e85a7d61bef9d32ca54bc013ec811725042a493
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-28 10:59:59 +0200

    architecture 2.0

--- supabase/topics/paymentCards.sql
+++ supabase/topics/paymentCards.sql
@@ -1,43 +1,39 @@
 -- create a topic, by simply renaming all instances of <<topic_name>
 -- version 6.4.23
--- topic   payment_cards
+-- topic   "topic_paymentCards"
 
 --- create the table, with default values
-CREATE TABLE payment_cards (
+CREATE TABLE "topic_paymentCards" (
 -- meta information used for processing
-    message_id          uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    message_entity_id   uuid        NOT NULL  DEFAULT uuid_generate_v4(),
-    message_created     timestamptz NOT NULL  DEFAULT (now() at time zone 'utc'),
-    message_sender      text        NOT NULL,
+    "id"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "entity"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "sender"      text                            NOT NULL,
 -- 
-    user_id             uuid        NOT NULL,
-    last_four_digits    CHAR(4),
-    expiry_year         CHAR(2),
-    expiry_month        CHAR(2),
-    cvc                 CHAR(3),
-    card_number         CHAR(16),
-    card_status         text,
+    "userId"             uuid        NOT NULL,
+    "lastFourDigits"    CHAR(4),
+    "year"         CHAR(2),
+    "month"        CHAR(2),
+    "cvc"                 CHAR(3),
+    "number"         CHAR(16),
+    "status"         text,
     "default"           boolean
 );
 
 --- add row level security
-ALTER TABLE payment_cards ENABLE ROW LEVEL SECURITY;
--- changing the postal_code type to text, to support postal codes prefixed with 0
-ALTER TABLE payment_cards ALTER COLUMN postal_code TYPE text;
--- adding check to ensure its only numbers 
-ALTER TABLE payment_cards ADD CONSTRAINT postal_code_numeric_check CHECK (postal_code ~ '^[0-9]*$');
+ALTER TABLE "topic_paymentCards" ENABLE ROW LEVEL SECURITY;
 
 --- Standard descriptions
-comment on column payment_cards.message_id 
+comment on column "topic_paymentCards"."id" 
 is 'this is the unique id of this message, not the unique id of its contents. (for instance, account_transactions have their own account_transaction_id';
 
-comment on column payment_cards.message_entity_id 
+comment on column "topic_paymentCards"."entity" 
 is 'Used to correlate messages that are associated with a single entity, since they will have updates in the same table with different message_ids, usually a 1:1 relation to an order_id or similar';
 
-comment on column payment_cards.message_created 
+comment on column "topic_paymentCards"."sent" 
 is 'when the message was generated, usually set in the application. It can be created in javascript by doing ok.timestamptz()';
 
-comment on column payment_cards.message_sender 
+comment on column "topic_paymentCards"."sender" 
 is 'where the message originates, usually set in the application.';
 
 
@@ -47,13 +43,13 @@ BEGIN
     SELECT 1
     FROM pg_policies
     WHERE schemaname = 'public'
-      AND tablename = 'payment_cards'
+      AND tablename = '"topic_paymentCards"'
       AND policyname = 'SELF — Insert'
   ) THEN
-    CREATE POLICY "SELF — Insert" ON public.payment_cards
+    CREATE POLICY "SELF — Insert" ON public."topic_paymentCards"
       AS PERMISSIVE FOR INSERT
       TO authenticated
-      WITH CHECK (auth.uid() = user_id);
+      WITH CHECK (auth.uid() = "userId");
   END IF;
 END
 $$;
