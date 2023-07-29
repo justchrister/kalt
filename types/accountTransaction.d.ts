8e8945ebc3e4af6790469f3ba73b78c573067944
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-29 20:44:30 +0200

    Update accountTransaction.d.ts

--- types/accountTransaction.d.ts
+++ types/accountTransaction.d.ts
@@ -1,13 +1,13 @@
-export interface accountTransaction {
-    id?: string; // UUID
-    entity?: string; // UUID
-    sent?: Date; // timestamptz
-    sender?: string;
-    userId?: string; // UUID
-    amount?: number; // numeric
-    currency?: string; // CHAR(3) - references currencies(iso)
-    type?: AccountTransactionsTypes;
-    subType?: AccountTransactionsSubTypes;
-    status: AccountTransactionsStatuses;
-    autoInvest: number; // DECIMAL(5, 4)
+export { accountTransaction };
+
+declare global {
+    interface accountTransaction {
+        userId?: string; // UUID
+        amount?: number; // numeric
+        currency?: string; // CHAR(3) - references currencies(iso)
+        type?: AccountTransactionsTypes;
+        subType?: AccountTransactionsSubTypes;
+        status: AccountTransactionsStatuses;
+        autoInvest: number; // DECIMAL(5, 4)
+    }
 }
