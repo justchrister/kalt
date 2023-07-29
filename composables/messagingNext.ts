f5f2b6572d8ba601cb61c01a3d71fd5f8666f9d0
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-29 15:08:02 +0200

    new pub function, returns data and error

--- composables/messagingNext.ts
+++ composables/messagingNext.ts
@@ -1,43 +1,32 @@
-// @ts-nocheck
 import { ok } from '~/composables/ok'
-  
+
+const createJsonAndPublish = async (content, topic) => {
+  const json = {
+    'id': meta.id || ok.uuid(),
+    'entity': meta.entity || ok.uuid(),
+    'sent': meta.sent || ok.timestamptz(),
+    'sender': meta.sender,
+    ...content
+  }
+  const { data, error } = await client.from(topic).insert(json).select()
+  if (error) {
+    ok.log('error', `failed to insert ${topic}: `+error.message)
+  } else {
+    ok.log('success', `inserted on ${topic}: `, data)
+  }
+  return { data, error }
+}
+
 export const pub = (client, meta: messageMeta) => {
   return {
     accountTransaction: async (content: accountTransaction) => {
-      const json = {
-        'id': meta.id || ok.uuid(),
-        'entity': meta.entity || ok.uuid(),
-        'sent': meta.sent || ok.timestamptz(),
-        'sender': meta.sender,
-        ...content
-      }
-      const { data, error } = await client.from('topic_accountTransaction').insert(json).select()
-      if(data) {
-        ok.log('success', 'inserted on accountTransaction: ', data)
-        return data
-      }
-      if(error) {
-        ok.log('error', 'failed to insert accountTransaction: '+error.message)
-        return error
-      }
+      return await createJsonAndPublish(content, topic_accountTransaction);
     },
     exchangeOrder: async (content: exchangeOrder) => {
-      const json = {
-        'id': meta.id || ok.uuid(),
-        'entity': meta.entity || ok.uuid(),
-        'sent': meta.sent || ok.timestamptz(),
-        'sender': meta.sender,
-        ...content
-      }
-      const { data, error } = await client.from(topic_exchangeOrder).insert(json).select()
-      if(data) {
-        ok.log('success', 'inserted on exchangeOrder: ', data)
-        return data
-      }
-      if(error) {
-        ok.log('error', 'failed to insert exchangeOrder: '+error.message)
-        return error
-      }
+      return await createJsonAndPublish(content, topic_exchangeOrder);
     },
+    userSubscriptions: async (content: userSubscription) => {
+      return await createJsonAndPublish(content, topic_userSubscriptions);
+    }
   }
 }
