dd5a0098756d40116ab776e6b3f156ce334ee4e7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-31 07:09:24 +0200

    Create exchangeOrder.d.ts

---
+++ types/exchangeOrder.d.ts
@@ -0,0 +1,14 @@
+export { exchangeOrder };
+
+declare global {
+  interface exchangeOrder {
+    userId: string;
+    quantity?: number;
+    ticker?: string;
+    type?: exchangeOrdersTypes;
+    status: exchangeOrdersStatuses;
+    fulfilledBy?: string;
+    splitInto?: string[];
+    partOf?: string;
+  }
+}
