f0e8515d340762249fca10d72fe3036682844c55
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-31 07:10:06 +0200

    Create userPreferences.d.ts

---
+++ types/userPreferences.d.ts
@@ -0,0 +1,15 @@
+export { userPreferences };
+
+declare global {
+    interface userPreferences {
+        userId: string;
+        autoInvest: number;
+        newsletters?: boolean;
+        termsOfService?: boolean;
+        performanceUpdates?: boolean;
+        colorScheme?: string;
+        profilePicture?: string;
+        language?: string;
+        currency?: string;
+    }
+}
