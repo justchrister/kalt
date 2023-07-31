f12aa96ae60dad85438bb99cf61d92d997a401c6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-31 07:09:20 +0200

    cleaning up

--- types/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface ExchangeOrder {
-  message_entity_id: string;
-  user_id: string;
-  ticker: string;
-  order_type: string;
-  order_status: string;
-  quantity: number;
-  quantity_absolute: number;
-}
