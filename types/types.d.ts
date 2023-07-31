b274084c8e1c4e104bb4374d312a855d85ebba5a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-31 07:09:56 +0200

    Update types.d.ts

--- types/types.d.ts
+++ types/types.d.ts
@@ -20,4 +20,35 @@ declare global {
       newShares = 'newShares',
       autoWithdraw = 'autoWithdraw'
   }
+  const enum paymentCardsStatuses {
+    incomplete = 'incomplete',
+    active = 'active',
+    rejected = 'rejected',
+    expired = 'expired'
+  }
+  const enum exchangeOrdersTypes {
+    buy = 'buy',
+    sell = 'sell'
+  }
+  const enum exchangeOrdersStatuses {
+    open = 'open',
+    fulfilled = 'fulfilled',
+    cancelled = 'cancelled',
+    split = 'split'
+  }
+  const enum paymentsPendingStatuses {
+    pending = 'pending',
+    processing = 'processing',
+    failed = 'failed',
+    complete = 'complete'
+  }
+  const enum paymentProviders {
+    stripe = 'stripe'
+  }
+  const enum tickers {
+    gi_ddf = 'gi.ddf',
+    mi_ddf = 'mi.ddf',
+    ffe_ddf = 'ffe.ddf',
+    ah_ddf = 'ah.ddf'
+  }
 }
