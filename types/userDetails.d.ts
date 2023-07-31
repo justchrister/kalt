55d6e8c78df7ced19e93a7d1d92f1b1f3ce60e0d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-31 07:09:03 +0200

    Create userDetails.d.ts

---
+++ types/userDetails.d.ts
@@ -0,0 +1,15 @@
+export { userDetails };
+
+declare global {
+    interface userDetails {
+        userId: string;
+        firstName?: string;
+        lastName?: string;
+        country?: string;
+        city?: string;
+        postalCode?: string;
+        birthdate?: Date;
+        addressLine1?: string;
+        addressLine2?: string;
+    }
+}
