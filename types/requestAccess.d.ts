a5dc4a6c885eb432f803dbe86e304872a70c3da6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-07-31 07:09:09 +0200

    Create requestAccess.d.ts

---
+++ types/requestAccess.d.ts
@@ -0,0 +1,10 @@
+export { requestAccess };
+
+declare global {
+    interface requestAccess {
+        email: string;
+        firstName?: string;
+        lastName?: string;
+        country?: string;
+    }
+}
