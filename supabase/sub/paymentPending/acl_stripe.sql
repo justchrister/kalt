184134ea35c5905f45d9a3b960a424e6ca2a6bcd
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-01 21:18:52 +0200

    architecture 2.0

--- supabase/sub/paymentPending/acl_stripe.sql
+++ supabase/sub/paymentPending/acl_stripe.sql
@@ -1,9 +1,9 @@
 -- version 29.7.23
 -- service acl_stripe
--- topic   paymentPending
+-- topic   paymentsPending
 
 --- create the table, with default values
-CREATE TABLE "sub_paymentPending_acl_stripe" (
+CREATE TABLE "sub_paymentsPending_acl_stripe" (
     "message_id"          uuid          NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "message_entity"      uuid          NOT NULL  DEFAULT uuid_generate_v4(),
     "message_sent"        timestamptz   NOT NULL  DEFAULT (now() at time zone 'utc'),
@@ -12,25 +12,25 @@ CREATE TABLE "sub_paymentPending_acl_stripe" (
 );
 
 -- Create the replicate function 
-CREATE OR REPLACE FUNCTION "replicate_paymentPending_acl_stripe"()
+CREATE OR REPLACE FUNCTION "replicate_paymentsPending_acl_stripe"()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO "sub_paymentPending_acl_stripe" (message_id, message_entity, message_sender, message_sent)
+  INSERT INTO "sub_paymentsPending_acl_stripe" (message_id, message_entity, message_sender, message_sent)
   VALUES (NEW.message_id, NEW.message_entity, NEW.message_sender, NEW.message_sent);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create replicate trigger
-CREATE TRIGGER "replicate_paymentPending_acl_stripe"
-AFTER INSERT ON "topic_paymentPending"
+CREATE TRIGGER "replicate_paymentsPending_acl_stripe"
+AFTER INSERT ON "topic_paymentsPending"
 FOR EACH ROW
-EXECUTE FUNCTION "replicate_paymentPending_acl_stripe"();
+EXECUTE FUNCTION "replicate_paymentsPending_acl_stripe"();
 
 
 -- Set up webhook function 
 
-CREATE OR REPLACE FUNCTION "webhook_paymentPending_acl_stripe"()
+CREATE OR REPLACE FUNCTION "webhook_paymentsPending_acl_stripe"()
 RETURNS TRIGGER AS $$
 DECLARE 
   response RECORD;
@@ -39,7 +39,7 @@ BEGIN
   -- Convert row data to json then to string format
   payload := row_to_json(NEW)::text;
   SELECT * INTO response FROM http_post(
-    'https://ka.lt/api/acl/stripe/webhooks/paymentPending',
+    'https://ka.lt/api/acl/stripe/webhooks/paymentsPending',
     payload,
     'application/json'
   );
@@ -49,10 +49,10 @@ END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create webhook trigger
-CREATE TRIGGER "webhook_paymentPending_acl_stripe"
-AFTER INSERT ON "sub_paymentPending_acl_stripe"
+CREATE TRIGGER "webhook_paymentsPending_acl_stripe"
+AFTER INSERT ON "sub_paymentsPending_acl_stripe"
 FOR EACH ROW
-EXECUTE FUNCTION "webhook_paymentPending_acl_stripe"(NEW);
+EXECUTE FUNCTION "webhook_paymentsPending_acl_stripe"(NEW);
 
 -- Enable RLS
-ALTER TABLE "sub_paymentPending_acl_stripe" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "sub_paymentsPending_acl_stripe" ENABLE ROW LEVEL SECURITY;
