00d480743e60cfd92d55a2a029399f624323f756
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-02 19:18:00 +0200

    formatting

--- supabase/services/payRevenue.sql
+++ supabase/services/payRevenue.sql
@@ -1,8 +1,8 @@
 --- create the table, with default values
 CREATE TABLE "payRevenue" (
-  "userId"          uuid        NOT NULL PRIMARY KEY,
-  amount            numeric,
-  currency          CHAR(3)
+  "userId"      uuid        NOT NULL    PRIMARY KEY,
+  "amount"      numeric,
+  "currency"    CHAR(3)
 );
 
 --- Add row level security
--- supabase/services/payments.sql
+++ supabase/services/payments.sql
@@ -1,10 +1,10 @@
 --- create the table, with default values
 CREATE TABLE payments (
-  "transactionId"    uuid                            NOT NULL  PRIMARY KEY,
-  "userId"           uuid                            NOT NULL,
-  "amount"            numeric                         NOT NULL,
-  "currency"          CHAR(3)                         NOT NULL,
-  "processing"        boolean                         NOT NULL  DEFAULT FALSE
+  "transactionId"    uuid           NOT NULL  PRIMARY KEY,
+  "userId"           uuid           NOT NULL,
+  "amount"           numeric        NOT NULL,
+  "currency"         CHAR(3)        NOT NULL,
+  "processing"       boolean        NOT NULL  DEFAULT FALSE
 );
 
 ALTER TABLE payments ENABLE ROW LEVEL SECURITY;
