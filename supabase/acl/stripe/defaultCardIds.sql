f21269e97802351ae3b42c69a04eeba4e60bf485
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-07 00:48:40 +0200

    + quotes

--- supabase/acl/stripe/defaultCardIds.sql
+++ supabase/acl/stripe/defaultCardIds.sql
@@ -1,5 +1,5 @@
-CREATE TABLE acl_stripe_defaultCardIds (
+CREATE TABLE "acl_stripe_defaultCardIds" (
     "userId"          uuid    NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "stripeCardId"    text
 );
-ALTER TABLE acl_stripe_defaultCardIds ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "acl_stripe_defaultCardIds" ENABLE ROW LEVEL SECURITY;
