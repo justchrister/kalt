659643904efdabbf53798cf0f8b0432849bf6985
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-06 19:25:00 +0200

    sijmplified

--- server/api/getPaymentCards/webhooks/paymentCards.ts
+++ server/api/getPaymentCards/webhooks/paymentCards.ts
@@ -12,20 +12,20 @@ export default defineEventHandler(async (event) => {
 
   const message = await sub(supabase, topic).entity(body.record.message_entity);
   await sub(supabase, topic).read(service, body.record.message_id);
-
-  const json = await {
-    'userId': message.userId,
-    'lastFourDigits': message.lastFourDigits,
-    'number': message.number,
-    'cardId': message.cardId,
-    'month': message.month,
-    'year': message.year,
-    'default': message.default,
-    'cvc': message.cvc
-  } as any;
+  ok.log('', message)
+  
   const { data, error } = await supabase
     .from(service)
-    .upsert(json)
+    .upsert({
+      'userId': message.userId,
+      'lastFourDigits': message.lastFourDigits,
+      'number': message.number,
+      'cardId': message.cardId,
+      'month': message.month,
+      'year': message.year,
+      'default': message.default,
+      'cvc': message.cvc    
+    })
     .select();
   
   if(data) return data;
