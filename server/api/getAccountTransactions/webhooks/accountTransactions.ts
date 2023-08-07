eee8ffbb1f2c5c13c40e033588fc5d2d58cbbee6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-07 23:15:36 +0200

    (body -> body.record) migrating to oob sb webhooks

--- server/api/getAccountTransactions/webhooks/accountTransactions.ts
+++ server/api/getAccountTransactions/webhooks/accountTransactions.ts
@@ -7,10 +7,10 @@ export default defineEventHandler( async (event) => {
   const service = 'getAccountTransactions';
   const topic = 'accountTransactions';
   const body = await readBody(event);
-  if(body.message_read) return 'message already read';
+  if(body.record.message_read) return 'message already read';
 
-  const message = await sub(supabase, topic).entity(body.message_entity);
-  await sub(supabase, topic).read(service, body.message_id);  
+  const message = await sub(supabase, topic).entity(body.record.message_entity);
+  await sub(supabase, topic).read(service, body.record.message_id);  
 
   const getDistinctTransactions = async () => {
     const { data, error } = await supabase
