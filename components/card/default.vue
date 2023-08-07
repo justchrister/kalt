be8afd8ba719a48899495751da66984683b8fc05
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-08 00:03:31 +0200

    + fallback of sorts

--- components/card/default.vue
+++ components/card/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nuxt-link to="/cards" v-if="data">
+    <nuxt-link to="/cards" v-if="card">
       <card :number="data.number" :default="true" />
     </nuxt-link>
   </div>
@@ -8,11 +8,19 @@
 <script setup>
   const supabase = useSupabaseClient()
   const hasDefaultCard = ref(false)
-  const { data, error } = await supabase
-    .from('getPaymentCardDefault')
-    .select()
-    .limit(1)
-    .single()
+  const getDefaultCard = async () => {
+    const { data } = await supabase
+      .from('getPaymentCardDefault')
+      .select()
+      .limit(1)
+      .single()
+    if(data) {
+      return data
+    } else {
+      return false
+    }
+  }
+  const card = await getDefaultCard();
   if(data) hasDefaultCard.value = true
   if(error) ok.log('error', 'Failed to fetch default card')
 </script>
