ad4f77fd7d5faf04df2aebd890cdf4dc18e4721d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-07 22:27:12 +0200

    Update newsletters.vue

--- components/toggle/newsletters.vue
+++ components/toggle/newsletters.vue
@@ -7,12 +7,21 @@
   const supabase = useSupabaseClient()
   const user = useSupabaseUser()
   const isOn = ref()
-  const { data, error } = await supabase
-    .from('getUser')
-    .select()
-    .limit(1)
-    .single()
-  if(data) isOn.value = data.newsletters;
+  const getPreference = async () => {
+    const { data, error } = await supabase
+      .from('getUser')
+      .select()
+      .limit(1)
+      .single()
+    if(error){
+      ok.log('error', 'Error getting user preferences: ', error)
+      return true
+    } else {
+      ok.log('success', 'Got user preference: ', data.newsletters)
+      return data.newsletters
+    }
+  }
+  isOn.value = await getPreference();
   const toggleValue = async () => {
     if(isOn.value) return false
     else return true
@@ -21,11 +30,17 @@
     const toggledValue = await toggleValue()
     isOn.value = toggledValue
     
-    const { error, data } = await pub(supabase, {
-      sender:'components/toggle/newsletters.vue'
+    const { error } = await pub(supabase, {
+      sender: 'components/toggle/newsletters.vue',
+      entity: user.value.id
     }).userPreferences({
-      "userId": user.value.id,
-      "newsletters": isOn.value
+      userId: user.value.id,
+      newsletters: isOn.value
     });
+    if(error) {
+      ok.log('error', 'Error updating user preferences: ', error)
+    } else {
+      ok.log('success', 'Updated user preferences: ', isOn.value)
+    }
   }
 </script>
