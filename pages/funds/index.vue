556b7f44173c6d2de3f62f864bbc88e0fb84cb33
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-07 02:00:50 +0200

    Create index.vue

---
+++ pages/funds/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+      
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Funds',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Funds',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
