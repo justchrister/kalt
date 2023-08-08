439d69d35df3ff238aeef131bffa180ff27718b7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-08 21:15:57 +0200

    this default card shit is making me go crazy

--- components/card/default.vue
+++ components/card/default.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
-    <nuxt-link to="/cards" v-if="card">
-      <card :number="data.number" :default="true" />
-    </nuxt-link>
-    <nuxt-link to="/cards" v-else>
-      card is missing, add one
+    <nuxt-link to="/cards">
+      <card :number="card.number" :default="true" v-if="card"/>
+      <div class="missing-card" v-else> card is missing, add one</div>
     </nuxt-link>
   </div>
+
 </template>
-<script setup>
+<script setup lang="ts">
   const supabase = useSupabaseClient()
-  const getDefaultCard = async () => {
-    const { error, data } = await supabase
-      .from('getPaymentCardDefault')
-      .select()
-      .limit(1)
-      .single()
-    if(error) {
-      return false
-    } else {
-      return data
-    }
+  const user = useSupabaseUser()
+  const { error, data:card } = await supabase
+    .from('getPaymentCardDefault')
+    .select()
+    .eq('userId', user.value.id)
+    .limit(1)
+    .single()
+  if(error) {
+    ok.log('', 'Failed to get default card', error)
+  } else {
+    ok.log('success', 'Got default card', card)
   }
-  const card = await getDefaultCard();
 </script>
+<style scoped lang="scss">
+  
+</style>
