2e10253a4ebb86572680da146a6331e129c9f01f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-13 05:38:11 +0200

    Update success.vue

--- pages/request/success.vue
+++ pages/request/success.vue
@@ -7,7 +7,7 @@
     </frame-wrap>
     <block margin="half">
       <h1> One small step for you, a big step for mankind. <omoji emoji="✨" /> </h1>
-      <p> You are just one step away from investing in the future of fossil-free energy. Check your email for our invitation. </p>
+      <p> You are just one step away from investing in the future of fossil-free energy. We will contact you once we have had time to review your request. Thank you for yur patience. </p>
     </block>
     <block margin="half">
       <nuxt-link to="/auth" >
