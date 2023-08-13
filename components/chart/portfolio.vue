fa9c56ed6858897b37478cf48018f0bdfac908ae
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-13 05:37:40 +0200

    Create portfolio.vue

---
+++ components/chart/portfolio.vue
@@ -0,0 +1,17 @@
+<template>
+    <chart-base :days="props.days" v-if="datapoints"/>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    days: {
+      type: Number,
+      required: true
+    }
+  })
+  let datapoints = [] as any;
+  let labels = [] as any;
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
