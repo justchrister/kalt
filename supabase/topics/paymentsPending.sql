8adb33b1bd71f4d2afef3367d8fec79be7a0b5c5
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-15 21:47:55 +0200

    REFERENCES sys_currencies(iso)

--- supabase/topics/paymentsPending.sql
+++ supabase/topics/paymentsPending.sql
@@ -10,9 +10,9 @@ CREATE TABLE "topic_paymentsPending" (
     "message_sent"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
     "message_sender"      text                            NOT NULL,
 -- 
-    "userId"             uuid                            NOT NULL,
+    "userId"              uuid                            NOT NULL,
     "amount"              numeric,
-    "currency"            text,
+    "currency"            text                                            REFERENCES sys_currencies(iso),
     "transactionId"       uuid,
     "status"              "paymentsPending_statuses"      NOT NULL,
     "provider"            "paymentProviders"              NOT NULL
