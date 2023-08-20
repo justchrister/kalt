5099d757bed7fca3d42c79f9c9edddfcabe9d926
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-20 19:51:25 +0200

    fund pages

---
+++ pages/funds/art.ddf.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <intro title="" paragraph="" />
+    <block>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'gi.ddf'
+  })
+  useHead({
+    title: 'gi.ddf',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
