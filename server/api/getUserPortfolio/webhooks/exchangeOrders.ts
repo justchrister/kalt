088ddeb4910070917a9c7aff7891d03d76249781
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-21 23:31:49 +0200

    Update exchangeOrders.ts

--- server/api/getUserPortfolio/webhooks/exchangeOrders.ts
+++ server/api/getUserPortfolio/webhooks/exchangeOrders.ts
@@ -11,7 +11,7 @@ export default defineEventHandler( async (event) => {
   if(body.record.message_read) return 'message already read';
   
   const message = await sub(supabase, topic).entity(body.record.message_entity);
-  await sub(supabase, topic).read(service, body.record.message_id);  
+  await sub(supabase, topic).read(service, body.record.message_id);
 
   const date = new Date(message.message_sent);
   const dateFrom = date;
@@ -19,19 +19,36 @@ export default defineEventHandler( async (event) => {
   
   const dateTo = new Date(dateFrom);
   dateTo.setDate(dateTo.getDate() + 1);
-
   const dateFromISOString = dateFrom.toISOString();
   const dateToISOString = dateTo.toISOString();
-  
-  const { data: dailyOrders, error: dailyOrdersError } = await supabase
-    .from('topic_exchangeOrders')
-    .select()
-    .eq('userId', message.userId)
-    .eq('status', 'fulfilled')
-    .eq('ticker', message.ticker)
-    .gte('message_sent', dateFromISOString)
-    .lt('message_sent', dateToISOString)
-  
+
+  const getFulfilledOrders = async () => {
+    const { data, error } = await supabase
+      .from('topic_exchangeOrders')
+      .select()
+      .eq('status', 'fulfilled')
+      .gte('message_sent', dateFromISOString)
+      .lt('message_sent', dateToISOString)
+    if(error){
+      console.log(error);
+      return error;
+    } else {
+      ok.log('',data)
+      return data;
+    }
+  }
+  const getFulfilledEntities = async (entries) => {
+    let entities = [];
+    for (let i = 0; i < entries.length; i++) {
+      const entity = await sub(supabase, topic).entity(entries[i]);
+      entries.push(entity);
+    }
+    return entities
+  }
+  const fulfilledOrders= await getFulfilledOrders();
+  const fulfilledEntities = await getFulfilledEntities(fulfilledOrders);
+  return fulfilledEntities;
+  // get all distinct entities
   const json = {
     'userId': message.userId,
     'date': date,
