4580156093cc778547447fea2a3290d381b17793
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-21 02:49:04 +0200

    Update linkedBankAccounts.ts

--- server/api/getLinkedBankAccounts/webhooks/linkedBankAccounts.ts
+++ server/api/getLinkedBankAccounts/webhooks/linkedBankAccounts.ts
@@ -4,14 +4,14 @@ import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler( async (event) => {
   const supabase = serverSupabaseServiceRole(event);
-  const service = 'getLinkedBankAccounts';
+  const service = 'getLinkedBankAccount';
   const topic = 'linkedBankAccounts';
   const body = await readBody(event);
   if(body.record.message_read) return 'message already read';
 
   const message = await sub(supabase, topic).entity(body.record.message_entity);
   await sub(supabase, topic).read(service, body.record.message_id);
-
+  
   const { data, error } = await supabase
     .from(service)
     .upsert({
