a5aea65fd830532873c5c571e666b0e501d837f0
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-21 02:38:19 +0200

    Update linkedBankAccounts.ts

--- server/api/getLinkedBankAccounts/webhooks/linkedBankAccounts.ts
+++ server/api/getLinkedBankAccounts/webhooks/linkedBankAccounts.ts
@@ -6,19 +6,20 @@ export default defineEventHandler( async (event) => {
   const supabase = serverSupabaseServiceRole(event);
   const service = 'getLinkedBankAccounts';
   const topic = 'linkedBankAccounts';
-  const serviceKebab = ok.camelToKebab(service);
-  const topicKebab = ok.camelToKebab(topic);
-  const query = getQuery(event);
   const body = await readBody(event);
   if(body.record.message_read) return 'message already read';
 
   const message = await sub(supabase, topic).entity(body.record.message_entity);
-  await sub(supabase, topic).read(service, body.record.message_id);  
-  const cleanedMessage = await ok.cleanMessage(message)
+  await sub(supabase, topic).read(service, body.record.message_id);
 
   const { data, error } = await supabase
-    .from(serviceKebab)
-    .upsert(cleanedMessage)
+    .from(service)
+    .upsert({
+      userId: message.userId,
+      iban: message.iban,
+      bankCode: message.bankCode,
+      reference: message.reference
+    })
     .select()
   return { data, error }
 });
