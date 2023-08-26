9302cc4a359c6abba9bf8cc56712e25d9e396ea5
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-26 13:11:39 +0200

    Create invest.vue

---
+++ pages/invest.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+      
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Invest',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Invest',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
