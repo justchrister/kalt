5f2d6daaa769d350e36eab032df44d64c14b1e5b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-28 21:43:40 +0200

    Create index.vue

---
+++ pages/invest/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <block>
+      <h1>
+        Let's invest in the future, today! <omoji emoji="✨" />
+      </h1>
+      <input-amount-buy />
+      <button> Invest </button>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Invest',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Invest',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
