142c2b957c6011ea7e2d6945f76fa6acb619e77e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-08-28 21:43:50 +0200

    Create index.vue

---
+++ components/gallery/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="block" :class="classes">
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+  const props = defineProps({
+    margin: {
+      type: String,
+      required: false
+    },
+    width: {
+      type: String,
+      required: false
+    }
+  })
+  const classes = ref('')
+  if(props.margin){ 
+    classes.value += ' margin-'+props.margin;
+  }
+  if(props.width){ 
+    classes.value += ' width-'+props.width;
+  }
+</script>
+<style scoped lang="scss">
+  .block {
+    width: $sitewidth;
+    max-width: $maxsitewidth;
+    margin:0 auto clamp($unit-min*10, $unit*10, $unit-max*10) auto;
+
+    &.margin-half{
+      margin-bottom: $clamp-5;
+    }
+    &.margin-double{
+      margin-bottom: $clamp-20;
+    }
+    &.margin-3{
+      margin-bottom: $clamp-3;
+    }
+    &.margin-2{
+      margin-bottom: $clamp-2;
+    }
+    &.margin-none{
+      margin-bottom: 0;
+    }
+    &.width-small{
+      max-width: $clamp-35;
+    }
+    
+  }
+</style>
