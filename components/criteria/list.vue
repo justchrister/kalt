5c80a20547eaca52ddc8ffd799b614108b2a6f5a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-02 18:58:59 +0200

    Create list.vue

---
+++ components/criteria/list.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span></span>
+    <span>
+      <slot></slot>
+    </span>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+  div{
+    display:grid; 
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
