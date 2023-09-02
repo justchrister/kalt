31cc4e622a5da59b7ffa09c1e8ccc711f0b67bcc
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-02 20:30:24 +0200

    Create artwork.vue

---
+++ components/artwork.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <div class="image" :style="`background-image: url('${props.image}')`"></div>
+    <p>
+      {{ props.title }} by {{ props.artist }}
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    artist: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+.wrapper{
+  margin:$clamp-2 0 $clamp-10 0;
+}
+.image{
+  width:100%;
+  height:$clamp-35;
+  background-size:contain;
+  background-position:center;
+  
+  background-repeat: no-repeat;
+
+}
+  p{padding-top:$clamp-2;
+    text-align:center;
+  }
+</style>
