de5d3c5e76512113c845b47baa0fac3cf210213e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-02 18:59:01 +0200

    Create index.vue

---
+++ components/criteria/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span>0{{ props.nr }}</span>
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    nr: {
+      type: Number,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+div{
+  display:grid;
+  grid-template-columns: $clamp-3-5 1fr;
+}
+  span{
+    width:$clamp-3;
+    display:inline-block;
+  }
+</style>
