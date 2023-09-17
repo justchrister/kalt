06989c51710ea04f27aaa9edbbbf7a28475af977
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-17 09:42:52 +0200

    Update your.vue

--- components/fund/your.vue
+++ components/fund/your.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="fund">
+  <div class="fund" v-if="data">
     <div class="icon">
       <span id="art">
       </span>
     </div>
     <div class="name">
-      Venture capital fund
+      {{data.name}}
     </div>
     <div class="amount" @click="adjustAmount()">
       <span :class="{ active: amount >= 1}"></span>
@@ -13,26 +13,61 @@
       <span :class="{ active: amount >= 3}"></span>
     </div>
   </div>
-  <button>
-    done
-  </button>
 </template>
 <script setup lang="ts">
+  const supabase = useSupabaseClient()
   const props = defineProps({
     ticker: {
       type: String,
-      required: 
+      required: true
     }
   })
+  const { data, error } = await supabase
+    .from('sys_funds')
+    .select()
+    .eq('ticker', props.ticker)
+    .limit(1)
+    .single()
+  ok.log('', data)
   const amount = ref(0)
-  const adjustAmount = (fund) => {
+
+  const adjustAmount = () => {
     if(amount.value===3){
       amount.value = 0
     } else {
-      amount.value =+ 1
+      amount.value=+1
     }
   }
 </script>
 <style scoped lang="scss">
   
+  .fund{
+    display:grid;
+    grid-template-columns: $clamp-4 1fr $clamp-5;
+    border:$border;
+    padding:$clamp-1;
+    line-height:$clamp-4;
+    margin-bottom:$clamp-1;
+  }
+  .icon{
+    #art{
+      height:$clamp-4;
+      width:$clamp-3;
+      display:block; 
+      background:url('/media/icons/funds/art.svg') no-repeat center center;
+      background-size:contain;
+    }
+  }
+  .amount span{
+    width:$clamp-1;
+    height:$clamp-1;
+    margin-right:$clamp-0-5;
+    display:inline-block;
+    background:url('/omoji/heart-outline.svg') no-repeat center center;
+    background-size:contain;
+  }
+  .amount span.active{
+    background:url('/omoji/heart-filled.svg') no-repeat center center;
+    background-size:contain;
+  }
 </style>
