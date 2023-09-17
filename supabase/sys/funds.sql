a6644b98af308be01791baf8d175dd6d311cac74
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-17 08:20:12 +0200

    Create funds.sql

---
+++ supabase/sys/funds.sql
@@ -0,0 +1,25 @@
+create table public."sys_funds" (
+    "ticker"               tickers                 not null   primary,
+    "name"                 text,
+    "description"          text
+);
+
+
+--- add row level security
+ALTER TABLE "sys_funds" ENABLE ROW LEVEL SECURITY;
+
+insert into public."sys_funds" ("ticker", "name", "description") values 
+  ('gi.ddf', 'Global Index', 'true'),
+  ('art.ddf', 'Art Fund', 'The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with clear set of criteria for picking the artworks and artists we invest in.'),
+  ('ffe.ddf', 'Fossil-free energy', 'The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by'),
+  ('ah.ddf', 'Affordable housing', ''),
+  ('vc.ddf', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.'),
+  ('smb.ddf', 'Small and medium sized businesses', 'The small and medium sized businesses fund invests in small and medium sized businesses with high growth potential, and a clear path to profitability.');
+
+CREATE POLICY "AUTH — Select"
+ON public."sys_funds"
+FOR SELECT 
+TO authenticated 
+USING (
+  true
+);
