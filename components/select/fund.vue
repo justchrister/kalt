182e1f3bf92de2d9de33d6536da8eedd0c4a8791
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-17 19:03:34 +0200

    Create fund.vue

---
+++ components/select/fund.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <block v-for="fund of funds" :key="fund.ticker">
+      <fund :ticker="fund.ticker"/>
+    </block>
+  </main>
+</template>
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+  let funds = [
+    {
+      'ticker': 'your',
+      'name': 'your fund', 
+      'description': 'your fund description'
+    }
+  ]
+  const { data, error } = await supabase
+    .from('sys_funds')
+    .select()
+    .neq('ticker', 'gi.ddf')
+  for (let i = 0; i < data.length; i++) {
+    funds.push(data[i])
+  }
+</script>
+<style scoped lang="scss">
+  
+</style>
