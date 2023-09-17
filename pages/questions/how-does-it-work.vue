c9dab003887c52cec0d72e45e710d25997bc7903
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-17 18:22:21 +0200

    Create how-does-it-work.vue

---
+++ pages/questions/how-does-it-work.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+      
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'How does it work?',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'How does it work?',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
