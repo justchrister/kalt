0de7ab05f9d6a0eb6268fa10b31037d49498ce4a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-17 08:19:45 +0200

    Create your.vue

---
+++ components/fund/your.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="fund">
+    <div class="icon">
+      <span id="art">
+      </span>
+    </div>
+    <div class="name">
+      Venture capital fund
+    </div>
+    <div class="amount" @click="adjustAmount()">
+      <span :class="{ active: amount >= 1}"></span>
+      <span :class="{ active: amount >= 2}"></span>
+      <span :class="{ active: amount >= 3}"></span>
+    </div>
+  </div>
+  <button>
+    done
+  </button>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    ticker: {
+      type: String,
+      required: 
+    }
+  })
+  const amount = ref(0)
+  const adjustAmount = (fund) => {
+    if(amount.value===3){
+      amount.value = 0
+    } else {
+      amount.value =+ 1
+    }
+  }
+</script>
+<style scoped lang="scss">
+  
+</style>
