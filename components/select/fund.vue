bbcf65bb79a2931ff584593c92b07b570ae72c96
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-18 18:41:29 +0200

    Update fund.vue

--- components/select/fund.vue
+++ components/select/fund.vue
@@ -1,27 +1,39 @@
 <template>
-  <main>
-    <block v-for="fund of funds" :key="fund.ticker">
-      <fund :ticker="fund.ticker"/>
-    </block>
-  </main>
+  <section>
+    <div class="icon"></div>
+    <div> Your fund </div>
+    <div class="link"> 
+      <nuxt-link to="funds/your">
+        adjust
+      </nuxt-link>
+    </div>
+  </section>
 </template>
 <script setup lang="ts">
-const supabase = useSupabaseClient()
-  let funds = [
-    {
-      'ticker': 'your',
-      'name': 'your fund', 
-      'description': 'your fund description'
-    }
-  ]
-  const { data, error } = await supabase
-    .from('sys_funds')
-    .select()
-    .neq('ticker', 'gi.ddf')
-  for (let i = 0; i < data.length; i++) {
-    funds.push(data[i])
-  }
 </script>
 <style scoped lang="scss">
+  section{
+    border: $border;
+    padding: $clamp-1-5 $clamp-2 $clamp-1-5 $clamp-1;
+    max-height: $clamp-3;
+    overflow:hidden;
+    display:grid;
+    grid-template-columns: $clamp-3 1fr $clamp-6;
+    line-height:$clamp-2;
+  }
+  .icon{
+    height:$clamp-2;
+    width:$clamp-2;
+    display:block; 
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size:contain;
+    background-image: url('/media/icons/funds/your.svg');
+  }
+  .link{
+    color:$blue;
+    text-align:right;
+
+  }
   
 </style>
