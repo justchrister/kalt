99db8966749dad867d2d5f7a31424a786c9e37c3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-20 19:40:07 +0200

    notification-fixed -> notification

---
+++ components/notification.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrap" :id="props.type">
+    <div class="notify">
+      {{props.message}}
+    </div>
+  </div>
+</template>
+<script setup>
+  const props = defineProps({
+    type: {
+      type: String,
+      required: false
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+  .wrap{
+    position:fixed;
+    width:100vw;
+    bottom:0;
+    padding:$clamp 0;
+    border-top:$border;
+    z-index:2;
+    left: 0;
+    right: 0;
+  }
+  .notify{
+    box-sizing: border-box;
+    width:$sitewidth;
+    max-width: $maxsitewidth; 
+    margin:auto;
+    padding:$clamp 0;
+  }
+  #warn{
+    background: $yellow-20;
+    border-top-color:$yellow;
+  }
+  #success{
+    background:$green-20;
+    border-top-color: $green;
+  }
+  #error{
+    background:$red-20;
+    border-top-color: $red;
+  }
+</style>
