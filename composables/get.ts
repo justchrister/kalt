8663165f58265c7abdd72f461b7be5dc147454d0
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-20 17:44:30 +0200

    GET USER

---
+++ composables/get.ts
@@ -0,0 +1,56 @@
+import { ok } from '~/composables/ok'
+
+export const get = (client: any) => {
+  return {
+    userDefinedFunds: async (userId: any, ticker: any) => {
+      const { data, error } = await client 
+        .from('topic_userDefinedFunds')
+        .select()
+        .eq('message_entity', userId)
+        .eq('ticker', ticker)
+        .order('message_sent', { ascending: true })
+      if(error) {
+        return null
+      } else {
+        return ok.combineJson(data)
+      }
+    },
+    user: async (userId: any) => {
+      const { data:userPreferences } = await client 
+        .from('topic_userPreferences')
+        .select()
+        .eq('message_entity', userId)
+        .order('message_sent', { ascending: true })
+      const { data:userDetails } = await client 
+        .from('topic_userDetails')
+        .select()
+        .eq('message_entity', userId)
+        .order('message_sent', { ascending: true })
+      const userDetailsCombined =ok.combineJson(userDetails);
+      const userPreferencesCombined =ok.combineJson(userPreferences);
+      if(!userDetails || !userPreferences) {
+        return null
+      } else {
+        return {
+          userId: userId,
+          firstName: userDetailsCombined.firstName || null,
+          lastName: userDetailsCombined.lastName || null,
+          country: userDetailsCombined.country || null,
+          city: userDetailsCombined.city || null,
+          postalCode: userDetailsCombined.postalCode || null,
+          birthdate: userDetailsCombined.birthdate || null,
+          addressLine1: userDetailsCombined.addressLine1 || null,
+          addressLine2: userDetailsCombined.addressLine2 || null,
+          autoInvest: userPreferencesCombined.autoInvest || null,
+          newsletters: userPreferencesCombined.newsletters || null,
+          termsOfService: userPreferencesCombined.termsOfService || null,
+          performanceUpdates: userPreferencesCombined.performanceUpdates || null,
+          colorScheme: userPreferencesCombined.colorScheme || null,
+          profilePicture: userDetailsCombined.profilePicture || null,
+          language: userDetailsCombined.language || null,
+          currency: userDetailsCombined.currency || null
+        }
+      }
+    }
+  }
+}
