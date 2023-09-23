e945994231ad3ef5135713d02c988aa8e7fa1d9b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-23 21:01:51 +0200

    Update success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -1,18 +1,15 @@
 <template>
   <main>
-    <div class="star star1">
-      <omoji emoji="✨" />
-    </div>
-    <div class="star star2">
-      <omoji emoji="✨" />
-    </div>
-    <div class="star star3">
-      <omoji emoji="✨" />
-    </div>
     <block>
-      <div class="rotater">
-      </div>
-      <button @click="goBack()"> ← back </button>
+      <div class="success-text">Success</div>
+      <span class="shard top-left"></span>
+      <span class="shard top-center-left"></span>
+      <span class="shard top-center-right"></span>
+      <span class="shard top-right"></span>
+      <span class="shard bottom-right"></span>
+      <span class="shard bottom-center-right"></span>
+      <span class="shard bottom-center-left"></span>
+      <span class="shard bottom-left"></span>
     </block>
   </main>
 </template>
@@ -28,66 +25,271 @@
       content: 'Make money, make a difference.'
     }]
   })
-  /*
+
+  const router = useRouter()
+
   const goBack = () => {
     clearTimeout(timer1)
-    navigateTo('/portfolio')
+    router.go(-2)
   }
-  var timer1 = setTimeout(goBack, 10000)
-  */
+  var timer1 = setTimeout(goBack, 3000)
+  
 </script>
 <style scoped lang="scss">
-  
-  button{
-    background: $blue-80;
-    background: linear-gradient(90deg, $blue-80 0%, $blue-80 50%, $light 50%, $light 100%);
-    background-size: 200% 200%;
-    animation: loading 10s;
-    &:hover{
-      cursor: pointer;
-      background: white;
+  $iteration: 1;
+  $from-sclare: 0.8;
+  $duration: 4s;
+  $base-delay: 700ms;
+  main{
+    margin-top:0;
+    padding-top:45vh;
+    .block{
+      font-size:$clamp-5;
+      text-align:center;
+    }
+    .success-text{
+        transform:scale($from-sclare) translateY($clamp-3);
+        opacity:0.5;
+
+        animation-name: success-text;
+        animation-duration: $duration;
+        animation-delay: $base-delay - 100ms;
+        animation-iteration-count: $iteration;
+    }
+    .shard{
+      position:absolute;
+      width:$clamp-5;
+      height:$clamp-5;
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-position: center;
+      top:40vh;
+      left:50vw;
+      &.top-left{
+        transform:scale($from-sclare) translateX($clamp-3) translateY($clamp-3);
+        animation-name: top-left;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 230ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-n-6;
+        margin-left:$clamp-n-33;
+        background-image:url('/images/shards/shard-top-left.svg');
+      }
+      &.top-center-left{
+        transform:scale($from-sclare) translateX(0) translateY($clamp-3); 
+        animation-name: top-center-left;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 140ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-n-11;
+        margin-left:$clamp-n-14;
+        background-image:url('/images/shards/shard-top-center-left.svg');
+      }
+      &.top-center-right{
+        transform:scale($from-sclare) translateX(0) translateY($clamp-3);
+        animation-name: top-center-right;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 110ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-n-11;
+        margin-left:$clamp-5;
+        background-image:url('/images/shards/shard-top-center-right.svg');
+      }
+      &.top-right{
+        transform:scale($from-sclare) translateX($clamp-n-4) translateY($clamp-2-5);
+        animation-name: top-right;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 270ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-n-4;
+        margin-left:$clamp-14;
+        background-image:url('/images/shards/shard-top-right.svg');
+      }
+      &.bottom-right{
+        transform:scale($from-sclare) translateX($clamp-n-1-5) translateY($clamp-n-3);
+        animation-name: bottom-right;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 210ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-7;
+        margin-left:$clamp-14;
+        background-image:url('/images/shards/shard-bottom-right.svg');
+      }
+      &.bottom-center-right{
+        transform:scale($from-sclare) translateX($clamp-n-1-5) translateY($clamp-n-3);
+        animation-name: bottom-center-right;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 190ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-14;
+        margin-left:$clamp-3;
+        background-image:url('/images/shards/shard-bottom-center-right.svg');
+      }
+      &.bottom-center-left{
+        transform:scale($from-sclare) translateX($clamp-1) translateY($clamp-n-3);
+        animation-name: bottom-center-left;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 130ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-14;
+        margin-left:$clamp-n-15;
+        background-image:url('/images/shards/shard-bottom-center-left.svg');
+      }
+      &.bottom-left{
+        transform:scale($from-sclare) translateX($clamp-4) translateY($clamp-n-3);
+        animation-name: bottom-left;
+        animation-duration: $duration;
+        animation-delay: $base-delay + 170ms;
+        animation-iteration-count: $iteration;
+        margin-top:$clamp-8;
+        margin-left:$clamp-n-32;
+        background-image:url('/images/shards/shard-bottom-left.svg');
+      }
     }
   }
-  @keyframes loading {
-    from {background-position:100% 50%}
-    to {background-position:0% 50%}
+  
+  @keyframes top-left {
+    0% {
+      transform:scale($from-sclare) translateX($clamp-3) translateY($clamp-3);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX($clamp-3) translateY($clamp-3);
+    }
   }
-  .star{
-    position: fixed;
+  
+  @keyframes top-center-left {
+    0% {
+      transform:scale($from-sclare) translateX(0) translateY($clamp-3); 
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX(0) translateY($clamp-3);
+    }
   }
-  .star1{
-    top:20vh;
-    left:20vw;
-    font-size:$clamp-20;
+  
+  @keyframes top-center-right {
+    0% {
+      transform:scale($from-sclare) translateX(0) translateY($clamp-3);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX(0) translateY($clamp-3);
+    }
   }
-  .star2{
-    font-size: $clamp-15;
-    top: 50vh;
-    left: 50vw;
+  
+  @keyframes top-right {
+    0% {
+      transform:scale($from-sclare) translateX($clamp-n-4) translateY($clamp-2-5);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX($clamp-n-4) translateY($clamp-2-5);
+    }
   }
-  .star3{
-    font-size: $clamp-15;
-    top: 10vh;
-    left: 80vw;
+  
+  @keyframes bottom-right {
+    0% {
+      transform:scale($from-sclare) translateX($clamp-n-3) translateY($clamp-n-3);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX($clamp-n-3) translateY($clamp-n-3);
+    }
   }
 
-  .rotater{
-    width:$clamp-20;
-    height:$clamp-20;
-    display:block;
-    border:$border;
-    border-radius:100%;
-    margin: clamp($unit-min*1.4, $unit*1.4, $unit-max*1.4) $clamp;
-    animation-name: example;
-    animation-duration: 9s;
-    animation-iteration-count: 99999;
+    
+  @keyframes bottom-center-right {
+    0% {
+      transform:scale($from-sclare) translateX($clamp-n-1-5) translateY($clamp-n-3);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX($clamp-n-1-5) translateY($clamp-n-3);
+    }
   }
-@keyframes example {
-  0% {
-    transform: scaleX(1) scaleY(1) scaleZ(1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) translateX(0px) translateY(0px) translateZ(0px) skewX(0deg) skewY(0deg);
+
+    
+  @keyframes bottom-center-left {
+    0% {
+      transform:scale($from-sclare) translateX($clamp-1) translateY($clamp-n-3);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX($clamp-1) translateY($clamp-n-3);
+    }
   }
-  100% {
-    transform: scaleX(1) scaleY(1) scaleZ(1) rotateX(177deg) rotateY(180deg) rotateZ(0deg) translateX(0px) translateY(0px) translateZ(0px) skewX(0deg) skewY(0deg);
+
+    
+  @keyframes bottom-left {
+    0% {
+      transform:scale($from-sclare) translateX($clamp-4) translateY($clamp-n-3);
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+    }
+    100% {
+      transform:scale($from-sclare) translateX($clamp-4) translateY($clamp-n-3);
+    }
   }
-}
+  
+  @keyframes success-text{
+    0% {
+      
+
+    transform:scale($from-sclare) translateY($clamp-3);
+        opacity:0.5;
+    }
+    10% {
+      transform:scale(1) translateX(0) translateY(0);
+        opacity:1;
+    }
+    90% {
+      transform:scale(1) translateX(0) translateY(0);
+        opacity:1;
+    }
+    100% {
+
+transform:scale($from-sclare) translateY($clamp-3);
+        opacity:0.5;
+    }
+  }
 </style>
