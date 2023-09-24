9404050afcfdd11a19dee444577c674f562d3e28
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-24 10:41:35 +0200

    Create question.vue

---
+++ components/chat/question.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="question-wrap">
+    <div class="question">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
+<style scoped lang="scss">
+  .question-wrap{
+    display: flex;  // Changed to flex
+    justify-content: flex-end;  // Pushes the child to the right
+    position:relative;
+    width: $clamp-40;
+    min-height:$clamp-6;
+    margin-left: $clamp-18;
+  }
+  .question{
+    display:inline-block;
+    background: $green-20;
+    border: $border;
+    border-color: $green;
+    border-radius: $clamp-0-5;
+    width: auto;
+    max-width: $clamp-40;
+    margin: $clamp-1 0;
+    margin-left: auto;  // Pushes to the right
+    padding: $clamp-1;
+  }
+</style>
