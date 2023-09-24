3a4867626936a4fda571a5defda5f1c69e918dce
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-24 11:57:28 +0200

    Create how-do-you-make-money.vue

---
+++ pages/questions/how-do-you-make-money.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <block>
+      <chat-question>
+        How does Kalt make money?
+      </chat-question>
+      <chat-answer>
+        When you invest, we take a 4% fee. 
+      </chat-answer>
+      <chat-answer>
+        When you make money, we take a 12% fee. 
+      </chat-answer>
+      <chat-link to="/invest">
+        invest now
+      </chat-link>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'How does Kalt make money?'
+  })
+  useHead({
+    title: 'How does Kalt make money?',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
