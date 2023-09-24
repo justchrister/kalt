9a1b2199231b9ff26d751f3aa654c3247a3b1b95
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-24 11:57:34 +0200

    Create what-is-residual-income.vue

---
+++ pages/questions/what-is-residual-income.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <block>
+      <chat-question>
+        What is residual income?
+      </chat-question>
+      <chat-answer>
+        Residual income is money that keeps coming in even after you've done the initial work. 
+      </chat-answer>
+      <chat-answer>
+        Investing in solar energy for instance is a great way to get some of that sweet residual income. You earn money from the electricity it generates.
+      </chat-answer>
+      <chat-answer>
+        And the best part?
+      </chat-answer>
+      <chat-answer>
+        This income keeps coming in for years, giving you a long-term and predictable stream of cash flow. 
+      </chat-answer>
+      <chat-answer>
+        Plus, it's not just good for your wallet, it's good for the planet too. Solar energy is a sustainable and clean source of power that helps reduce our dependence on fossil fuels. And with our platform, it's easy to invest in solar and make a positive impact on the world.
+      </chat-answer>
+      <chat-link to="/invest">
+        invest now
+      </chat-link>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'What is residual income?'
+  })
+  useHead({
+    title: 'What is residual income?',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
