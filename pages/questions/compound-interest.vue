e61e1b41ba5eddbf6d725a9e086b5c4c72f2e700
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-24 11:57:24 +0200

    Create compound-interest.vue

---
+++ pages/questions/compound-interest.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <block>
+      <chat-question>
+        How does compound interest work?
+      </chat-question>
+      <chat-answer>
+        Compound interest is a powerful financial concept that allows your money to grow more and more over time. When you invest, what you earn on that investment is added to the original investment, making it larger. This larger investment then earns even more in the next period, and so on. 
+      </chat-answer>
+      <chat-answer>
+        This process repeats itself, causing the interest earned to become larger and larger, faster and faster. The longer the investment is left to grow, the greater the impact of compound interest will be. This is why we automatically re-invest for you.
+      </chat-answer>
+      <chat-link to="/invest">
+        invest now
+      </chat-link>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Compound interest?'
+  })
+  useHead({
+    title: 'Compound interest?',
+    meta: [{
+      name: 'description',
+      content: 'Make money, make a difference.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
