bf364f3eb97d1a763dc6ddede15d741447197c36
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-24 10:41:52 +0200

    Update how-does-it-work.vue

--- pages/questions/how-does-it-work.vue
+++ pages/questions/how-does-it-work.vue
@@ -1,12 +1,21 @@
 <template>
   <main>
-      
+    <block>
+      <chat-question>
+        How does Kalt work?
+      </chat-question>
+      <chat-answer>
+        U make money
+      </chat-answer>
+      <chat-link to="/invest">
+        invest now
+      </chat-link>
+    </block>
   </main>
 </template>
 <script lang="ts" setup>
   definePageMeta({
-    pagename: 'How does it work?',
-    middleware: 'auth'
+    pagename: 'How does it work?'
   })
   useHead({
     title: 'How does it work?',
