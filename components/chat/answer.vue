c33e3cdbf8d270df55d0dec11ea92502797ba57c
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-24 10:41:29 +0200

    Create answer.vue

---
+++ components/chat/answer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="answer-wrap">
+    <div class="answer">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
+<style scoped lang="scss">
+  .answer-wrap{
+    display: flex;  // Changed to flex
+    justify-content: flex-start;  // Pushes the child to the right
+    position:relative;
+    width: $clamp-40;
+    min-height:$clamp-6;
+    margin-right: $clamp-18;
+  }
+  .answer{
+    display:inline-block;
+    background: $blue-20;
+    border: $border;
+    border-color: $blue;
+    border-radius: $clamp-0-5;
+    width: auto;
+    max-width: $clamp-40;
+    margin: $clamp-1 0;
+    margin-right: auto;  // Pushes to the right
+    padding: $clamp-1;
+  }
+</style>
