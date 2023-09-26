101c30dcec9da991c6cc9d0774ded023d6caaa4f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-09-26 18:05:06 +0200

    minor improvement

--- components/toggle/index.vue
+++ components/toggle/index.vue
@@ -22,29 +22,26 @@ const props = defineProps({
 })
 </script>
 <style scoped lang="scss">
-$clamp:clamp($unit-min*1.8, $unit*1.8, $unit-max*1.8);
 .input-wrapper {
   display: grid; 
   grid-template-columns: 1fr $clamp-12;
-  gap: 2% clamp(calc($unit-min/2), calc($unit/2), calc($unit-max/2));
 }
 .text{
   height:$clamp;
   display:inline-block;
 }
 .toggle-wrap{
-  text-align:right;
+  display: grid; 
+  grid-template-columns: 1fr 1fr;
+  grid-gap:0;
+  &:hover{
+    cursor:pointer;
+  }
   span{
-    margin-left:$clamp-2;
+    margin:0;
+    width:$clamp-5;
+    text-align:center;
     border:$border;
-    padding:0 $clamp-0-5;
-    &#on:hover,
-    &#off:hover{
-      cursor:pointer;
-      background:white;
-      border: $border;
-      border-color:$dark;
-    }
   }
   #on{ border-color:transparent; }
   #off{ border-color:$dark; }
@@ -52,23 +49,5 @@ $clamp:clamp($unit-min*1.8, $unit*1.8, $unit-max*1.8);
     #on{ border-color:$dark; }
     #off{ border-color:transparent; }
   }
-}/*
-.dark-mode{
-  .input-wrapper:hover{
-    .toggle-wrap{
-      border:1px solid $light;
-      background:black;
-    }
-    .toggle{
-      background:$light;
-    }
-  }
-  .toggle{
-    background:$light;
-    border-color:$dark;
-  }
-  .toggle-wrap{
-    border-color:$light;
-  }
-}*/
+}
 </style>
