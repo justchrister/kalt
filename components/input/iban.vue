a0768857c04cd9757e812f6f46da1d08f6576919
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-02 23:52:03 +0200

    Update iban.vue

--- components/input/iban.vue
+++ components/input/iban.vue
@@ -6,7 +6,7 @@
 </template>
 <script setup>
   const supabase = useSupabaseClient()
-  const user = useSupabaseUser()
+  const userId = useSupabaseUser()
   const props = defineProps({
     initialValue: {
       type: String,
@@ -21,10 +21,10 @@
 
   const updateIBAN = async () => {
     const { error, data } = await pub(supabase, {
-      entity: user.value.id,
+      entity: userId.value.id,
       sender:'components/input/iban.vue'
     }).linkedBankAccounts({
-      userId: user.value.id,
+      userId: userId.value.id,
       iban: iban.value
     });
     if(error) {
