9bfcc020fe073160a6e7e1a2b04d70d655621d5f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-05 13:26:55 +0200

    Create request.vue

---
+++ pages/success/request.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <main>
+    <frame-wrap>
+      <frame-image src="/media/images/audun-alvestad/waiting-room.png" />
+      <frame-image src="/media/images/apollo-celebration.jpg" />
+    </frame-wrap>
+    <block margin="half">
+      <h1> One small step for you, a big step for mankind. <omoji emoji="✨" /> </h1>
+      <p> You are just one step away from investing in the future of fossil-free energy. We will contact you once we have had time to review your request. Thank you for yur patience. </p>
+    </block>
+    <block margin="half">
+      <nuxt-link to="/auth" >
+          <button tabindex="-1">sign in</button>
+      </nuxt-link>
+    </block>
+  </main>
+</template>
+<script setup>
+  definePageMeta({
+    pagename: 'Lobby'
+  })
+  useHead({
+    title: 'Lobby'
+  })
+</script>
