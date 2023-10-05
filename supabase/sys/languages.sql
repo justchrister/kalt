468c37356bf87dd895e497f0acb60633027010b3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-05 14:22:47 +0200

    Update languages.sql

--- supabase/sys/languages.sql
+++ supabase/sys/languages.sql
@@ -14,9 +14,13 @@ insert into public."sys_languages" (iso, name, available) values
   ('GER', 'German', true);
 
 CREATE POLICY "AUTH — Select"
-ON public."sys_languages"
-FOR SELECT 
-TO authenticated 
-USING (
-  true
-);
+  ON public."sys_languages"
+  FOR SELECT 
+  TO authenticated 
+  USING (true);
+
+CREATE POLICY "PUBLIC — Select"
+  ON public."sys_languages"
+  AS PERMISSIVE FOR SELECT
+  TO public
+  WITH CHECK (true);
