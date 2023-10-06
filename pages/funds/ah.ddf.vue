9b35525510a8eb5d77da721d83bf0e4cfcd2b754
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-06 14:43:03 +0200

    Update ah.ddf.vue

--- pages/funds/ah.ddf.vue
+++ pages/funds/ah.ddf.vue
@@ -3,27 +3,36 @@
     <block margin="half">
       <intro-fund ticker="art" text="The fund strategically allocates capital in development of affordable housing around the world. By doing so, we yield a stable return on the investments, but also contributes to social good by combating housing inequality."/>
     </block>
-    <block>
+    <block margin="2">
       <criteria-list>
         <criteria :nr="1">
-          The artist has a strong expression, and is aesthetical.
+          The project should be in a location with demand for affordable housing, supported by demographic and economic data.
         </criteria>
         <criteria :nr="2">
-          The artist has already exhibited in more than 3 countries.
+          The project must hacve clear indicators that it is likely to provide a stable or growing yield.
         </criteria>
         <criteria :nr="3">
-          The artist is represented by a gallery that has a reputation for quality.
+          Must have measurable positive impact the community and include communal social spaces for residents to socialize and engage.
         </criteria>
-        <criteria :nr="3">
-          The art has seen a 15%+ average increase in value.
+        <criteria :nr="4">
+          Should include communal social spaces for residents to socialize and engage.
+        </criteria>
+        <criteria :nr="5">
+          Preference is given to projects that aligning with our broader sustainability goals.
         </criteria>
       </criteria-list>
     </block>
-    <block>
-      By investing in the art fund you can own a piece of all our artworks, and enjoy the benefits of a diversified portfolio of art.
-      <br><br>
-      <button @click="navigateTo('/invest')"> invest now -> </button>
+    <block margin="2">
+      The Affordable Housing Fund by Kalt strategically allocates capital to development projects around the world. Our focus is fourfold: invest in areas with high demand for affordable housing, seek projects that offer stable or growing yields, prioritize eco-friendly initiatives, and aim for a strong social impact that includes communal spaces for community engagement. By aligning with these criteria, we not only secure a stable return on investments but also contribute to social good by combating housing inequality.
     </block>
+    <block>
+      <div class="button-group">
+        <button @click="navigateTo('/subscription')"> auto investments -> </button>
+        <button @click="navigateTo('/invest')"> invest now -> </button>
+      </div>
+      <select-fund />
+    </block>
+    <!-- insert map here -->
   </main>
 </template>
 <script lang="ts" setup>
@@ -40,5 +49,9 @@
   
 </script>
 <style scoped lang="scss">
-
+  .button-group{
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: $clamp-1;
+  }
 </style>
