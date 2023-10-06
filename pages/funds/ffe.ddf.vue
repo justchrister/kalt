ff27ac2c15b61b4ad3b3d80d6d57f8c8ed0df6a8
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-06 12:14:43 +0200

    Update ffe.ddf.vue

--- pages/funds/ffe.ddf.vue
+++ pages/funds/ffe.ddf.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <block margin="half">
-      <intro-fund ticker="ffe" text="The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by "/>
+      <intro-fund ticker="ffe" text="Our goal is to accelerate the global transition to sustainable energy sources, fostering a better future for our planet. "/>
     </block>
     <block>
       <criteria-list>
@@ -17,7 +17,7 @@
       </criteria-list>
     </block>
     <block>
-      By investing in the Fossil-free energy you can own a piece of all our artworks, and enjoy the benefits of a diversified portfolio of art.
+      By investing with us, you directly contribute to the development and deployment of fossil-free energy alternatives. Together, we can create a more sustainable world while enjoying the financial benefits of supporting fossil-free energy solutions.
       <br><br>
       <button @click="navigateTo('/invest')"> invest now -> </button>
     </block>
