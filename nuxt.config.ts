104acc1788bb02a4162ebfa7da1e3272f3d14f75
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-06 15:02:16 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,10 +38,21 @@ export default defineNuxtConfig({
         { rel: 'manifest', type: "image/x-icon", href: '/manifest.webmanifest' }
       ],
       meta: [
-        { name: 'description', content: 'Make money, make a difference.' },
+        { 
+          name: 'description', 
+          content: 'Make money, make a difference.' 
+        },
         {
           name: "viewport",
           content: "width=device-width, initial-scale=1, maximum-scale=1"
+        },
+        { 
+          name: 'og:image',
+          content: 'https://ka.lt/images/meta.png' 
+        },
+        { 
+          name: 'twitter:image',
+          content: 'https://ka.lt/images/meta.png' 
         }
       ],
     }
