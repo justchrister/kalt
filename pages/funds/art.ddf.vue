9109affea450d910ab90f4ae65e588a0e36191ae
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-08 00:05:58 +0200

    Update art.ddf.vue

--- pages/funds/art.ddf.vue
+++ pages/funds/art.ddf.vue
@@ -15,21 +15,31 @@
           The artist is represented by a gallery that has a reputation for quality.
         </criteria>
         <criteria :nr="3">
-          The art has seen a 15%+ average increase in value.
+          Comparitive art has seen a 15%+ average increase in value.
         </criteria>
       </criteria-list>
     </block>
+    <block margin="2">
+      <h2> Our thesis </h2>
+      <p>Art is not just an emotional investment, it's a financial one too. Pieces by iconic artists like Monet or Picasso are rare, adding a built-in value that stands the test of time. In an era where wealth is growing for the affluent, their appetite for tangible and meaningful assets like art is becoming increasingly clear.</p>
+      <p>Art offers something unique: it's not only a financial asset but also a status symbol and a source of emotional enrichment. The growing demand from wealthy investors is a key factor that's likely to push the value of art even higher.</p>
+      <p>Including art in your investment portfolio is not just a wise choice, but a forward-thinking strategy. Unlike traditional financial assets like stocks and bonds, art adds a dimension of tangible ownership and emotional engagement that's unmatched.</p>
+      <p>By investing in the art fund you can own a piece of all the artworks in our portfolio, and enjoy the benefits of a diversified portfolio of art.</p>
+    </block>
     <block>
-      By investing in the art fund you can own a piece of all our artworks, and enjoy the benefits of a diversified portfolio of art.
-      <br><br>
       <button @click="navigateTo('/invest')"> invest now -> </button>
     </block>
     <block>
-      <h1>the art</h1>
+      <h2>some of the art</h2>
       <artwork 
         title="All colored cast, part II"
         artist="Jean-Michel Basquiat"
-        image="/art/all-coloured-mockup.webp"
+        image="/art/jean-michel-basquiat/all-coloured-cast-ii-mockup.webp"
+      />
+      <artwork 
+        title="Untitled"
+        artist="Nicolas Party"
+        image="/art/nicolas-party/untitled-mockup.webp"
       />
     </block>
   </main>
