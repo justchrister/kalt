ac735696b8c9b821ab9e0b6b739e08731d766f5c
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-07 21:07:10 +0200

    Update register-revenue.vue

--- pages/hq/register-revenue.vue
+++ pages/hq/register-revenue.vue
@@ -33,7 +33,7 @@
     title: 'Register revenue',
     meta: [{
       name: 'description',
-      content: 'Make money, make a difference.'
+      content: 'Invest in the future, today.'
     }]
   })
   const supabase = useSupabaseClient();
