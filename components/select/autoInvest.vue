4c67f2543c8ce679ceb16159c5c0630d2ce23613
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-08 19:08:07 +0200

    combine user preferences and details tables

--- components/select/autoInvest.vue
+++ components/select/autoInvest.vue
@@ -21,7 +21,7 @@
     pub(supabase, {
       sender:'components/select/autoInvest.vue',
       entity: userId.value.id
-    }).user({
+    }).users({
       userId: userId.value.id,
       autoInvest: autoInvest
     });
