7d30656ca7c55aaa027bdb51b455face1053bccb
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-08 19:07:47 +0200

    combine user preferences and details tables

--- components/toggle/termsOfService.vue
+++ components/toggle/termsOfService.vue
@@ -19,7 +19,7 @@
     const { error } = await pub(supabase, {
       sender:'components/toggle/termsOfService.vue',
       entity: userId.value.id
-    }).user({
+    }).users({
       userId: userId.value.id,
       termsOfService: isOn.value
     });
