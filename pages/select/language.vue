eb972f2e6c4e503af9af866c0e2e6746defbe8f6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-08 19:07:53 +0200

    combine user preferences and details tables

--- pages/select/language.vue
+++ pages/select/language.vue
@@ -34,7 +34,7 @@
     const { error, data } = await pub(supabase, {
       sender:"pages/select/language.vue",
       entity: userId.value.id
-    }).user({
+    }).users({
       userId: userId.value.id,
       language: iso
     });
