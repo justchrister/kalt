2f140639ba05af44281943e01d5dacfefeee30bb
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-08 19:06:17 +0200

    Update default.vue

--- components/card/default.vue
+++ components/card/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nuxt-link to="/cards" v-if="defaultCard">
-      <card :number="defaultCard" :default="true"/>
+      <card :number="defaultCard.number" :default="true"/>
     </nuxt-link>
   </div>
 
@@ -9,29 +9,8 @@
 <script setup lang="ts">
   const supabase = useSupabaseClient()
   const userId = useSupabaseUser()
-  const getPaymentCardDefault = async () => {
-    const { data, error } = await supabase
-      .from('getPaymentCardDefault')
-      .select()
-      .eq('userId', userId.value.id)
-      .limit(1)
-      .single()
-    if(data) {
-      ok.log('', 'Got default card: •••• •••• •••• '+data.lastFourDigits)
-      if(data.number){
-        return data.number
-      } else {
-        ok.log('', 'Failed to get default card',data)
-        return false
-      }
-    }
-    if(error) {
-      ok.log('', 'Failed to get default card')
-      ok.log('', error)
-      return false
-    }
-  }
-  const defaultCard = await getPaymentCardDefault();
+  const user = await get(supabase).user(userId.value.id)
+  const defaultCard = await get(supabase).defaultPaymentCard(user);
   if(!defaultCard) {
     ok.log('error', 'User does not have default card')
     await navigateTo('/cards')
