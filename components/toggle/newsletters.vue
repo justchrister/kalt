b23f6a27df7c44e31b6ddd1b38ec91cba7cacf88
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-08 18:49:15 +0200

    combine user details and preferences

--- components/toggle/newsletters.vue
+++ components/toggle/newsletters.vue
@@ -21,7 +21,7 @@
     const { error } = await pub(supabase, {
       sender: 'components/toggle/newsletters.vue',
       entity: userId.value.id
-    }).userPreferences({
+    }).user({
       userId: userId.value.id,
       newsletters: isOn.value
     });
