28a20e1b2e84732cc620f78accbf946d78d22b7d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-26 21:33:21 +0200

    Create bannerNotification.vue

---
+++ components/bannerNotification.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrap-notification" :id="props.color">
+    {{ props.message }} <span id="x">x</span>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    message: {
+      type: String,
+      required: true
+    },
+    color:{
+      type: String,
+      required: false
+    }
+  })
+</script>
+<style scoped lang="scss">
+  
+  .wrap-notification{
+    position:fixed;
+    bottom:$clamp-4;
+    z-index:8999;
+    box-sizing: border-box;
+    width:$sitewidth;
+    max-width: $maxsitewidth; 
+    margin:auto;
+    padding:$clamp $clamp-2;
+  }
+  #yellow{
+    background: $yellow-20;
+    border-color:$yellow;
+  }
+  #green{
+    background:$green-20;
+    border-color: $green;
+  }
+  #red{
+    background:$red-20;
+    border-color: $red;
+  }
+  #x{
+    float:right;
+    cursor:pointer;
+  }
+</style>
