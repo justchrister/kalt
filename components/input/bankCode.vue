2ee4333f434c16d2bd6c3e5522990dc940d15d56
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-29 01:17:58 +0200

    Update bankCode.vue

--- components/input/bankCode.vue
+++ components/input/bankCode.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="input-wrap">
     <label for="bankCode">Bank code (swift/bic)</label>
-    <input type="text" v-model="bankCode" v-maska data-maska="@@@@ @@ ** ***" @input="updateBankCode()" placeholder="KALT NO 11 234"/>
+    <input type="text" 
+           v-model="bankCode" 
+           v-maska data-maska="@@@@ @@ ** ***" 
+           @input="updateBankCode()" 
+           placeholder="KALT NO 11 234" 
+           :class="state"/>
   </div>
 </template>
 <script setup>
@@ -14,11 +19,13 @@
       default: null
     }
   })
+  const state = ref('')
   const bankCode = ref(props.initialValue)
   if(props.initialValue) {
     ok.log('success', 'initial value: '+props.initialValue)
   }
   const updateBankCode = async () => {
+    state.value = 'loading'
     const { error, data } = await pub(supabase, {
       entity: userId.value.id,
       sender:'components/input/bankCode.vue'
@@ -27,8 +34,10 @@
       bankCode: bankCode.value
     });
     if(error) {
+      state.value = 'error'
       ok.log('error', 'error updating bank code', error)
     } else{
+      state.value = 'success'
       ok.log('success', 'updated bank code: '+bankCode.value)
     }
   }
