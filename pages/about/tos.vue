6f50d0b08c7c19bdd8bdc448c527d97b2a2c7313
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:44:11 +0200

    Update tos.vue

--- pages/about/tos.vue
+++ pages/about/tos.vue
@@ -2,41 +2,41 @@
   <main>
     <block margin="3">
       <h1> Terms of service </h1>
-      <p>At Kalt LLC, we value.</p>
+      <p>At Kalt LLC, we value transparency.</p>
     </block>
     <block margin="3">
       <h2>Introduction</h2>
       <p>By using our website and services ("Platform"), you agree to these terms of use ("Terms"). These Terms apply to everyone who uses the Platform, whether you're a registered member or just browsing.</p>
       <p><strong>Important: Read these Terms carefully. They include a clause that requires binding arbitration for resolving disputes.</strong></p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Updates to these terms</h2>
       <p>We may update these Terms at any time. If we do, we'll notify you on the Platform. Your continued use of the Platform means you agree to the updated Terms.</p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Who can use our platform</h2>
       <p>You must be at least 18 years old to use our Platform. By using it, you confirm that you meet this age requirement.</p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Ownership and use of content</h2>
       <p>All content on the Platform is our property. You're granted a limited license to use it for personal purposes, as long as you don't remove any copyright notices.</p>
       <p>Everything you see on our Platform is owned by us or used with permission. You can use it for personal, non-commercial purposes. Any other use is not allowed without our written consent.</p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Your responsibilities</h2>
       <p>-> You won't post any content that violates someone else's rights or breaks the law.</p>
       <p>-> You won't use the Platform to send spam or scam others.</p>
       <p>-> You're responsible for all activity on your account.</p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Privacy</h2>
       <p>We collect and process your data to provide our services. We don't share this data with third parties. For data deletion, email us at <a href="mailto:gdpr@ka.lt">gdpr@ka.lt</a>.</p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Arbitration clause</h2>
       <p>Disputes between you and Kalt LLC will be resolved by binding arbitration on an individual basis, rather than in court.</p>
     </block>
-    <block>
+    <block margin="half">
       <h2>Limitation of liability</h2>
       <p>Kalt LLC is not liable for any damages that result from your use or inability to use the Platform.</p>
     </block>
