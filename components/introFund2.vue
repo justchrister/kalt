9a0d91253ffae1e385d684302e27d7191c4163db
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:24:49 +0200

    Create introFund2.vue

---
+++ components/introFund2.vue
@@ -0,0 +1,51 @@
+<template>
+  <section :class="props.ticker">
+    <h1>{{ props.text }}</h1>
+  </section>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    text: {
+      type: String,
+      required: true
+    },
+    ticker: {
+      type: String,
+      required: false
+    }
+  })
+</script>
+<style scoped lang="scss">
+  section{
+    background-color:$test-color-10;
+    padding: $clamp-1-5 $clamp-2;
+    border:$border;
+    border-radius:$clamp-1;
+    &:before{
+      content:'';
+      display:block;
+      width:$clamp-60;
+      height:$clamp-70;
+      top:0;
+      transform:translateX(-66%);
+      position:absolute;
+      z-index:-1;
+    }
+  }
+  section.ffe:before{
+    background:url('/ffe-bg-elem.svg') no-repeat;
+    background-size:contain;
+    background-position:right;
+  }
+  section.art:before{
+    background:url('/art-bg-elem.svg') no-repeat;
+    background-size:contain;
+    background-position:right;
+  }
+  h1{
+    font-size: $clamp-2-5;
+    margin:0;
+    padding:0;
+  }
+
+</style>
