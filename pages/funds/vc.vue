d7fde803df0b3275e29eb1e4dd45dee5f8b5efaa
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:25:04 +0200

    Update vc.vue

--- pages/funds/vc.vue
+++ pages/funds/vc.vue
@@ -1,34 +1,57 @@
 <template>
   <main>
-    <block margin="half">
-      <intro-fund ticker="ffe" text="The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by "/>
-    </block>
+    <intro title="Invests in early stage startups, with decreased risk." text=""/>
     <block>
-      <criteria-list>
+      <criteria-list2>
         <criteria :nr="1">
-          The geographical area must have high GDP growth potential
+          A feasible path to profitability.
         </criteria>
         <criteria :nr="2">
-          The geographical area must have high energy generation potential
+          An evaluation of the qualifications and experience of the venture's founding team.
         </criteria>
         <criteria :nr="3">
-          The country must have moderate to low political risk
+          It must incorporate innovative technology, application, or business practices.
         </criteria>
-      </criteria-list>
+        <criteria :nr="4">
+          Ventures must align closely with our overarching objectives and mission.
+        </criteria>
+        <criteria :nr="5">
+          Consideration for ventures with a strong focus on sustainability and responsible practices.
+        </criteria>
+        <criteria :nr="6">
+          An analysis of the market, growth potential, and target audience for the venture.
+        </criteria>
+        <criteria :nr="7">
+          Ventures should adhere to ethical and responsible business conduct.
+        </criteria>
+      </criteria-list2>
     </block>
-    <block>
-      By investing in the Fossil-free energy you can own a piece of all our artworks, and enjoy the benefits of a diversified portfolio of art.
+    <block margin="2">
+      <h2> Our thesis </h2>
+      <p>
+        In the dynamic world of venture capital, we operate at the speed of opportunity, embracing ventures that epitomize innovation, sustainability, and profitability. Central to our investment philosophy is the belief in the catalytic power of innovation. We actively seek ventures that chart a feasible path to profitability, underpinned by pioneering technology, forward-thinking applications of technology, or groundbreaking business practices. The caliber of the founding team, with their qualifications and experience, remains pivotal in shaping a venture's potential for success.
+      </p>
+      <p>
+        In our pursuit of impact-driven investments, alignment with our overarching objectives and mission is non-negotiable. We place a premium on ventures that exhibit a strong focus on sustainability and responsible practices, recognizing their capacity to create lasting change. Our thesis hinges on agile investment, assessing market dynamics, growth potential, and target audience analysis at the speed of opportunity. These criteria reflect our commitment to fostering innovation, sustainability, and ethical responsibility in the ventures we support. Together, we aim to not only fuel growth but also drive meaningful change, shaping a future where innovation, sustainability, and profitability coalesce.
+      </p>
       <br><br>
       <button @click="navigateTo('/invest')"> invest now -> </button>
+    </block>
+    <block margin="2">
+      <h2> Ventures </h2>
+      <venture-list>
+        <venture-shield id="tillit"/>
+        <venture-shield id="venture with really loong name "/>
+      </venture-list>
     </block>
   </main>
 </template>
 <script lang="ts" setup>
   definePageMeta({
-    pagename: 'Fossil-free energy'
+    pagename: 'Venture capital'
   })
   useHead({
-    title: 'Fossil-free energy',
+    title: 'Venture capital',
     meta: [{
       name: 'description',
       content: 'Invest in the future, today.'
@@ -37,5 +60,10 @@
   
 </script>
 <style scoped lang="scss">
-
+  main{
+    padding-top:0;
+  }
+  button{
+    background:$test-color-5;
+  }
 </style>
