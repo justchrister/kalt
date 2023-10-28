a677bdd753272e56cae4278786b0de5c37bf0f84
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:24:54 +0200

    Create ventureShield.vue

---
+++ components/ventureShield.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :id="props.id">{{ props.id }}</div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    id: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+  div{
+    width:$clamp-10;
+    text-align:center;
+    display:inline-block;
+  }
+</style>
