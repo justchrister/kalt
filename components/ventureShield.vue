6431f3a49ef23de2707379756ac020eb2d5ef9fe
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:37:33 +0200

    Update ventureShield.vue

--- components/ventureShield.vue
+++ components/ventureShield.vue
@@ -1,5 +1,8 @@
 <template>
-  <div :id="props.id">{{ props.id }}</div>
+  <div :id="props.id">
+    <div class="logo"></div>
+    <div class="name">{{ props.id }}</div>
+  </div>
 </template>
 <script setup lang="ts">
   const props = defineProps({
@@ -14,5 +17,22 @@
     width:$clamp-10;
     text-align:center;
     display:inline-block;
+  }
+  .logo{
+    background-position: center;
+    background-size:contain;
+    background-repeat:no-repeat;
+    height:$clamp-6;
+    width:$clamp-6;
+    margin:0 $clamp-2;
+  }
+  #tillit .logo{
+    background-image:url('/ventures/tillit.svg');
+  }
+  #empress .logo{
+    background-image:url('/ventures/empress.png');
+  }
+  .name{
+    font-size:$clamp-1;
   }
 </style>
