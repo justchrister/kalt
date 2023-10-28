34afdc9e266df58f4113cc0bc7cbbc3e603399f7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:24:52 +0200

    Create ventureList.vue

---
+++ components/ventureList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+  div{
+    display:grid; 
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: $clamp-1;
+    gap: $clamp-1;
+  }
+</style>
