6c2e9d4bf84928df79b3970c8ce062632a879d7b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-28 03:24:44 +0200

    Create list2.vue

---
+++ components/criteria/list2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <strong>criteria</strong>
+    <span>
+      <slot></slot>
+    </span>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+  div{
+    display:grid; 
+    grid-template-columns: 1fr 2fr;
+  }
+</style>
