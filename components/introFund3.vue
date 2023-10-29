13304ae0d11a196d96496aa8d6b4be2aa2136fa4
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-10-29 22:43:00 +0100

    Create introFund3.vue

---
+++ components/introFund3.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="intro">
+    <block margin="none">
+      <h1 class="sans-serif no-margin">{{props.title}}</h1>
+    </block>
+  </div>
+</template>
+<script setup>
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+<style scoped lang="scss">
+.intro{
+  padding:$clamp-9 0 $clamp-7 0;
+  margin-bottom:$clamp-5;
+  background:url('/gradient-2.jpg') no-repeat center center;
+  background-size:cover;
+  h1{
+    margin:0;
+    color:$dark-90;
+  }
+  p{
+    line-height:1.5;
+    padding-top:$clamp-1-5;
+  }
+}
+</style>
