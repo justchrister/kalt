412e98f8275224c2de4b7327d071a4079a3a3200
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-11-07 01:31:23 +0100

    Update autoInvest.sql

--- supabase/sub/accountTransactions/autoInvest.sql
+++ supabase/sub/accountTransactions/autoInvest.sql
@@ -1,9 +1,9 @@
 -- version 29.7.23
--- service autoInvest
+-- service autoVest
 -- topic   accountTransactions
 
 --- create the table, with default values
-CREATE TABLE "sub_accountTransactions_autoInvest" (
+CREATE TABLE "sub_accountTransactions_autoVest" (
     "message_id"          uuid          NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "message_entity"      uuid          NOT NULL  DEFAULT uuid_generate_v4(),
     "message_sent"        timestamptz   NOT NULL  DEFAULT (now() at time zone 'utc'),
@@ -12,20 +12,20 @@ CREATE TABLE "sub_accountTransactions_autoInvest" (
 );
 
 -- Create the replicate function 
-CREATE OR REPLACE FUNCTION "replicate_accountTransactions_autoInvest"()
+CREATE OR REPLACE FUNCTION "replicate_accountTransactions_autoVest"()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO "sub_accountTransactions_autoInvest" (message_id, message_entity, message_sender, message_sent)
+  INSERT INTO "sub_accountTransactions_autoVest" (message_id, message_entity, message_sender, message_sent)
   VALUES (NEW.message_id, NEW.message_entity, NEW.message_sender, NEW.message_sent);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create replicate trigger
-CREATE TRIGGER "replicate_accountTransactions_autoInvest"
+CREATE TRIGGER "replicate_accountTransactions_autoVest"
 AFTER INSERT ON "topic_accountTransactions"
 FOR EACH ROW
-EXECUTE FUNCTION "replicate_accountTransactions_autoInvest"();
+EXECUTE FUNCTION "replicate_accountTransactions_autoVest"();
 
 -- Enable RLS
-ALTER TABLE "sub_accountTransactions_autoInvest" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "sub_accountTransactions_autoVest" ENABLE ROW LEVEL SECURITY;
