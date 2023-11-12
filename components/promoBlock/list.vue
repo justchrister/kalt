e61bd54f2409b55c75acac6d2507a4135d63c6a6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-11-12 17:34:40 +0100

    Create list.vue

---
+++ components/promoBlock/list.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrap">
+    <slot>
+    </slot>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+.wrap{
+  display:grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap:$clamp-2;
+  width: $sitewidth;
+  max-width: $maxsitewidth*1.2;
+  margin:0 auto;
+  padding:$clamp-5 0;
+}
+</style>
