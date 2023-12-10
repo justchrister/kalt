7f05d370d1db0e791f9c1a5b04c03aeb67f77218
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-10 21:33:31 +0100

    Create art.vue

---
+++ pages/hq/classify/art.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <block>
+      <h1>Classify artworks</h1>
+    <hq-classify-artwork 
+      v-for="artwork of artworks" 
+      :key="artwork.id" 
+      :data="artwork.data"/>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Classify: Art',
+    middleware: 'auth-hq'
+  })
+  useHead({
+    title: 'Classify: Art',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const supabase = useSupabaseClient()
+  const artworks = await get(supabase).data('artwork');
+</script>
+<style scoped lang="scss">
+
+</style>
