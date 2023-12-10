05504717a084f9d3e97fccc09e276d907913aacc
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-10 21:27:20 +0100

    Create artwork.vue

---
+++ components/hq/classify/artwork.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="image">
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    data: {
+      type: String,
+      required: true
+    }
+  })
+  console.log(props.data)
+</script>
+<style scoped lang="scss">
+  
+</style>
