4be769425869cc13213ba7bedf111a37df8f5a0a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-13 00:20:01 +0100

    Update auto.vue

--- pages/invest/auto.vue
+++ pages/invest/auto.vue
@@ -1,15 +1,14 @@
 
 <template>
   <main>
-    <navbar-tabs />
+    <intro title="Build your own fund" paragraph="Choose what you want to invest in both through your subscription and when completing single investments." />
     <block margin="1">
       <input-amount-subscription :amount="subscription.amount || 0"/>
     </block>
     <block margin="2">
-      <intervalSelector type="daily" />
-      <intervalSelector type="weekly" />
-      <intervalSelector type="monthly" />
-      
+      <intervalSelector type="daily" :selected="dailySelected" @click="select('daily')"/>
+      <intervalSelector type="weekly" :selected="weeklySelected" @click="select('weekly')"/>
+      <intervalSelector type="monthly" :selected="monthlySelected" @click="select('monthly')"/>
     </block>
     <block margin="1">
       <select-fund />
@@ -34,7 +33,27 @@
   useHead({
     title: 'Subscription'
   })
+  
+  const dailySelected = ref(true);
+  const weeklySelected = ref(false);
+  const monthlySelected = ref(false);
 
+  const select = (iteration) => {
+    ok.log('', iteration)
+    if(iteration==='daily'){
+      dailySelected.value = true;
+      weeklySelected.value = false;
+      monthlySelected.value = false;
+    } else if (iteration==='weekly'){
+      dailySelected.value = false;
+      weeklySelected.value = true;
+      monthlySelected.value = false;
+    } else if (iteration==='monthly'){
+      dailySelected.value = false;
+      weeklySelected.value = false;
+      monthlySelected.value = true;
+    }
+  }
   const defaultCard = await get(supabase).defaultPaymentCard(user);
   if(!defaultCard){
     ok.log('error', 'User does not have default card')
@@ -45,4 +64,7 @@
   
 </script>
 <style scoped lang="scss">
+main{
+  padding-top:0;
+}
 </style>
