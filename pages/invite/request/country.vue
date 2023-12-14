97ade4239a5132af645e7433c0871813bd764814
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-14 08:53:23 +0100

    Create country.vue

---
+++ pages/invite/request/country.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <block>
+      <h1>Where do you live?</h1>
+      <input type="text" placeholder="Country" v-model="country" class="next"/>
+      <button class="next" @click="requestInvite()"> Next -> </button>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Request invite'
+  })
+  useHead({
+    title: 'Request invite',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const country = ref('')
+  const supabase = useSupabaseClient()
+  const requestUuid = useCookie('requestUuid')
+
+  const requestInvite = async () => {
+    
+    const { error, data } = await pub(supabase, {
+      "sender":"pages/invite/request/index.vue",
+      "entity": requestUuid.value
+    }).requestAccess({
+      country: country.value,
+    });
+    if(error){
+      ok.log('error', 'could not request access '+error.message)
+    } else {
+      ok.log('success', 'requested access')
+    }
+    navigateTo('/success/request')
+  }
+  
+</script>
+<style scoped lang="scss">
+  input{
+    margin-bottom:$clamp-1;
+  }
+  .inputGroup{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: $clamp-1;
+  }
+</style>
