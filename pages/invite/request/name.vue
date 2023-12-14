8c57242ff8f43506c6a368ff848edcdafd4b2cd1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-14 08:53:28 +0100

    Create name.vue

---
+++ pages/invite/request/name.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <block>
+      <h1>Where should we send the invite?</h1>
+      <div class="inputGroup">
+        <input type="text" placeholder="First name" v-model="firstName" class="next"/>
+        <input type="text" placeholder="Last name" v-model="lastName" class="next"/>
+      </div>
+      <button class="next" @click="requestInvite()"> Next -> </button>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Request invite'
+  })
+  useHead({
+    title: 'Request invite',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const firstName = ref('')
+  const lastName = ref('')
+  const supabase = useSupabaseClient()
+  const requestUuid = useCookie('requestUuid')
+
+  const requestInvite = async () => {
+    
+    const { error, data } = await pub(supabase, {
+      "sender":"pages/invite/request/index.vue",
+      "entity": requestUuid.value
+    }).requestAccess({
+      firstName: firstName.value,
+      lastName: lastName.value
+    });
+    if(error){
+      ok.log('error', 'could not request access '+error.message)
+    } else {
+      ok.log('success', 'requested access')
+    }
+    navigateTo('/invite/request/name')
+  }
+  
+</script>
+<style scoped lang="scss">
+  input{
+    margin-bottom:$clamp-1;
+  }
+  .inputGroup{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: $clamp-1;
+  }
+</style>
