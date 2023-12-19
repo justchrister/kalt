cd47db32a582448dd7550c977c1ebc0b50740ae2
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-19 23:45:07 +0100

    implemented sizer() and primary() sass functions

--- components/chat/link.vue
+++ components/chat/link.vue
@@ -21,21 +21,21 @@
     display: flex;  // Changed to flex
     justify-content: flex-start;  // Pushes the child to the right
     position:relative;
-    width: $clamp-40;
-    min-height:$clamp-6;
-    margin-right: $clamp-18;
+    width: sizer(40);
+    min-height:sizer(6);
+    margin-right: sizer(18);
   }
   .link{
     display:inline-block;
     background: $marine-10;
     border: $border;
     border-color: $marine-80;
-    border-radius: $clamp-0-5;
+    border-radius: sizer(0.5);
     width: auto;
-    max-width: $clamp-40;
-    margin: $clamp-1 0;
+    max-width: sizer(40);
+    margin: sizer(1) 0;
     margin-right: auto;  // Pushes to the right
-    padding: $clamp-1;
+    padding: sizer(1);
   }
   .link:hover{
     cursor: pointer;
