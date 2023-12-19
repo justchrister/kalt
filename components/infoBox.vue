c79188bf89d6a64eb9853d047b7236672f357770
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-19 23:50:04 +0100

    implemented sizer() and primary() sass functions

--- components/infoBox.vue
+++ components/infoBox.vue
@@ -18,14 +18,14 @@ const props = defineProps({
     border:$border-width solid $blue;
     border-radius:$border-radius;
     padding:clamp(calc($unit-min/2), calc($unit/2), calc($unit-max/2))
-            $clamp;
+            sizer(1);
     background:$blue-20;
     font-family: $sans-serif;
     font-size: clamp($unit-min*1.2, $unit*1.2, $unit-max*1.2);
     font-weight:normal;
     line-height: 150%;
     font-display: optional;
-    margin: $clamp 0 ;
+    margin: sizer(1) 0 ;
     span{
         margin-right: clamp(calc($unit-min/2), calc($unit/2), calc($unit-max/2));
         font-size:90%;
