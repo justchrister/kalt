d50d38a250d52c2a14f0071b0dc1daf34df561e0
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-19 23:49:18 +0100

    implemented sizer() and primary() sass functions

--- components/promoBlock/index.vue
+++ components/promoBlock/index.vue
@@ -25,21 +25,21 @@
 <style scoped lang="scss">
   .wrap{
     width:100%;
-    min-height: $clamp-10;
+    min-height: sizer(10);
     border:$border;
     border-color:$dark-20;
-    padding:$clamp-1 $clamp-1-5;
+    padding:sizer(1) sizer(1.5);
     background:$light;
     box-sizing:border-box;
-    box-shadow: 0 0 $clamp-2 0 $test-color-5;
+    box-shadow: 0 0 sizer(2) 0 $test-color-5;
   }
   .content p{
     color:$dark-85;
-    margin-top:$clamp-0-5;
+    margin-top:sizer(0.5);
   }
   .content strong{
     color:$dark-90;
-    font-size:$clamp-1-5-no-min;
+    font-size:sizer(1.5);
   }
   .content strong span{
     float:right;
