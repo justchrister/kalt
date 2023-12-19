7ce1d82664f33e7eef7ce8a43bb214d271804691
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-19 23:45:01 +0100

    implemented sizer() and primary() sass functions

--- components/chat/answer.vue
+++ components/chat/answer.vue
@@ -11,20 +11,20 @@
 <style scoped lang="scss">
   .answer-wrap{
     display: grid;  // Changed to flex
-    grid-template-columns: 1fr $clamp-12 ;
+    grid-template-columns: 1fr sizer(12) ;
     position:relative;
-    min-height:$clamp-6;
+    min-height:sizer(6);
   }
   .answer{
     display:inline-block;
     background: $blue-20;
     border: $border;
     border-color: $blue;
-    border-radius: $clamp-0-5;
+    border-radius: sizer(0.5);
     width: auto;
-    max-width: $clamp-40;
-    margin: $clamp-1 0;
+    max-width: sizer(40);
+    margin: sizer(1) 0;
     margin-right: auto;  // Pushes to the right
-    padding: $clamp-1;
+    padding: sizer(1);
   }
 </style>
