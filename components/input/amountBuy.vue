a3a3cc8e0b351a4d626d289a95d987afa8da34b2
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-19 23:50:01 +0100

    implemented sizer() and primary() sass functions

--- components/input/amountBuy.vue
+++ components/input/amountBuy.vue
@@ -80,8 +80,8 @@ const userId = useSupabaseUser()
     grid-template-columns: 6fr 1fr;
   }
   .currency{
-    height:$clamp-4;
-    line-height:$clamp-4;
+    height:sizer(4);
+    line-height:sizer(4);
     border-left:$border;
     text-align:center;
   }
