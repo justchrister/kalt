1bfe8532b8f4dd01d8b32b053ec01be5971d6f72
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-22 17:13:40 +0100

    preloading shards (KALT-475)

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,3 +5,54 @@
     <navbar-bottom />
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/gradients/successAlt.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-top-left.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-top-center-left.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-top-center-right.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-top-right.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-bottom-right.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-bottom-center-right.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-bottom-center-left.svg',
+    },
+    {
+      rel: 'preload',
+      as: 'image',
+      href: '/images/shards/shard-bottom-left.svg',
+    }
+  ]
+})
+</script>
