33e1f2b3aeb9e13a8ae286129144592032c22a92
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-24 01:13:08 +0100

    creating button component

---
+++ components/input/button.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="input-wrap" @click="navigateToLink()">
+    <button>
+      <slot></slot>
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    link: {
+      type: String,
+      required: false
+    }
+  })
+  const navigateToLink = async () => {
+    if(!props.link) return;
+    navigateTo(props.link)
+  }
+</script>
+<style scoped lang="scss">
+  .input-wrap{
+    margin-bottom: sizer(1);
+    width: 100%;
+    box-sizing:border-box;
+    @include border;
+    @include hoverable;
+
+    button{
+      width: 100%;
+      padding: sizer(1) sizer(2);
+      @include border;
+      border-color:transparent;
+      @include font-body;
+    }
+
+    &:hover {
+      button{
+        @include hovering;
+        border-radius:100%;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
