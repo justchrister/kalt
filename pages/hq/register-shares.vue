9b30f0ed44905e6e188dabbb62ce614fb1d0383a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-24 01:14:26 +0100

    creating button component

--- pages/hq/register-shares.vue
+++ pages/hq/register-shares.vue
@@ -17,9 +17,7 @@
         total: {{val*1*1.2}} EUR
       </div>
       <div>
-        <button @click="create()">
-          create
-        </button>
+        <input-button @click="create()">create</input-button>
       </div>
     </block>
   </main>
