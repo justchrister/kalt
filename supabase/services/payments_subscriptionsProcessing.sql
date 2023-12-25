8c7189d9b5e72cc8a56e22c9191c741943790c4d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 15:20:17 +0100

    Delete payments_subscriptionsProcessing.sql

--- supabase/services/payments_subscriptionsProcessing.sql
+++
@@ -1,7 +0,0 @@
---- create the table, with default values
-CREATE TABLE "payments_subscriptionsProcessing" (
-  "processingId"  uuid          NOT NULL    PRIMARY KEY,
-  "timestampz"    timestamptz   NOT NULL    DEFAULT now()
-);
-
-ALTER TABLE "payments_subscriptionsProcessing" ENABLE ROW LEVEL SECURITY;
