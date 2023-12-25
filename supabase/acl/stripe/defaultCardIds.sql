c79f8af8b842643c05bb30d9d74e28a75c2547db
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 19:01:38 +0100

    Delete defaultCardIds.sql

--- supabase/acl/stripe/defaultCardIds.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "acl_stripe_defaultCardIds" (
-    "userId"          uuid    NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "stripeCardId"    text
-);
-ALTER TABLE "acl_stripe_defaultCardIds" ENABLE ROW LEVEL SECURITY;
