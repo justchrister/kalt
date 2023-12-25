9f1cb1ecf7359c1fc01a399aa9eb5ca21608fd33
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 13:45:00 +0100

    autoMatchOrders -> match

---
+++ supabase/sub/exchange/match.sql
@@ -0,0 +1,31 @@
+-- version 29.7.23
+-- service autoMatchOrders
+-- topic   exchangeOrders
+
+--- create the table, with default values
+CREATE TABLE "sub_exchange_autoMatchOrders" (
+    "message_id"          uuid          NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "message_entity"      uuid          NOT NULL  DEFAULT uuid_generate_v4(),
+    "message_sent"        timestamptz   NOT NULL  DEFAULT (now() at time zone 'utc'),
+    "message_sender"      text          NOT NULL,
+    "message_read"        boolean       NOT NULL  DEFAULT FALSE
+);
+
+-- Create the replicate function 
+CREATE OR REPLACE FUNCTION "replicate_exchangeOrders_autoMatchOrders"()
+RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO "sub_exchange_autoMatchOrders" (message_id, message_entity, message_sender, message_sent)
+  VALUES (NEW.message_id, NEW.message_entity, NEW.message_sender, NEW.message_sent);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+-- Create replicate trigger
+CREATE TRIGGER "replicate_exchangeOrders_autoMatchOrders"
+AFTER INSERT ON "topic_exchange"
+FOR EACH ROW
+EXECUTE FUNCTION "replicate_exchangeOrders_autoMatchOrders"();
+
+-- Enable RLS
+ALTER TABLE "sub_exchange_autoMatchOrders" ENABLE ROW LEVEL SECURITY;
