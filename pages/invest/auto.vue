6f9d1c35eb780fba9b9c2c79db4f972e997f5dd1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 10:33:30 +0100

    -sub + auto invest

--- pages/invest/auto.vue
+++ pages/invest/auto.vue
@@ -3,13 +3,13 @@
   <main>
     <intro title="Automatic investments" paragraph="Wealth is built over time, its always better to invest steadily than a single sum one time." />
     <block margin="1">
-      <input-amount-invest-auto :amount="subscription.amount || 0"/>
+      <input-amount-invest-auto :amount="autoInvest.amount || 0"/>
     </block>
     <block margin="2">
       <label> Select interval: </label>
-      <intervalSelector type="daily" :selected="dailySelected" @click="select('daily')"/>
-      <intervalSelector type="weekly" :selected="weeklySelected" @click="select('weekly')"/>
-      <intervalSelector type="monthly" :selected="monthlySelected" @click="select('monthly')"/>
+      <select-auto-invest-interval type="daily" :selected="selectedInterval" @click="selectInterval('daily')"/>
+      <select-auto-invest-interval type="weekly" :selected="selectedInterval" @click="selectInterval('weekly')"/>
+      <select-auto-invest-interval type="monthly" :selected="selectedInterval" @click="selectInterval('monthly')"/>
     </block>
     <block margin="1">
       <select-fund />
@@ -37,34 +37,25 @@
     title: 'Invest'
   })
   
-  const dailySelected = ref(true);
-  const weeklySelected = ref(false);
-  const monthlySelected = ref(false);
-
-  const select = (iteration) => {
-    ok.log('', iteration)
-    if(iteration==='daily'){
-      dailySelected.value = true;
-      weeklySelected.value = false;
-      monthlySelected.value = false;
-    } else if (iteration==='weekly'){
-      dailySelected.value = false;
-      weeklySelected.value = true;
-      monthlySelected.value = false;
-    } else if (iteration==='monthly'){
-      dailySelected.value = false;
-      weeklySelected.value = false;
-      monthlySelected.value = true;
+  const autoInvest = await get(supabase).autoInvest(userId.value.id);
+  const selectedInterval = ref(autoInvest.interval || 'daily')
+  const selectInterval = async (interval) => {
+    if(interval === 'monthly'){
+      if(autoInvest.type === 'monthlyBeginning') {
+        selectedInterval.value = 'monthlyBeginning';
+      } else if(autoInvest.type === 'monthlyMiddle') {
+        selectedInterval.value = 'monthlyMiddle';
+      } else if(autoInvest.type === 'monthlyEnd') {
+        selectedInterval.value = 'monthlyEnd';
+      } else {
+        selectedInterval.value = 'monthlyMiddle';
+      }
+    } else {
+      selectedInterval.value = interval;
     }
   }
-  const defaultCard = await get(supabase).defaultPaymentCard(user);
-  if(!defaultCard){
-    ok.log('error', 'User does not have default card')
-    await navigateTo('/cards')
-  }
-  const subscription = await get(supabase).subscription(userId.value.id);
-  ok.log('', subscription)
-  
+  ok.log('', autoInvest);
+
 </script>
 <style scoped lang="scss">
 main{
