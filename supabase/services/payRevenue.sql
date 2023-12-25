3398d23ed53a64c6456ef591ddb94917b0ce4c58
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 16:50:28 +0100

    Delete payRevenue.sql

--- supabase/services/payRevenue.sql
+++
@@ -1,9 +0,0 @@
---- create the table, with default values
-CREATE TABLE "payRevenue" (
-  "userId"      uuid        NOT NULL    PRIMARY KEY,
-  "amount"      numeric,
-  "currency"    text                    REFERENCES sys_currencies(iso)
-);
-
---- row level security
-ALTER TABLE "payRevenue" ENABLE ROW LEVEL SECURITY;
