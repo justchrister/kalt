3cb96784e45a005e82cc4ba9a0c6a21ea79aafa5
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 10:34:15 +0100

    simplified

--- supabase/topics/autoInvest.sql
+++ supabase/topics/autoInvest.sql
@@ -1,31 +1,30 @@
 -- create a topic, by simply renaming all instances of <<topic_name>
 -- version 6.4.23
--- topic   "topic_userSubscriptions"
+-- topic   "topic_autoInvest"
 
 --- create the table, with default values
-CREATE TABLE "topic_userSubscriptions" (
+CREATE TABLE "topic_autoInvest" (
 -- meta information used for processing
     "message_id"             uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "message_entity"         uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
     "message_sent"           timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
     "message_sender"         text                            NOT NULL,
 --
-    "userId"         uuid, 
-    "amount"         numeric,
-    "currency"       text                                                    REFERENCES sys_currencies(iso),
-    "active"         boolean,
-    "days"           integer[]
+    "userId"                 uuid, 
+    "amount"                 numeric,
+    "active"                 boolean,
+    "interval"               "autoInvest_intervals"
 );
 
 --- row level security
-ALTER TABLE "topic_userSubscriptions" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "topic_autoInvest" ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY "SELF — Insert" ON public."topic_userSubscriptions"
+CREATE POLICY "SELF — Insert" ON public."topic_autoInvest"
   AS PERMISSIVE FOR INSERT
   TO authenticated
   WITH CHECK (auth.uid() = "userId");
 
-CREATE POLICY "SELF — Select" ON public."topic_userSubscriptions"
+CREATE POLICY "SELF — Select" ON public."topic_autoInvest"
   AS PERMISSIVE FOR SELECT
   TO authenticated
   USING (auth.uid() = "userId");
@@ -34,4 +33,4 @@ CREATE POLICY "SELF — Select" ON public."topic_userSubscriptions"
 
 --- Indexes
 
-CREATE INDEX "index_topic_userSubscriptions_userId_message_sent" ON public."topic_userSubscriptions" USING btree ("userId", "message_sent")
+CREATE INDEX "index_topic_autoInvest_userId_message_sent" ON public."topic_autoInvest" USING btree ("userId", "message_sent")
