d3d688de4b15ac164263dc0d963a946f8b8b5acc
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 19:01:52 +0100

    Update funds.sql

--- supabase/sys/funds.sql
+++ supabase/sys/funds.sql
@@ -1,5 +1,5 @@
 create table public."sys_funds" (
-    "ticker"               tickers                 not null   primary,
+    "ticker"               "tickers"                 not null   primary key,
     "name"                 text,
     "description"          text
 );
@@ -9,12 +9,11 @@ create table public."sys_funds" (
 ALTER TABLE "sys_funds" ENABLE ROW LEVEL SECURITY;
 
 insert into public."sys_funds" ("ticker", "name", "description") values 
-  ('gi.ddf', 'Global Index', 'true'),
-  ('art.ddf', 'Art Fund', 'The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with clear set of criteria for picking the artworks and artists we invest in.'),
-  ('ffe.ddf', 'Fossil-free energy', 'The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by'),
-  ('ah.ddf', 'Affordable housing', ''),
-  ('vc.ddf', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.'),
-  ('smb.ddf', 'Small and medium sized businesses', 'The small and medium sized businesses fund invests in small and medium sized businesses with high growth potential, and a clear path to profitability.');
+  ('art', 'Art Fund', 'The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with clear set of criteria for picking the artworks and artists we invest in.'),
+  ('ffe', 'Fossil-free energy', 'The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by'),
+  ('ah', 'Affordable housing', ''),
+  ('vc', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.'),
+  ('smb', 'Small and medium sized businesses', 'The small and medium sized businesses fund invests in small and medium sized businesses with high growth potential, and a clear path to profitability.');
 
 CREATE POLICY "PUBLIC — Select"
   ON public.sys_funds
