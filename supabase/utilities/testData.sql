c23b55ca8c6912ce4045b3416b09d0062a940025
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 19:02:07 +0100

    Update testData.sql

--- supabase/utilities/testData.sql
+++ supabase/utilities/testData.sql
@@ -1,14 +1,14 @@
 insert into public."topic_exchange" ("message_sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', -100, 'art.ddf', 'sell', 'open', null, null, null, true);
+  ('setup script',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', -100, 'art', 'sell', 'open', null, null, null, true);
 
 insert into public."topic_exchange" ("message_sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00003-9933-4eaf-886b-e6e7e5b0205a', -20, 'vc.ddf', 'sell', 'open', null, null, null, true);
+  ('setup script',  'DDF00003-9933-4eaf-886b-e6e7e5b0205a', -20, 'vc', 'sell', 'open', null, null, null, true);
 
 insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
-  values ('setup script', 'vc.ddf', 200, 'Tillit');
+  values ('setup script', 'vc', 200, 'Tillit');
 
 insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
-  values ('setup script', 'art.ddf', 1000, 'Basquiat');
+  values ('setup script', 'art', 1000, 'Basquiat');
 
 insert into public."topic_userDetails" ("message_sender", "userId", "firstName", "lastName", "country", "birthdate", "city", "postalCode") values
                                        ('setup script',   'ae7aa0e5-cabe-4c62-b80c-fd8cc061a4c4', 'Christer',  'Bjornstad', 'NO',      '1996-10-30T15:35:25+00:00', 'OSLO', '0254');
