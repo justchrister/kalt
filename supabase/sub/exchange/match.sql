3adac3cf64cb8f30b0f6b05ac01a204a6a8641b4
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 16:50:32 +0100

    Update match.sql

--- supabase/sub/exchange/match.sql
+++ supabase/sub/exchange/match.sql
@@ -1,9 +1,9 @@
 -- version 29.7.23
--- service autoMatchOrders
--- topic   exchangeOrders
+-- service match
+-- topic   exchange
 
 --- create the table, with default values
-CREATE TABLE "sub_exchange_autoMatchOrders" (
+CREATE TABLE "sub_exchange_match" (
     "message_id"          uuid          NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "message_entity"      uuid          NOT NULL  DEFAULT uuid_generate_v4(),
     "message_sent"        timestamptz   NOT NULL  DEFAULT (now() at time zone 'utc'),
@@ -12,20 +12,20 @@ CREATE TABLE "sub_exchange_autoMatchOrders" (
 );
 
 -- Create the replicate function 
-CREATE OR REPLACE FUNCTION "replicate_exchangeOrders_autoMatchOrders"()
+CREATE OR REPLACE FUNCTION "replicate_exchange_match"()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO "sub_exchange_autoMatchOrders" (message_id, message_entity, message_sender, message_sent)
+  INSERT INTO "sub_exchange_match" (message_id, message_entity, message_sender, message_sent)
   VALUES (NEW.message_id, NEW.message_entity, NEW.message_sender, NEW.message_sent);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create replicate trigger
-CREATE TRIGGER "replicate_exchangeOrders_autoMatchOrders"
+CREATE TRIGGER "replicate_exchange_match"
 AFTER INSERT ON "topic_exchange"
 FOR EACH ROW
-EXECUTE FUNCTION "replicate_exchangeOrders_autoMatchOrders"();
+EXECUTE FUNCTION "replicate_exchange_match"();
 
 -- Enable RLS
-ALTER TABLE "sub_exchange_autoMatchOrders" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "sub_exchange_match" ENABLE ROW LEVEL SECURITY;
