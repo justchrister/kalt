3be3fd04b4f9b8a5b4a933001ad17c4c0996bc06
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 15:20:33 +0100

    added message_*

--- types/autoInvest.ts
+++ types/autoInvest.ts
@@ -6,5 +6,11 @@ declare global {
     amount?: number;
     active?: boolean;
     interval?: autoInvestIntervals;
+    charged?: string;
+
+    message_entity?: string;
+    message_id?: string;
+    message_sender?: string;
+    message_sent?: string;
   }
 }
