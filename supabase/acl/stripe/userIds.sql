953528ceef8603197a8b2112782679dad3b9a2e4
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-25 19:01:41 +0100

    Delete userIds.sql

--- supabase/acl/stripe/userIds.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "acl_stripe_userIds" (
-    "userId"          uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "stripeUserId"   text
-);
-ALTER TABLE "acl_stripe_userIds" ENABLE ROW LEVEL SECURITY;
