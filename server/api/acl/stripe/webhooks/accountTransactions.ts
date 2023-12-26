a39f68006e8517f5c411ef62088c2cb000f4be37
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-26 14:04:04 +0100

    paymentsPending->accountTransactions

--- server/api/acl/stripe/webhooks/accountTransactions.ts
+++ server/api/acl/stripe/webhooks/accountTransactions.ts
@@ -6,20 +6,21 @@ import Stripe from 'stripe';
 export default defineEventHandler( async (event) => {
   const supabase = serverSupabaseServiceRole(event);
   const body = await readBody(event);
-  const topic = 'paymentsPending';
+  const topic = 'accountTransactions';
   const service = 'aclStripe';
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY) as Stripe;
   if (body.record.message_read) return 'message already read';
 
   const message = await sub(supabase, topic).entity(body.record.message_entity);
-  await sub(supabase, topic).read(service, body.record.message_id);  
+  await sub(supabase, topic).read(service, body.record.message_id);
+  if(message.status !== 'pending') return 'transaction not pending payment';
+  if(message.type !== 'deposit') return 'transaction not deposit';
+  if(message.subType !== 'card') return 'transaction not card';
 
-  if(message.status !== 'pending') return 'message status is not pending';
-  if(message.provider !== 'stripe') return 'charge is not for Stripe';
   const amountCents = message.amount*100;
   const currencyLower = message.currency.toLowerCase();
 
-  const chargeCard = async (customerId, cardId) => {
+  const chargeCard = async (customerId: string, cardId: string) => {
     try {
       const charge = await stripe.paymentIntents.create({
         amount: amountCents, 
@@ -38,35 +39,20 @@ export default defineEventHandler( async (event) => {
       return 'error'
     }
   }
-  const updatePaymentPendingStatus = async (status: string) => {
+  const updateTransactionStatus = async (status: string, entity: string, userId: string) => {
     const error = await pub(supabase, {
-      entity: message.message_entity,
-      sender: 'service/api/acl/stripe/webhooks/paymentsPending.ts'
-    }).paymentsPending({
-      userId: message.userId,
-      transactionId: message.transactionId,
-      provider: 'stripe',
+      entity: entity,
+      sender: 'service/api/acl/stripe/webhooks/accountTransactions.ts'
+    }).accountTransactions({
+      userId: userId,
       status: status
-    } as paymentsPending);
+    } as accountTransaction);
     if(error){
       ok.log('error', 'error updating payment pending status', error)
       return 'error'
     } else {
       ok.log('success', 'updated payment pending status')
       return 'success'
-
-    }
-  }
-  const updateTransactionStatus = async (status: string) => {
-    const error = await pub(supabase, {
-      sender:'server/api/acl/stripe/webhooks/paymentsPending.ts',
-      entity: message.transactionId
-    }).accountTransactions({
-      userId: message.userId,
-      status: status
-    });
-    if(error){
-      ok.log('error', 'error updating transaction status', error)
     }
   }
   const getStripeIds = async (userId: string) => {
@@ -81,18 +67,21 @@ export default defineEventHandler( async (event) => {
     }
   }
 
-  const paymentPendingStatus = await updatePaymentPendingStatus('processing')
-  
-  if(paymentPendingStatus=='success') await updateTransactionStatus('processing')
-  if(paymentPendingStatus=='error') return 'failed to set as processing'
+  const transactionStatus = await updateTransactionStatus('processing', message.message_entity, message.userId)
 
-  const stripeIds = await getStripeIds(message.userId);
+  if(transactionStatus=='error') {
+    return 'failed to set as processing'
+  } else {
+    const stripeIds = await getStripeIds(message.userId);
+    if(!stripeIds || !stripeIds.stripeUserId || !stripeIds.stripeCardId) return 'no stripe ids found'
+    const charge = await chargeCard(stripeIds?.stripeUserId, stripeIds?.stripeCardId)
+    if(charge==='success'){
+      await updateTransactionStatus('complete', message.message_entity, message.userId)
+      return charge
+    } else {
+      await updateTransactionStatus('failed', message.message_entity, message.userId)
+      return 'failed to charge card'
+    }
+  }
 
-  const charge = await chargeCard(stripeIds?.stripeCustomerId, stripeIds?.stripeCardId)
-
-  if(charge=='success') await updatePaymentPendingStatus('complete')
-  if(charge=='success') await updateTransactionStatus('complete')
-  if(charge=='error') await updatePaymentPendingStatus('failed')
-  
-  return charge
 });
