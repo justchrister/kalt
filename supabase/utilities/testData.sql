ff55d7d0029a7300515f8ef86ad29dbba655f092
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-26 16:23:46 +0100

    added more test data

--- supabase/utilities/testData.sql
+++ supabase/utilities/testData.sql
@@ -3,9 +3,13 @@ insert into public."topic_exchange" ("message_sender", "userId", "quantity", "ti
 
 insert into public."topic_exchange" ("message_sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
   ('setup script',  'DDF00003-9933-4eaf-886b-e6e7e5b0205a', -20, 'vc', 'sell', 'open', null, null, null, true);
+insert into public."topic_exchange" ("message_sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('setup script',  'DDF00001-9933-4eaf-886b-e6e7e5b0205a', -20, 'ffe', 'sell', 'open', null, null, null, true);
 
 insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
   values ('setup script', 'vc', 200, 'Tillit');
+insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
+  values ('setup script', 'ffe', 200, 'Fossil free energy');
 
 insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
   values ('setup script', 'art', 1000, 'Basquiat');
