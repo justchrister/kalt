c64d761f1ff8da43aa37c9b1a173e3fa58c6dc71
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-26 19:18:19 +0100

    Update truncate.sql

--- supabase/utilities/truncate.sql
+++ supabase/utilities/truncate.sql
@@ -1,44 +1,19 @@
 -- ACL
-TRUNCATE TABLE "acl_stripe_defaultCardIds";
-TRUNCATE TABLE "acl_stripe_userIds";
--- Services
-TRUNCATE TABLE "getAccountBalance";
-TRUNCATE TABLE "getAssetPrice";
-TRUNCATE TABLE "getLinkedBankAccount";
-TRUNCATE TABLE "getPaymentCardDefault";
-TRUNCATE TABLE "getPaymentCards";
-TRUNCATE TABLE "getUser";
-TRUNCATE TABLE "getUserPortfolio";
-TRUNCATE TABLE "getUserSubscriptions";
-TRUNCATE TABLE "payments";
-TRUNCATE TABLE "paymentsSubscriptionsProcessing";
-TRUNCATE TABLE "payRevenue";
+TRUNCATE TABLE "acl_stripe";
 -- sub
-TRUNCATE TABLE "sub_accountTransactions_payments";
-TRUNCATE TABLE "sub_exchange_getUserPortfolio";
-TRUNCATE TABLE "sub_exchange_payRevenue";
-TRUNCATE TABLE "sub_linkedBankAccounts_getLinkedBankAccount";
+TRUNCATE TABLE "sub_accountTransactions_acl_stripe";
+TRUNCATE TABLE "sub_accountTransactions_exchange_autoOrder";
+TRUNCATE TABLE "sub_exchange_match";
 TRUNCATE TABLE "sub_paymentCards_acl_stripe";
-TRUNCATE TABLE "sub_paymentCards_getPaymentCardDefault";
-TRUNCATE TABLE "sub_paymentCards_getPaymentCards";
-TRUNCATE TABLE "sub_paymentsPending_acl_stripe";
-TRUNCATE TABLE "sub_revenueTransactions_payRevenue";
-TRUNCATE TABLE "sub_userDetails_acl_stripe";
-TRUNCATE TABLE "sub_userDetails_getUser";
-TRUNCATE TABLE "sub_userPreferences_getAccountBalance";
-TRUNCATE TABLE "sub_userPreferences_getUser";
-TRUNCATE TABLE "sub_userPreferences_getUserPortfolio";
-TRUNCATE TABLE "sub_userSubscriptions_getUserSubscriptions";
+TRUNCATE TABLE "sub_users_acl_stripe";
 -- topic
 TRUNCATE TABLE "topic_accountTransactions";
+TRUNCATE TABLE "topic_assets";
+TRUNCATE TABLE "topic_autoInvest";
 TRUNCATE TABLE "topic_exchange";
 TRUNCATE TABLE "topic_exchangeRates";
-TRUNCATE TABLE "topic_fundValuations";
 TRUNCATE TABLE "topic_linkedBankAccounts";
 TRUNCATE TABLE "topic_paymentCards";
-TRUNCATE TABLE "topic_paymentsPending";
 TRUNCATE TABLE "topic_requestAccess";
-TRUNCATE TABLE "topic_revenueTransactions";
-TRUNCATE TABLE "topic_userDetails";
-TRUNCATE TABLE "topic_userPreferences";
-TRUNCATE TABLE "topic_userSubscriptions";
+TRUNCATE TABLE "topic_userDefinedFunds";
+TRUNCATE TABLE "topic_users";
