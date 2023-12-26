4d31ef70a23108320fda26ecd43674adbad4a4be
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-26 19:17:50 +0100

    Update auth-hq.ts

--- middleware/auth-hq.ts
+++ middleware/auth-hq.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
   const userId = useSupabaseUser()
-  if (userId.value.id==='ae7aa0e5-cabe-4c62-b80c-fd8cc061a4c4'){
+  if (userId?.value?.id==='ae7aa0e5-cabe-4c62-b80c-fd8cc061a4c4'){
     console.log('HQ user')
   } else if (!userId.value){
     return navigateTo('/auth')
