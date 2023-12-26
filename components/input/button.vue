5208193ae309d39f0df2d0a5dbc8095bc165b34d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-26 17:19:04 +0100

    Update button.vue

--- components/input/button.vue
+++ components/input/button.vue
@@ -22,21 +22,29 @@
     margin-bottom: sizer(1);
     width: 100%;
     box-sizing:border-box;
+    transition: border-color 0.2s $easing-in-out;
     @include border;
     @include hoverable;
 
     button{
       width: 100%;
       padding: sizer(1) sizer(2);
+      color: $dark-90;
+      @include font-body;
       @include border;
       border-color:transparent;
-      @include font-body;
+      transition: border-color 0.2s $easing-in-out;
+      border-radius:100%;
     }
 
     &:hover {
+      @include hovering;
+      border-color:transparent; 
+      transition: border-color 0.2s $easing-in-out;
+      background-color:transparent;
       button{
+        color:$dark;
         @include hovering;
-        border-radius:100%;
         cursor: pointer;
       }
     }
