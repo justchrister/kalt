f879823b513248218d1cf63f1a3195888b059289
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-26 10:46:09 +0100

    Update testData.sql

--- supabase/utilities/testData.sql
+++ supabase/utilities/testData.sql
@@ -9,9 +9,3 @@ insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
 
 insert into public."topic_assets" ("message_sender", "ticker", "value", "name")
   values ('setup script', 'art', 1000, 'Basquiat');
-
-insert into public."topic_userDetails" ("message_sender", "userId", "firstName", "lastName", "country", "birthdate", "city", "postalCode") values
-                                       ('setup script',   'ae7aa0e5-cabe-4c62-b80c-fd8cc061a4c4', 'Christer',  'Bjornstad', 'NO',      '1996-10-30T15:35:25+00:00', 'OSLO', '0254');
-
-insert into public."topic_userPreferences" ("message_sender", "userId",                               "language", "currency",  "autoVest") values
-                                           ('setup script',   'ae7aa0e5-cabe-4c62-b80c-fd8cc061a4c4', 'NOR',      'EUR',       1);
