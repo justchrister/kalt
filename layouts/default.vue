6344005092a7e7a86a0ea87399524e93e81088d3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-27 14:21:43 +0100

    remove preloads

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,7 @@
 <template>
   <div class="container">
     <navbar-top />
-    <slot />
+    <slot></slot>
     <navbar-bottom />
   </div>
 </template>
-<script setup lang="ts">
-useHead({
-  link: [
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/gradients/successAlt.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-top-left.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-top-center-left.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-top-center-right.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-top-right.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-bottom-right.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-bottom-center-right.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-bottom-center-left.svg',
-    },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/images/shards/shard-bottom-left.svg',
-    }
-  ]
-})
-</script>
