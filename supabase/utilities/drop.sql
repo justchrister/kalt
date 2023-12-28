1fb00e89adfdeff77f65c50928786c65bc57dffd
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 05:17:54 +0100

    Create drop.sql

---
+++ supabase/utilities/drop.sql
@@ -0,0 +1,24 @@
+drop table if exists "acl_stripe";
+
+drop table if exists "sub_transactions_acl_stripe";
+drop table if exists "sub_transactions_exchange_autoOrder";
+drop table if exists "sub_exchange_match";
+drop table if exists "sub_cards_acl_stripe";
+drop table if exists "sub_users_acl_stripe";
+
+drop table if exists "topic_transactions";
+drop table if exists "topic_assets";
+drop table if exists "topic_autoInvest";
+drop table if exists "topic_exchange";
+drop table if exists "topic_exchangeRates";
+drop table if exists "topic_invites";
+drop table if exists "topic_linkedBankAccounts";
+drop table if exists "topic_cards";
+drop table if exists "topic_requestAccess";
+drop table if exists "topic_userDefinedFunds";
+drop table if exists "topic_users";
+
+drop table if exists "sys_countries";
+drop table if exists "sys_currencies";
+drop table if exists "sys_languages";
+drop table if exists "sys_funds";
