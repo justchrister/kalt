dbbf9ec4687dd6c304ef88dae8dd914cc235e798
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 05:41:25 +0100

    Update users.sql

--- supabase/topics/users.sql
+++ supabase/topics/users.sql
@@ -5,22 +5,20 @@
 --- create the table, with default values
 CREATE TABLE "topic_users" (
 -- meta information used for processing
-    "event"            uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "id"        uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
-    "timestamp"          timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
-    "sender"        text                            NOT NULL,
+    "event"                 uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "id"                    uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "timestamp"             timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "sender"                text                            NOT NULL,
     -- 
-    "userId"                uuid                            NOT NULL,
     "firstName"             text,
     "lastName"              text,
-    "email"                 text,
     "country"               text,
     "city"                  text,
     "postalCode"            text,
     "birthdate"             date,
     "addressLine1"          text,
     "addressLine2"          text,
-    "autoVest"              numeric                         NOT NULL        DEFAULT 1,
+    "autoVest"              numeric,
     "newsletters"           boolean, 
     "termsOfService"        boolean, 
     "performanceUpdates"    boolean, 
