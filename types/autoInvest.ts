6ae5e57cfe278bceea68d17b2cd1d2602ceeb348
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 17:52:12 +0100

    Update autoInvest.ts

--- types/autoInvest.ts
+++ types/autoInvest.ts
@@ -2,15 +2,10 @@ export { autoInvest };
 
 declare global {
   interface autoInvest {
-    userId?: string;
+    id?: string;
     amount?: number;
     active?: boolean;
     interval?: autoInvestIntervals;
     lastCharged?: string;
-
-    id?: string;
-    event?: string;
-    sender?: string;
-    timestamp?: string;
   }
 }
