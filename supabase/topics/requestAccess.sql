fd1ea0e07f51e6c2c5aa1e3299de253ff4604d77
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 05:41:11 +0100

    Update requestAccess.sql

--- supabase/topics/requestAccess.sql
+++ supabase/topics/requestAccess.sql
@@ -5,10 +5,10 @@
 --- create the table, with default values
 CREATE TABLE "topic_requestAccess" (
 -- meta information used for processing
-    "event"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "id"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
-    "timestamp"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
-    "sender"      text                            NOT NULL,
+    "event"               uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "id"                  uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "timestamp"           timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "sender"              text                            NOT NULL,
 -- 
     "email"               text,
     "firstName"           text,
