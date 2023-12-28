13c4450521ec825509f0719d43eed11abd7e7617
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 04:32:14 +0100

    Update exchange_autoOrder.sql

--- supabase/sub/accountTransactions/exchange_autoOrder.sql
+++ supabase/sub/accountTransactions/exchange_autoOrder.sql
@@ -1,9 +1,9 @@
 -- version 29.7.23
 -- service exchange: autoOrder
--- topic   accountTransactions
+-- topic   transactions
 
 --- create the table, with default values
-CREATE TABLE "sub_accountTransactions_exchange_autoOrder" (
+CREATE TABLE "sub_transactions_exchange_autoOrder" (
     "event"          uuid          NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "id"      uuid          NOT NULL  DEFAULT uuid_generate_v4(),
     "timestamp"        timestamptz   NOT NULL  DEFAULT (now() at time zone 'utc'),
@@ -12,20 +12,20 @@ CREATE TABLE "sub_accountTransactions_exchange_autoOrder" (
 );
 
 -- Create the replicate function 
-CREATE OR REPLACE FUNCTION "replicate_accountTransactions_exchange_autoOrder"()
+CREATE OR REPLACE FUNCTION "replicate_transactions_exchange_autoOrder"()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO "sub_accountTransactions_exchange_autoOrder" (event, id, sender, timestamp)
+  INSERT INTO "sub_transactions_exchange_autoOrder" (event, id, sender, timestamp)
   VALUES (NEW.event, NEW.id, NEW.sender, NEW.timestamp);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create replicate trigger
-CREATE TRIGGER "replicate_accountTransactions_exchange_autoOrder"
-AFTER INSERT ON "topic_accountTransactions"
+CREATE TRIGGER "replicate_transactions_exchange_autoOrder"
+AFTER INSERT ON "topic_transactions"
 FOR EACH ROW
-EXECUTE FUNCTION "replicate_accountTransactions_exchange_autoOrder"();
+EXECUTE FUNCTION "replicate_transactions_exchange_autoOrder"();
 
 -- Enable RLS
-ALTER TABLE "sub_accountTransactions_exchange_autoOrder" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "sub_transactions_exchange_autoOrder" ENABLE ROW LEVEL SECURITY;
