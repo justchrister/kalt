5522b23279c07cae9d132b022923952ea854c6bd
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 05:41:21 +0100

    Update userDefinedFunds.sql

--- supabase/topics/userDefinedFunds.sql
+++ supabase/topics/userDefinedFunds.sql
@@ -3,12 +3,11 @@
 --- create the table, with default values
 CREATE TABLE "topic_userDefinedFunds" (
 -- meta information used for processing
-    "event"          uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "id"      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
-    "timestamp"        timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
-    "sender"      text                            NOT NULL,
+    "event"               uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "id"                  uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
+    "timestamp"           timestamptz                     NOT NULL        DEFAULT (now() at time zone 'utc'),
+    "sender"              text                            NOT NULL,
     --- 
-    "userId"              uuid                            NOT NULL,
     "rate"                numeric,
     "ticker"              tickers                                         REFERENCES sys_funds(ticker), 
                           CHECK ("rate" >= 0 
