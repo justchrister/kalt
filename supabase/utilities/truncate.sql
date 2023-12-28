3cf2b622271efd2136f3c45f23d69c1a90ac8da9
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-28 04:37:12 +0100

    Update truncate.sql

--- supabase/utilities/truncate.sql
+++ supabase/utilities/truncate.sql
@@ -4,7 +4,7 @@ TRUNCATE TABLE "acl_stripe";
 TRUNCATE TABLE "sub_transactions_acl_stripe";
 TRUNCATE TABLE "sub_transactions_exchange_autoOrder";
 TRUNCATE TABLE "sub_exchange_match";
-TRUNCATE TABLE "sub_paymentCards_acl_stripe";
+TRUNCATE TABLE "sub_cards_acl_stripe";
 TRUNCATE TABLE "sub_users_acl_stripe";
 -- topic
 TRUNCATE TABLE "topic_transactions";
@@ -13,7 +13,7 @@ TRUNCATE TABLE "topic_autoInvest";
 TRUNCATE TABLE "topic_exchange";
 TRUNCATE TABLE "topic_exchangeRates";
 TRUNCATE TABLE "topic_linkedBankAccounts";
-TRUNCATE TABLE "topic_paymentCards";
+TRUNCATE TABLE "topic_cards";
 TRUNCATE TABLE "topic_requestAccess";
 TRUNCATE TABLE "topic_userDefinedFunds";
 TRUNCATE TABLE "topic_users";
