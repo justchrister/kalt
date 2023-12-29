5d4fd806c7a30a138a1e7b28295c00e56c679b3f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 16:53:16 +0100

    onboarding

--- supabase/utilities/assets.sql
+++ supabase/utilities/assets.sql
@@ -1,15 +1,19 @@
-insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', -100, 'art', 'sell', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "origin") values
+  ('setup script',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', -100, 'art', 'sell', 'open', true);
 
-insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00003-9933-4eaf-886b-e6e7e5b0205a', -20, 'vc', 'sell', 'open', null, null, null, true);
-insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00001-9933-4eaf-886b-e6e7e5b0205a', -20, 'ffe', 'sell', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "origin") values
+  ('setup script',  'DDF00003-9933-4eaf-886b-e6e7e5b0205a', -20, 'vc', 'sell', 'open', true);
+insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "origin") values
+  ('setup script',  'DDF00001-9933-4eaf-886b-e6e7e5b0205a', -20, 'ffe', 'sell', 'open', true);
 
-insert into public."topic_assets" ("sender", "ticker", "value", "name")
-  values ('setup script', 'vc', 200, 'Tillit');
+insert into public."topic_assets" ("sent", "sender", "ticker", "value", "name")
+  values (1637043686, 'setup script', 'vc', 200, 'Tillit');
+
 insert into public."topic_assets" ("sender", "ticker", "value", "name")
   values ('setup script', 'ffe', 200, 'Fossil free energy');
 
-insert into public."topic_assets" ("sender", "ticker", "value", "name")
-  values ('setup script', 'art', 1000, 'Basquiat');
+insert into public."topic_assets" ("sent", "sender", "ticker", "value", "name")
+  values (1627363286, 'setup script', 'art', 1000, 'Basquiat');
+
+insert into public."topic_assets" ("sent", "sender", "ticker", "value", "name")
+  values (1658899286, 'setup script', 'art', 1200, 'Basquiat');
