6fdce194d385f1c7993395b5ae2c1352410c8021
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 12:25:38 +0100

    Update testData.sql

--- supabase/utilities/testData.sql
+++ supabase/utilities/testData.sql
@@ -1,15 +1,14 @@
-insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', -100, 'art', 'sell', 'open', null, null, null, true);
-
-insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00003-9933-4eaf-886b-e6e7e5b0205a', -20, 'vc', 'sell', 'open', null, null, null, true);
-insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
-  ('setup script',  'DDF00001-9933-4eaf-886b-e6e7e5b0205a', -20, 'ffe', 'sell', 'open', null, null, null, true);
-
-insert into public."topic_assets" ("sender", "ticker", "value", "name")
-  values ('setup script', 'vc', 200, 'Tillit');
-insert into public."topic_assets" ("sender", "ticker", "value", "name")
-  values ('setup script', 'ffe', 200, 'Fossil free energy');
-
-insert into public."topic_assets" ("sender", "ticker", "value", "name")
-  values ('setup script', 'art', 1000, 'Basquiat');
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1672122086', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', 'buy', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1674800486', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', true);
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1677478886', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', 'buy', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1682572886', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', 'buy', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1672122086', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', 'buy', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1672122086', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', 'buy', 'open', null, null, null, true);
+insert into public."topic_exchange" ("sent", "sender", "userId", "quantity", "ticker", "type", "status", "fulfilledBy", "splitInto", "partOf", "origin") values
+  ('1672122086', 'test data',  'DDF00002-9933-4eaf-886b-e6e7e5b0205a', 10, 'art', 'buy', 'open', null, null, null, true);
