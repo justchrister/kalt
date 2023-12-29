2fe3fbdfd93a86b02ab269e8f4ec2ae43cf0c642
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 16:53:04 +0100

    onboarding

--- pages/invite/accept/name.vue
+++ pages/invite/accept/name.vue
@@ -1,7 +1,12 @@
 <template>
   <main>
     <block>
-      <input-user :user="user" id="user"/>
+      <h1> What should we call you?</h1>
+      <div class="inputGroup">
+        <input-user :user="user" id="firstName"/>
+        <input-user :user="user" id="lastName"/>
+      </div>
+      <input-button link="/invite/accept/legal">next →</input-button>
     </block>
   </main>
 </template>
@@ -17,8 +22,16 @@
       content: 'Invest in the future, today.'
     }]
   })
-  
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value) as user;
 </script>
 <style scoped lang="scss">
+
+.inputGroup{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: sizer(1);
+  }
   
 </style>
