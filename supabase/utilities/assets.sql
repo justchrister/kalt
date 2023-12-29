2246a4a28287fde818c17007f3b058e5257ff266
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 17:47:59 +0100

    fixing timestamps

--- supabase/utilities/assets.sql
+++ supabase/utilities/assets.sql
@@ -6,14 +6,14 @@ insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "
 insert into public."topic_exchange" ("sender", "userId", "quantity", "ticker", "type", "status", "origin") values
   ('setup script',  'DDF00001-9933-4eaf-886b-e6e7e5b0205a', -20, 'ffe', 'sell', 'open', true);
 
-insert into public."topic_assets" ("sent", "sender", "ticker", "value", "name")
-  values (1637043686, 'setup script', 'vc', 200, 'Tillit');
+insert into public."topic_assets" ("timestamp", "sender", "ticker", "value", "name")
+  values ('2021-7-29 16:42:50.498065+00', 'setup script', 'vc', 200, 'Tillit');
 
 insert into public."topic_assets" ("sender", "ticker", "value", "name")
   values ('setup script', 'ffe', 200, 'Fossil free energy');
 
-insert into public."topic_assets" ("sent", "sender", "ticker", "value", "name")
-  values (1627363286, 'setup script', 'art', 1000, 'Basquiat');
+insert into public."topic_assets" ("timestamp", "sender", "ticker", "value", "name")
+  values ('2021-7-29 16:42:50.498065+00', 'setup script', 'art', 1000, 'Basquiat');
 
-insert into public."topic_assets" ("sent", "sender", "ticker", "value", "name")
-  values (1658899286, 'setup script', 'art', 1200, 'Basquiat');
+insert into public."topic_assets" ("timestamp", "sender", "ticker", "value", "name")
+  values ('2023-12-29 16:42:50.498065+00', 'setup script', 'art', 1200, 'Basquiat');
