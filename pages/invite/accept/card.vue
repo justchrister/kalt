5b63816a5122b1348f05635292bea6661aff5b60
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 16:52:42 +0100

    onboarding

---
+++ pages/invite/accept/card.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <block>
+      <h1> Which card would you like to charge?</h1>
+      <card />
+      <input-button link="/funds/your">build your fund →</input-button>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'City',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'City',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value) as user;
+</script>
+<style scoped lang="scss">
+
+.inputGroup{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: sizer(1);
+  }
+  
+</style>
