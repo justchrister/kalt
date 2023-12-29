aef335b9c5e7ee24e7d059c81dbb8389d5efbc2e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 14:55:00 +0100

    Create legal.vue

---
+++ pages/invite/accept/legal.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <block>
+      <h1>Legal</h1>
+      <p>Some legal stuff, you can update the concent at any later point.</p>
+      <ul>
+        <li>Terms of Service (you should read this)</li>
+        <li>Performance updates</li>
+        <li>Monthly newsletter</li>
+        <li>Privacy policy (we dont share your data)</li>
+      </ul>
+      <input-button link="/invite/accept/auth">accept →</input-button>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Legal',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Legal',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
