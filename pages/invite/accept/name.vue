69a9561a9d887360c18d3e00635403d11ecb6433
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 14:55:02 +0100

    Create name.vue

---
+++ pages/invite/accept/name.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <block>
+      <input-user :user="user" id="user"/>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Name',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Name',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
