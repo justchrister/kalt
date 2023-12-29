e5698d677bb8688b1a76a055716314a99ef5e744
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-29 12:25:32 +0100

    Update funds.sql

--- supabase/sys/funds.sql
+++ supabase/sys/funds.sql
@@ -12,8 +12,8 @@ insert into public."sys_funds" ("ticker", "name", "description") values
   ('art', 'Art Fund', 'The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with clear set of criteria for picking the artworks and artists we invest in.'),
   ('ffe', 'Fossil-free energy', 'The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by'),
   ('ah', 'Affordable housing', ''),
-  ('vc', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.'),
-  ('smb', 'Small and medium sized businesses', 'The small and medium sized businesses fund invests in small and medium sized businesses with high growth potential, and a clear path to profitability.');
+--  ('smb', 'Small and medium sized businesses', 'The small and medium sized businesses fund invests in small and medium sized businesses with high growth potential, and a clear path to profitability.'),
+  ('vc', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.');
 
 CREATE POLICY "PUBLIC — Select"
   ON public.sys_funds
