818caa78c2ad28d1ad97637fc0416bcfe37ae7cf
christerwaters <42318403+christerwaters@users.noreply.github.com>
2023-12-30 09:50:50 +0100

    Update edit.vue

--- pages/accounts/edit.vue
+++ pages/accounts/edit.vue
@@ -4,9 +4,9 @@
       <h3>Edit linked bank account</h3>
     </block>
     <block>
-      <input-iban :initialValue="linkedBankAccount.iban"/>
-      <input-bank-code :initialValue="linkedBankAccount.bankCode"/>
-      <input-reference-text :initialValue="linkedBankAccount.reference"/>
+      <input-iban :initialValue="iban"/>
+      <input-bank-code :initialValue="bankCode"/>
+      <input-reference-text :initialValue="reference"/>
       <input-button link="/profile">done</input-button>
     </block>
   </main>
@@ -24,7 +24,9 @@
   const auth = useSupabaseUser()
   const user = await get(supabase).user(auth.value) as user;
   const linkedBankAccount = await get(supabase).linkedBankAccount(user);
-  ok.log('', linkedBankAccount)
+  const iban = linkedBankAccount.iban || null;
+  const reference = linkedBankAccount.reference || null;
+  const bankCode = linkedBankAccount.bankCode || null;
   
 </script>
 <style scoped lang="scss">
