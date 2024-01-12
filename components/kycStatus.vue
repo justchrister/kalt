9399c3d8ac8a43afa52ee97d73fcf15a2c12fd1b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-12 22:39:25 +0100

    Create kycStatus.vue

---
+++ components/kycStatus.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="{'icon': true, 'unverified': !verified }"></div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    user: {
+      type: Object,
+      required: true
+    }, 
+    initialValue: {
+      type: String,
+      required: false,
+      default: null
+    }
+  })
+  const verified = false;
+</script>
+<style scoped lang="scss">
+  .icon{
+    display:inline-block;
+    width: sizer(1.5);
+    height: sizer(1.5);
+    background-image: url('/omoji/kyc/verified.svg');
+    background-repeat: no-repeat;
+    background-size: 80%;
+    background-position: bottom;
+    &.unverified{
+      display:none;
+    }
+  }
+</style>
