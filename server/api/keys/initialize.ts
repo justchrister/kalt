a1dd4cdb5df0853f29263779ffcf760af7c5e634
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-12 22:37:57 +0100

    Create initialize.ts

---
+++ server/api/keys/initialize.ts
@@ -0,0 +1,21 @@
+import { ok } from '~/composables/ok'
+import { get } from '~/composables/get'
+import { pub } from '~/composables/messaging'
+import { serverSupabaseServiceRole } from '#supabase/server'
+
+export default defineEventHandler( async (event) => {
+
+  const keyPair = await ok.verifyKeyPair(event)
+  if(!keyPair) return 'unauthorized'
+
+  const supabase = serverSupabaseServiceRole(event)
+  const body = await readBody(event)
+  const user = await get(supabase).user(body.record.id) as user;
+  
+  // get data from on-boarding flow here
+  await pub(supabase, {
+    sender: 'server/api/keys/generate.ts',
+    id: user.id
+  }).keys({});
+  return 'generated'
+});
