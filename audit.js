45e214b058aa193fa41eb7b5ea916a9eff48597e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-14 13:55:57 +0100

    Update audit.js

--- audit.js
+++ audit.js
@@ -1,26 +1,36 @@
 const exec = require('child_process').exec;
+const fetch = require('node-fetch'); // Ensure this is installed
 
-const SLACK_WEBHOOK_URL = 'your_slack_webhook_url';
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
 const sendSlackNotification = async (message) => {
-  /*
-  await fetch(SLACK_WEBHOOK_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ text: message }),
-  });
-  */
+  try {
+    await fetch(SLACK_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: message }),
+    });
+  } catch (error) {
+    console.error(`Error sending Slack notification: ${error}`);
+  }
 };
 
-exec('npm audit --json', (error) => {
+exec('npm audit --json', (error, stdout) => {
   if (error) {
     console.error(`NPM Audit Failure: ${error}`);
-    sendSlackNotification(`NPM Audit Failure: ${error}`);
+   // sendSlackNotification(`NPM Audit Failure: ${error}`);
     process.exit(1); // Exit with a failure code
   } else {
-    console.log('NPM Audit Success: No issues found');
-    sendSlackNotification(`NPM Audit Success: No issues found`);
+    const auditResults = JSON.parse(stdout);
+    if (auditResults.metadata.vulnerabilities.total > 0) {
+      // Customize your message based on auditResults
+      const message = `NPM Audit: ${auditResults.metadata.vulnerabilities.total} vulnerabilities found.`;
+     // sendSlackNotification(message);
+    } else {
+      console.log('NPM Audit Success: No issues found');
+     // sendSlackNotification('NPM Audit Success: No issues found');
+    }
   }
 });
