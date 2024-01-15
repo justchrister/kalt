94afb0077d380c563a5c87bb12649c13a5fa32d9
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-15 18:35:49 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
+    <SpeedInsights />
     <navbar-top />
     <slot></slot>
     <navbar-bottom />
   </div>
 </template>
+<script lang="ts" setup>
+  import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
