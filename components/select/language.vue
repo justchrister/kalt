1b1833a3f8ed7f064a819b628b39947a1ada202a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-15 19:12:30 +0100

    Update language.vue

--- components/select/language.vue
+++ components/select/language.vue
@@ -1,23 +1,28 @@
 <template>
-  <div>
-  <label>Language: </label>
-  <nuxt-link to="/profile/edit/language">
-    <span class="iso">{{languageDetails.iso}}</span>
-    <span>{{languageDetails.name}}</span>
-    <span>→</span>
-  </nuxt-link></div>
+  <div @click="markedAsUnavail=true" class="outside-wrapper">
+    <modal title="This feature is not available (yet)" :show="markedAsUnavail">
+      We only offer our website in english, but will add more languages in the future.
+    </modal>
+    <label>Language: </label>
+    <div class="wrapper">
+      <span class="iso">{{languageDetails.iso}}</span>
+      <span>{{languageDetails.name}}</span>
+      <span>→</span>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
   const supabase = useSupabaseClient()
   const auth = useSupabaseUser()
   const user = await get(supabase).user(auth.value) as user;
   const languageDetails = await get(supabase).languageDetails(user?.language || 'ENG');
+  const markedAsUnavail = ref(false)
 </script>
 <style scoped lang="scss">
-  div{
+  .outside-wrapper{
     margin: sizer(1) 0 0 0 ;
   }
-  a{
+  .wrapper{
     padding: sizer(1) sizer(2);
     display:grid;
     grid-template-columns: sizer(4) 4fr sizer(1);
