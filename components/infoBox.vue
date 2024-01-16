7c9bb04ca68d40f25b7a3c56899ddf76c05234c8
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-16 01:34:36 +0100

    removing min clamp

--- components/infoBox.vue
+++ components/infoBox.vue
@@ -17,17 +17,16 @@ const props = defineProps({
   .infobox{
       border: $border-width solid $blue;
       border-radius: $border-radius;
-      padding:clamp(calc($unit-min/2), calc($unit/2), calc($unit-max/2))
-              sizer(1);
+      padding:sizer(0.5) sizer(1);
       background: $blue-20;
       font-family: $sans-serif;
-      font-size: clamp($unit-min*1.2, $unit*1.2, $unit-max*1.2);
+      font-size: sizer(1.2);
       font-weight:normal;
       line-height: 150%;
       font-display: optional;
       margin: sizer(1) 0 ;
       span{
-          margin-right: clamp(calc($unit-min/2), calc($unit/2), calc($unit-max/2));
+          margin-right: sizer(0.5);
           font-size:90%;
       }
   }
