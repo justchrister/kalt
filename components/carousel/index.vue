ed2fd55f73ab71f8f820b8e3082d3ddb4e247daf
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-17 00:11:49 +0100

    Create index.vue

---
+++ components/carousel/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="scroll-wrap">
+    <div class="wrap">
+      <slot>
+      </slot>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+.wrap{
+  display:grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: sizer(2);
+  width: $sitewidth;
+  min-width: $sitewidth;
+  max-width: $maxsitewidth*1.2;
+  margin:0 auto;
+  padding: sizer(5) 0;
+}
+.scroll-wrap{
+  width: $sitewidth;
+  max-width: $maxsitewidth*1.2;
+  padding:0 sizer(4);
+  overflow-y: scroll;
+  margin:0 auto;
+  &::-webkit-scrollbar {
+    width: 0px;
+    background: transparent; /* make scrollbar transparent */
+  }
+}
+</style>
