48c364ec9479a4e637edcd3585840c4b70df3133
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-18 08:08:16 +0100

    Update server.ts

--- composables/cryptography/server.ts
+++ composables/cryptography/server.ts
@@ -1,23 +1,22 @@
 import { randomBytes, createCipheriv, createDecipheriv } from 'crypto';
 
-export const encryptServerSide = (text: string, secretKey: string) => {
+export const encryptServerSide = (text: string, secretKeyHex: string) => {
   const iv = randomBytes(16);
-  const cipher = createCipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
-  let encrypted = cipher.update(text);
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return { iv: iv.toString('hex'), content: encrypted.toString('hex') };
+
+  const secretKey = Buffer.from(secretKeyHex, 'hex');
+
+  const cipher = createCipheriv('aes-256-cbc', secretKey, iv);
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  
+  return { iv: iv.toString('hex'), content: encrypted };
 };
+
 export const decryptServerSide = (hash: { iv: string, content: string }, secretKeyHex: string): string => {
   const iv = Buffer.from(hash.iv, 'hex');
   const encryptedText = Buffer.from(hash.content, 'hex');
 
-  // Convert hex string to Buffer
   const secretKey = Buffer.from(secretKeyHex, 'hex');
-
-  // Ensure that the secretKey is 32 bytes for AES-256-CBC
-  if (secretKey.length !== 32) {
-    throw new Error('Invalid key length');
-  }
 
   const decipher = createDecipheriv('aes-256-cbc', secretKey, iv);
   let decrypted = decipher.update(encryptedText);
