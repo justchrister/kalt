87186d30d7097c132fadcb1317e1652847c66208
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-18 07:50:29 +0100

    Update server.ts

--- composables/cryptography/server.ts
+++ composables/cryptography/server.ts
@@ -7,11 +7,19 @@ export const encryptServerSide = (text: string, secretKey: string) => {
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   return { iv: iv.toString('hex'), content: encrypted.toString('hex') };
 };
-
-export const decryptServerSide = (hash: { iv: string, content: string }, secretKey: string): string => {
+export const decryptServerSide = (hash: { iv: string, content: string }, secretKeyHex: string): string => {
   const iv = Buffer.from(hash.iv, 'hex');
   const encryptedText = Buffer.from(hash.content, 'hex');
-  const decipher = createDecipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+
+  // Convert hex string to Buffer
+  const secretKey = Buffer.from(secretKeyHex, 'hex');
+
+  // Ensure that the secretKey is 32 bytes for AES-256-CBC
+  if (secretKey.length !== 32) {
+    throw new Error('Invalid key length');
+  }
+
+  const decipher = createDecipheriv('aes-256-cbc', secretKey, iv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
