bcab46834680d52826dd40a8fb41123ffa96ae1a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-20 13:50:17 +0100

    Create user.d.ts

---
+++ types/user.d.ts
@@ -0,0 +1,23 @@
+export type { user };
+
+declare global {
+    interface user {
+        id?: string;
+        firstName?: string;
+        lastName?: string;
+        email?: string;
+        country?: string;
+        city?: string;
+        postalCode?: string;
+        birthdate?: Date;
+        addressLine1?: string;
+        addressLine2?: string;
+        autoVest?: number;
+        newsletters?: boolean;
+        termsOfService?: boolean;
+        performanceUpdates?: boolean;
+        profilePicture?: string;
+        language?: string;
+        currency?: string;
+    }
+}
