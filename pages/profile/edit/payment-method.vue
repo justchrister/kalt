fe1801dc934a80c873c3a72f6a89b991ddfe508b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-23 21:59:18 +0100

    Create payment-method.vue

---
+++ pages/profile/edit/payment-method.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <block>
+      <stripe-elements-payment :user="user" />
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Payment method',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Payment method',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth)
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
