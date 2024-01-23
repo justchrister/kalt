eee6ae7cb2256d0eecea154e0ce42b729b59f3d0
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-23 21:58:29 +0100

    Delete stripe.sql

--- supabase/acl/stripe.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "acl_stripe" (
-    "userId"          uuid    NOT NULL   PRIMARY KEY,
-    "stripeUserId"    text,
-    "stripeCardId"    text
-);
-ALTER TABLE "acl_stripe" ENABLE ROW LEVEL SECURITY;
