75f19210573ed3cd3c9eb80160c7f01181283618
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-23 22:09:24 +0100

    Update paymentMethods.sql

--- supabase/topics/paymentMethods.sql
+++ supabase/topics/paymentMethods.sql
@@ -1,9 +1,9 @@
 -- create a topic, by simply renaming all instances of <<topic_name>
 -- version 6.4.23
--- topic   "topic_paymentmethods"
+-- topic   "topic_paymentMethods"
 
 --- create the table, with default values
-CREATE TABLE "topic_paymentmethods" (
+CREATE TABLE "topic_paymentMethods" (
 -- meta information used for processing
     "event"                   uuid                            NOT NULL        DEFAULT uuid_generate_v4()         PRIMARY KEY,
     "id"                      uuid                            NOT NULL        DEFAULT uuid_generate_v4(),
@@ -16,14 +16,14 @@ CREATE TABLE "topic_paymentmethods" (
 );
 
 --- row level security
-ALTER TABLE "topic_paymentmethods" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "topic_paymentMethods" ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY "SELF — Insert" ON public."topic_paymentmethods"
+CREATE POLICY "SELF — Insert" ON public."topic_paymentMethods"
   AS PERMISSIVE FOR INSERT
   TO authenticated
   WITH CHECK (auth.uid() = "id");
 
-CREATE POLICY "SELF — Select" ON "public"."topic_paymentmethods"
+CREATE POLICY "SELF — Select" ON "public"."topic_paymentMethods"
   AS PERMISSIVE FOR SELECT
   TO authenticated
   USING (auth.uid() = "id")
