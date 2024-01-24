150a7afac5b0f7a341a7e1ffd96378325e3c9481
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-24 21:36:03 +0100

    Create paymentMethod.d.ts

---
+++ types/paymentMethod.d.ts
@@ -0,0 +1,10 @@
+export { paymentMethod };
+
+declare global {
+    interface paymentMethod {
+        id?: string;
+        provider?: string;
+        intentToken?: string;
+        authenticationRequested?: boolean;
+    }
+}
