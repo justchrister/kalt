172fb0c768a54dd801f9ec0e66585bc58f6882c4
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-27 19:17:00 +0100

    Create vision.vue

---
+++ pages/about/vision.vue
@@ -0,0 +1,67 @@
+<template>
+  <main>
+    <block>
+      <strong> Vision </strong>
+      <h2> Align capitalism with a sustainable future. </h2>
+    </block>
+    <block>
+      <strong> Mission </strong>
+      <h2> Redifine impact investing </h2>
+      <p> 
+        We want to empower individuals to maximize their positive impact on the world. By providing accessible investment tools and meaningful investment opportunities. Through innovation and technology, we are building the tools for anyone build a purpose-driven portfolio.
+      </p>
+    </block>
+    <block>
+      <strong> Goal 01 </strong>
+      <h2> 2x market returns </h2>
+      <p>
+        Portfolio performance drive impact, is our guiding principle. To make investing in a sustainable future lucrative, we aim to double the returns of a global index fund. 
+      </p>
+      <p>
+        Our strategy is designed to create a powerful synergy between financial gain and impactful change. Through investing in real assets, you can build real value — we call it wealth with values.
+      </p>
+    </block>
+    <block>
+      <strong> Goal 02 </strong>
+      <h2> Impact that grows </h2>
+      <p>
+        The positive environmental and societal impact has to grow over time. The true measure of an investment lies not just in its financial returns but in its capacity to effect positive change. Our approach is to enhance the impact of every dollar, euro or yen invested. Planting seeds, not just harvesting returns.
+      </p>
+    </block>
+    <block>
+      <strong> Goal 03 </strong>
+      <h2> Make it transparent </h2>
+      <p>
+        Transparency is fundamental when making huge promises. It is vital that you both see and understand the impact of your investments. Our commitment is to provide comprehensive and clear updates that details both financial performance and the real-world impact. 
+      </p>
+      <p>
+        This way, you are not only informed but can also appreciate the broader significance of your financial decisions. This is the biggest difference from Kalt and traditional ESG funds.
+      </p>
+    </block>
+    <block>
+      <h2> Principles </h2>
+      <p>
+      </p>
+    </block>
+    <block>
+      <intro-block-fund text="all our activities, from the smallest task to long term strategic investments, follow a goal, mission, vision  structure where everything aligns with our vision for a better future"/>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Vision',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Vision',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
