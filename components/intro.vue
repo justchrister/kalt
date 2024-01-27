bbce80e4a62afbca358a78ee964c27a97a1f5a9d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-27 22:33:48 +0100

    Update intro.vue

--- components/intro.vue
+++ components/intro.vue
@@ -1,5 +1,5 @@
 <template>
-  <section :class="'gradient-'+props.gradient">
+  <section>
     <block margin="none">
       <h1 class="sans-serif no-margin">{{props.title}}</h1>
       <p v-if="props.paragraph">
@@ -19,7 +19,7 @@
       required: false
     },
     gradient: {
-      type: String,
+      type: Boolean,
       required: false
     }
   })
@@ -28,18 +28,8 @@
 section{
   padding: sizer(9) 0 sizer(7) 0;
   margin-bottom: sizer(5);
-  &.gradient-2{
-    background:url('/gradient-2-min.jpg') no-repeat center center;
-    background-size:cover;
-  }
-  &.gradient-3{
-    background:url('/gradient-3-min.jpg') no-repeat center center;
-    background-size:cover;
-  }
-  &.gradient-4{
-    background:url('/gradient-4-min.jpg') no-repeat center center;
-    background-size:cover;
-  }
+  background:url('/gradient.jpg') no-repeat center center;
+  background-size:cover;
   h1{
     margin:0;
     color: $dark-90;
