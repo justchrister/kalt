e2e1f75b4306912248e24f6bc915fed2b1567035
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-27 19:56:24 +0100

    Create vision.vue

---
+++ pages/vision.vue
@@ -0,0 +1,125 @@
+<template>
+  <main>
+    <frame-wrap>
+      <frame-image src="/images/architecture/brutalist-house-in-affligem.jpg" />
+      <frame-image src="/images/eddy-mercx-1970.jpg" />
+    </frame-wrap>
+    <block>
+      <h1> Introduction </h1>
+      <p> We believe the only way to have actual impact is to use the market forces to make a change. The issues we are facing require a lot of funding, and the only way to allocate the adequate capital, is if that capital actually gives a return on investment </p>
+      <p> To guide us towards this improved future, we work in a methodical and structured way. We have defined three goals that we believe are the most important to achieve our mission and broader vision for a more sustainable future. </p>
+    </block>
+    <block>
+      <strong> Vision </strong>
+      <h2> Align capitalism with a sustainable future. </h2>
+    </block>
+    <block>
+      <strong> Mission </strong>
+      <h2> Redifine impact investing </h2>
+      <p> 
+        We want to empower individuals to maximize their positive impact on the world. By providing accessible investment tools and meaningful investment opportunities. Through innovation and technology, we are building the tools for anyone build a purpose-driven portfolio.
+      </p>
+    </block>
+    <block>
+      <strong> Goal 01 </strong>
+      <h2> 2x market returns </h2>
+      <p>
+        Portfolio performance drive impact, is our guiding principle. To make investing in a sustainable future lucrative, we aim to double the returns of a global index fund. 
+      </p>
+      <p>
+        Our strategy is designed to create a powerful synergy between financial gain and impactful change. Through investing in real assets, you can build real value — we call it wealth with values.
+      </p>
+    </block>
+    <block>
+      <strong> Goal 02 </strong>
+      <h2> Impact that grows </h2>
+      <p>
+        The positive environmental and societal impact has to grow over time. The true measure of an investment lies not just in its financial returns but in its capacity to effect positive change. Our approach is to enhance the impact of every dollar, euro or yen invested. Planting seeds, not just harvesting returns.
+      </p>
+    </block>
+    <block>
+      <strong> Goal 03 </strong>
+      <h2> Make it transparent </h2>
+      <p>
+        Transparency is fundamental when making huge promises. It is vital that you both see and understand the impact of your investments. Our commitment is to provide comprehensive and clear updates that details both financial performance and the real-world impact. 
+      </p>
+      <p>
+        This way, you are not only informed but can also appreciate the broader significance of your financial decisions. This is the biggest difference from Kalt and traditional ESG funds.
+      </p>
+    </block>
+    <block margin="none">
+      <h2> Product development principles </h2>
+    </block>
+    <block margin="half">
+      <strong>Build for people</strong>
+      <p>
+        Both our customer facing solution, such as this website, but also the infrastructure we are creating globally, needs to be built for the people actually living there.
+      </p>
+    </block>
+    <block margin="half">
+      <strong>Agressively optimistic</strong>
+      <p>
+        Creating Kalt won't be a walk in the park; it demands years of dedication and innovation. We are driven by our mission and determined to stay optimistic, even when faced with immense challenges.
+      </p>
+    </block>
+    <block margin="half">
+      <strong>Equal opportunity</strong>
+      <p>
+        We believe that everyone should have access to this kind of investment opportunity. You should not have to be a millionaire to invest in fossil-free energy, and you can feel good knowing that your money is making a difference for families in developing regions, for the environment, and generating you a passive income.
+      </p>
+    </block>
+    <block margin="half">
+      <strong>Simple first, then powerful</strong>
+      <p>
+        Focus on simplicity in functionality before adding a bunch of shiny features that might not accomplish much. Skip the nonesense and jargon.
+      </p>
+    </block>
+    <block margin="half">
+      <strong>Maximize impact</strong>
+      <p>
+        Impact and wealth creation are not in opposition, they should work together, where wealth creation creates more impact and vice versa.
+      </p>
+    </block>
+    <block>
+      <strong>Aim for clarity</strong>
+      <p>
+        Everyone should understand what they are investing in, the risks, and how its contributing to the wealth creation.
+      </p>
+    </block>
+    <block>
+      <h2> Questions and answers </h2>
+      <chat-link to="/questions/how-does-it-work?">
+        How does it work?
+      </chat-link>
+      <chat-link to="/questions/ddf?">
+        What is a direct dividend fund?
+      </chat-link>
+      <chat-link to="/questions/how-do-you-make-money">
+        How does Kalt make money?
+      </chat-link>
+      <chat-link to="/questions/what-is-residual-income?">
+        What is residual income?
+      </chat-link>
+      <chat-link to="/questions/compound-interest?">
+        How does compound interest work?
+      </chat-link>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Vision',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Vision',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
