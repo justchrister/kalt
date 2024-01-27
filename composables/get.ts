45003ed99313b3cf543e03b91b668ea5be0d867e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-27 22:33:53 +0100

    Update get.ts

--- composables/get.ts
+++ composables/get.ts
@@ -347,6 +347,32 @@ export const get = (client: any) => {
         return ok.cleanMessage(combined)
       }
     },
+    setupIntent: async (user: user)=>{
+      ok.log('', user.id)
+      const { data, error } = await client
+        .from('topic_paymentMethods')
+        .select()
+        .eq('id', user.id)
+        .order('timestamp', { ascending: true })
+      if(error || !data.intentToken) {
+        if(error){
+          ok.log('error', 'failed getting payment method: '+error.message)
+        }
+        const apiResponse = await fetch('/api/paymentMethods/generateIntentToken', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            id: user.id
+          })
+        }) as paymentMethod;
+        return apiResponse.intentToken
+      } else {
+        const combined = ok.merge(data, 'id')[0]
+        return combined.intentToken
+      }
+    },
     card: async (user: user) => {
       const { data, error } = await client
         .from('topic_cards')
