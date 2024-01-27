5e094af9496a5ac5aeb9e7549a28a87edbb94193
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-27 20:50:31 +0100

    Create add.vue

---
+++ pages/payment-methods/add.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <block>
+      <add-payment-method :user="user" />
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Payment method',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Payment method',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value) as user;
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
