36016b9e800b69f70e43a7a3621533ee0ed8a1c3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 03:20:13 +0100

    Update vc.vue

--- pages/funds/vc.vue
+++ pages/funds/vc.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <intro title="A safer way to invest in early stage startups." gradient="3"/>
+    <intro title="A safer way to invest in early stage startups."/>
     <block margin="half" :border="true">
       <h3>The venture capital fund gives you access to a wide variety of early stage startups around the world — with clear set of criteria for picking the companies and founders we invest in.</h3>
     </block>
