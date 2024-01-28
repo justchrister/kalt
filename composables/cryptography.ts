e04971861af97030fe96a3edc62f9da456b56f7a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 04:23:04 +0100

    Update cryptography.ts

--- composables/cryptography.ts
+++ composables/cryptography.ts
@@ -1,31 +1,31 @@
 import { ok } from '~/composables/ok'
 const minKeyLength = 64;
 
-const keyLengthRequirement = (key:string) => {
-    if (key.length !== minKeyLength) {
-        ok.log('error', 'Invalid secret key length: '+key.length)
-    }
+const keyLengthRequirement = (key: string) => {
+  if (key.length !== minKeyLength) {
+    ok.log('error', 'Invalid secret key length: ' + key.length)
+  }
 }
 
 export const cryptography = {
-    async decrypt(secretKey: string, hash: { iv: string, content: string }) {
-        keyLengthRequirement(secretKey);
-        if (process.client) {
-            const { decryptClientSide } = await import('./cryptography/client');
-            return await decryptClientSide(hash, secretKey);
-        } else if (process.server) {
-            const { decryptServerSide } = await import('./cryptography/server');
-            return await decryptServerSide(hash, secretKey);
-        }
-    },
-    async encrypt(secretKey: string, text: string) {
-        keyLengthRequirement(secretKey);
-        if (process.client) {
-            const { encryptClientSide } = await import('./cryptography/client');
-            return await encryptClientSide(text, secretKey);
-        } else if (process.server) {
-            const { encryptServerSide } = await import('./cryptography/server');
-            return await encryptServerSide(text, secretKey);
-        }
+  async decrypt(secretKey: string, hash: { iv: string, content: string }) {
+    keyLengthRequirement(secretKey);
+    if (process.client) {
+      const { decryptClientSide } = await import('./cryptography/client');
+      return await decryptClientSide(hash, secretKey);
+    } else if (process.server) {
+      const { decryptServerSide } = await import('./cryptography/server');
+      return await decryptServerSide(hash, secretKey);
     }
+  },
+  async encrypt(secretKey: string, text: string) {
+    keyLengthRequirement(secretKey);
+    if (process.client) {
+      const { encryptClientSide } = await import('./cryptography/client');
+      return await encryptClientSide(text, secretKey);
+    } else if (process.server) {
+      const { encryptServerSide } = await import('./cryptography/server');
+      return await encryptServerSide(text, secretKey);
+    }
+  }
 }
