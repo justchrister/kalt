df8da6ab6fd1732365f426102e2829533c9dd7d6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:28 +0100

    splitting get() into multiple files

---
+++ composables/get/linkedBankAccount.ts
@@ -0,0 +1,15 @@
+import { ok } from '~/composables/ok'
+
+export const getLinkedBankAccount = async (client, user) => {
+  
+  const { data, error } = await client
+    .from('topic_linkedBankAccounts')
+    .select()
+    .eq('userId', user.id)
+    .order('timestamp', { ascending: true })
+  if(error || data.length === 0 || !data) {
+    return null
+  } else {
+    return ok.merge(data, 'id')[0];
+  }
+};
