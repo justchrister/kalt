b307a624c62039d3b3be17163e4419943532d540
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:57 +0100

    splitting get() into multiple files

---
+++ composables/get/user.ts
@@ -0,0 +1,31 @@
+import { ok } from '~/composables/ok'
+
+export const getUser = async (client, auth) => {
+  let userId = auth.id;
+  let email = auth.email;
+  if(!auth.id){
+    userId = auth;
+  }
+  if(!auth.email){
+    email = null;
+  }
+  const { data } = await client 
+    .from('topic_users')
+    .select()
+    .eq('id', userId)
+    .order('timestamp', { ascending: true })
+  const merged = ok.merge(data, 'id')[0] as user;
+  if(!merged) {
+    return {
+      "id": userId,
+      email, 
+      auth
+    } as user
+  } else {
+    return {
+      ...merged, 
+      email, 
+      auth
+    } as user
+  }
+};
