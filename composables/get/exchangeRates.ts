f37a252822ea2a3ad3bc0b42d1e2287d88a3622c
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:16 +0100

    splitting get() into multiple files

---
+++ composables/get/exchangeRates.ts
@@ -0,0 +1,20 @@
+import { ok } from '~/composables/ok'
+import { get } from '~/composables/get'
+
+export const getExchangeRates = async (client, from, to) => {
+  if(from===to) return 1;
+  const { data, error } = await client
+    .from('topic_exchangeRates')
+    .select()
+    .eq('from', from)
+    .eq('to', to)
+    .order('timestamp', { ascending: true })
+    .limit(1)
+    .single()
+  if(error) {
+    ok.log('warn', 'could not get exchange rates')
+    return null
+  } else {
+    return data.rate
+  }
+};
