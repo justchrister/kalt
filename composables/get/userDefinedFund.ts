1d2d5df01692563967ce17e147a3b8a5be400853
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 15:36:17 +0100

    Update userDefinedFund.ts

--- composables/get/userDefinedFund.ts
+++ composables/get/userDefinedFund.ts
@@ -1,4 +1,15 @@
 import { ok } from '~/composables/ok'
 
 export const getUserDefinedFund = async (client, user) => {
+  const { data, error } = await client
+    .from('topic_userDefinedFunds')
+    .select()
+    .eq('id', user.id)
+    .order('timestamp', { ascending: true })
+  if(error) {
+    ok.log('error', error)
+    return null
+  } else {
+    return ok.combineJsonByKeys(data, 'ticker')
+  }
 };
