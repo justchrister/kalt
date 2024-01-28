5718dabc794c45ca1fce98531668f64a0a4ffca7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:15:48 +0100

    Create list.vue

---
+++ components/venture/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+  div{
+    display:grid; 
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: sizer(1);
+    gap: sizer(1);
+  }
+</style>
