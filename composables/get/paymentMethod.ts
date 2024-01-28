150538fdf20ea296d351cafe75f36066b069baf2
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:34 +0100

    splitting get() into multiple files

---
+++ composables/get/paymentMethod.ts
@@ -0,0 +1,17 @@
+import { ok } from '~/composables/ok'
+
+export const getPaymentMethod = async (client, user) => {
+  ok.log('', user.id)
+  const { data, error } = await client
+    .from('topic_paymentMethods')
+    .select()
+    .eq('id', user.id)
+    .order('timestamp', { ascending: true })
+  if(error) {
+    ok.log('error', 'failed getting payment method: ', error)
+    return null
+  } else {
+    const combined = ok.merge(data, 'id')[0]
+    return ok.cleanMessage(combined)
+  }
+};
