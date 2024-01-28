dce9da8f6478dff117631c9ebb1eb57a0bef96e3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:44 +0100

    splitting get() into multiple files

---
+++ composables/get/processingExchangeOrder.ts
@@ -0,0 +1,15 @@
+import { ok } from '~/composables/ok'
+
+export const getProcessingExchangeOrder = async (client, id) => {
+  const { data, error } = await client
+    .from('topic_exchange')
+    .select()
+    .eq('id', id)
+    .order('timestamp', { ascending: true })
+  if(error){
+    ok.log('error', error)
+    return 'error'
+  } else {
+    return ok.merge(data, 'id')[0]
+  }
+};
