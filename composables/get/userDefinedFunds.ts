9e6b75fa5278f7f188cba7280f6bb072180b3a77
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 15:36:19 +0100

    Update userDefinedFunds.ts

--- composables/get/userDefinedFunds.ts
+++ composables/get/userDefinedFunds.ts
@@ -1,15 +1,14 @@
 import { ok } from '~/composables/ok'
-import { get } from '~/composables/get'
 
 export const getUserDefinedFunds = async (client, user, ticker) => {
 
-  const { data, error } = await client 
+  const { data, error } = await client
     .from('topic_userDefinedFunds')
     .select()
     .eq('id', user.id)
     .eq('ticker', ticker)
     .order('timestamp', { ascending: true })
-  if(error) {
+  if (error) {
     return null
   } else {
     return ok.merge(data, 'id')[0]
