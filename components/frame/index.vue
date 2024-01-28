e883d25f41026aaf4c16094fd064a9b6e738ec4e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 04:22:58 +0100

    Create index.vue

---
+++ components/frame/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="frame">
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+.frame {
+  width: $sitewidth;
+  max-width: $maxsitewidth;
+  margin:0 auto sizer(3) auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: flex-end;
+  align-content: flex-start;
+  height: 50vw;
+  max-height: 418px;
+}
+</style>
