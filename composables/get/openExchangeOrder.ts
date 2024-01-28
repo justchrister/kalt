91d58b6bc2c3f7729ccc0bf250b9ffc12259673c
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:31 +0100

    splitting get() into multiple files

---
+++ composables/get/openExchangeOrder.ts
@@ -0,0 +1,22 @@
+import { ok } from '~/composables/ok'
+
+export const getOpenExchangeOrder = async (client, ticker, type, quantityAbsolute) => {
+  const { data, error } = await client
+    .from('topic_exchange')
+    .select()
+    .eq('ticker', ticker)
+    .eq('type', type)
+    .order('timestamp', { ascending: true })
+  if(error){
+    return error
+  } else {
+    const combined = ok.combineJsonByKeys(data, 'id');
+    const unfulfilledOrders = combined.filter(message => message.status === 'open');
+    const ordersWithQuantityAbsolute = unfulfilledOrders.map(message => {
+      message.quantityAbsolute = Math.abs(message.quantity);
+      return message;
+    });
+    const order = ordersWithQuantityAbsolute.find(message => message.quantityAbsolute >= quantityAbsolute);
+    return order;
+  }
+};
