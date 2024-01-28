4a190b644faea33517c3aaee297d43bdadb14605
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:17:03 +0100

    splitting get() into multiple files

---
+++ composables/get/userDefinedFunds.ts
@@ -0,0 +1,17 @@
+import { ok } from '~/composables/ok'
+import { get } from '~/composables/get'
+
+export const getUserDefinedFunds = async (client, user, ticker) => {
+
+  const { data, error } = await client 
+    .from('topic_userDefinedFunds')
+    .select()
+    .eq('id', user.id)
+    .eq('ticker', ticker)
+    .order('timestamp', { ascending: true })
+  if(error) {
+    return null
+  } else {
+    return ok.merge(data, 'id')[0]
+  }
+};
