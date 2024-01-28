d2ba2f011a46e6f2202e14886542b69bbc3be33b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-28 12:16:53 +0100

    splitting get() into multiple files

---
+++ composables/get/transactions.ts
@@ -0,0 +1,15 @@
+import { ok } from '~/composables/ok'
+
+export const getTransactions = async (client, user) => {
+  const { data, error } = await client 
+    .from('topic_transactions')
+    .select()
+    .eq('userId', user.id)
+    .order('timestamp', { ascending: true })
+  if(error) {
+    ok.log('error', error)
+    return null
+  } else {
+    return ok.merge(data, 'entity');
+  }
+};
