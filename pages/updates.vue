5325f34067fe00292a66ededa35c04597c463e2a
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-29 20:05:34 +0100

    Create updates.vue

---
+++ pages/updates.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Updates',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Updates',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
