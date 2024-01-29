415511e4c8527d9a2efeb853b9cd01ae12eb8560
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-29 23:58:40 +0100

    Update setupIntent.ts

--- composables/get/setupIntent.ts
+++ composables/get/setupIntent.ts
@@ -1,35 +1,23 @@
 import { ok } from '~/composables/ok'
 
-export const getSetupIntent = async (client, user) => {
-  const yesterday = new Date()
-  yesterday.setDate(yesterday.getDate() - 1)
-  const { data, error } = await client
-    .from('topic_paymentMethods')
-    .select()
-    .eq('id', user.id)
-    .eq('used', false)
-    .gte('timestamp', yesterday.toISOString())
-    .order('timestamp', { ascending: false })
-    .limit(1)
-    .single()
-  if(error || !data.intentToken) {
-    if(error){
-      ok.log('error', 'failed getting payment method: '+error.message)
-    }
-    const apiResponse = await fetch('/api/paymentMethods/generateIntentToken', {
+export const getSetupIntent = async (user: user) => {
+  try {
+    const response = await fetch('/api/paymentMethods/generateSetupIntent', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({user})
+      body: JSON.stringify({ user })
     });
-    if(!apiResponse.intentToken){
-      return 
-    } else {
-      return apiResponse.intentToken
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  } else {
-    const combined = ok.merge(data, 'id')[0]
-    return combined.intentToken
+
+    const data = await response.json();
+
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
   }
 };
