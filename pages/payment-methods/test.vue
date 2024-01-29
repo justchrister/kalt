ac04c245ceb6626973dfb9cab66242e7880f040f
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-29 21:35:35 +0100

    Create test.vue

---
+++ pages/payment-methods/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Page',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Page',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value)
+  const setupIntent = await get(supabase).setupIntent(user);
+  ok.log('', setupIntent)
+</script>
+<style scoped lang="scss">
+  
+</style>
