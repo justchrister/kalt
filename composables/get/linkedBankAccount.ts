4a046074437032542e47d730d733add71493fe08
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:09:21 +0100

    Chore: Fix formatting

--- composables/get/linkedBankAccount.ts
+++ composables/get/linkedBankAccount.ts
@@ -1,13 +1,13 @@
 import { ok } from '~/composables/ok'
 
 export const getLinkedBankAccount = async (client, user) => {
-  
+
   const { data, error } = await client
     .from('topic_linkedBankAccounts')
     .select()
     .eq('userId', user.id)
     .order('timestamp', { ascending: true })
-  if(error || data.length === 0 || !data) {
+  if (error || data.length === 0 || !data) {
     return null
   } else {
     return ok.merge(data, 'id')[0];
