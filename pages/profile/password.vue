11a089f70c03c4fbcca7607e26765938a2f919a3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:11:57 +0100

    Chore: Fix formatting

--- pages/profile/password.vue
+++ pages/profile/password.vue
@@ -3,23 +3,20 @@
   <main>
     <block>
       <h1>
-        Set a new password! <omoji emoji="😃" /> <omoji emoji="☀️" />
+        Set a new password!
+        <omoji emoji="😃" />
+        <omoji emoji="☀️" />
       </h1>
       <form @submit.prevent="resetPassword()">
         <div class="input-wrap">
           <label for='password'> Password:</label>
-          <input
-            type="password"
-            placeholder="password"
-            v-model="password"
-            id='password'
-          />
+          <input type="password" placeholder="password" v-model="password" id='password' />
         </div>
-        <input-button>change password <loading-icon v-if="loading"/></input-button>
+        <input-button>change password <loading-icon v-if="loading" /></input-button>
       </form>
     </block>
     <span v-if="notification" @click="setNotification(null)">
-      <banner-notification color="yellow" :message="notification"/>
+      <banner-notification color="yellow" :message="notification" />
     </span>
   </main>
 </template>
@@ -36,8 +33,8 @@
 
   const setNotification = async (message: string) => {
     ok.log('error', message)
-    notification.value=message
-    loading.value=false
+    notification.value = message
+    loading.value = false
     return
   }
 
@@ -45,19 +42,19 @@
   const loading = ref(false)
   const password = ref('')
   const resetPassword = async () => {
-    loading.value=true
-    if (password.value.length < 8){
+    loading.value = true
+    if (password.value.length < 8) {
       setNotification('Password must be at least 8 characters')
     } else {
       const { data, error } = await supabase.auth.updateUser({
         password: password.value
       })
       await ok.sleep(200);
-      if(error){
-        loading.value=false
+      if (error) {
+        loading.value = false
         ok.log('error', 'password not changed', error)
-      } else{
-        loading.value=false
+      } else {
+        loading.value = false
         navigateTo('/auth')
         ok.log('success', 'changed password')
       }
@@ -65,5 +62,7 @@
   }
 </script>
 <style scoped lang="scss">
-  button{margin-top: sizer(2);}
+  button {
+    margin-top: sizer(2);
+  }
 </style>
