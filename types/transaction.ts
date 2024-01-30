7285c64580d11821fc59176ab04e241716c8a783
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:14:04 +0100

    Chore: Fix formatting

--- types/transaction.ts
+++ types/transaction.ts
@@ -1,13 +1,13 @@
 export { transaction };
 
 declare global {
-    interface transaction {
-        userId?: string;
-        amount?: number;
-        currency?: string;
-        type?: AccountTransactionsTypes;
-        subType?: AccountTransactionsSubTypes;
-        status?: AccountTransactionsStatuses;
-        autoVest?: number;
-    }
+  interface transaction {
+    userId?: string;
+    amount?: number;
+    currency?: string;
+    type?: transactionsTypes;
+    subType?: transactionsSubTypes;
+    status?: transactionsStatuses;
+    autoVest?: number;
+  }
 }
