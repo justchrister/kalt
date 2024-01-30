b2b7ad02f14c45c3eead43eda107b68057ded2d9
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:10 +0100

    Chore: Fix formatting

--- pages/auth/password.vue
+++ pages/auth/password.vue
@@ -3,25 +3,21 @@
   <main>
     <block>
       <h1 class="sans-serif">
-        Request a new password! <omoji emoji="🙃" />
+        Request a new password!
+        <omoji emoji="🙃" />
       </h1>
       <form @submit.prevent="requestPassword()">
         <div class="input-wrap">
           <label for='email'> E-mail:</label>
-          <input
-            type="email"
-            placeholder="Email"
-            v-model="email"
-            id='email'
-          />
+          <input type="email" placeholder="Email" v-model="email" id='email' />
         </div>
-        <input-button>request new password <loading-icon v-if="loading"/></input-button>
+        <input-button>request new password <loading-icon v-if="loading" /></input-button>
       </form>
       <link-group>
         <nuxt-link to="/auth/">sign in</nuxt-link>
       </link-group>
     </block>
-    <notification :type="notification.type" :message="notification.message" v-if="notification.message"/>
+    <notification :type="notification.type" :message="notification.message" v-if="notification.message" />
   </main>
 </template>
 
@@ -35,24 +31,24 @@
   const supabase = useSupabaseClient()
   const loading = ref(false)
   const notification = ref({
-    type: null, 
+    type: null,
     message: null
   });
   const redirectTo = window.location.origin + '/profile/password';
   const email = ref('')
   const requestPassword = async () => {
-    if(loading.value) return
+    if (loading.value) return
     loading.value = true
     const { error } = await supabase.auth.resetPasswordForEmail(
       email.value, {
-        redirectTo: redirectTo,
+      redirectTo: redirectTo,
     })
     await ok.sleep(200);
-    if(!error){
+    if (!error) {
       navigateTo('/success/password')
     } else {
       ok.log('error', 'password reset did not work? ' + error)
-      notification.value={
+      notification.value = {
         type: 'error',
         message: error.message
       }
