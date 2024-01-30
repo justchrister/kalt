c7ccf1b9c120ef51adf851c224f21afee0c3ff8e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:12:39 +0100

    Chore: Fix formatting

--- pages/questions/index.vue
+++ pages/questions/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <intro title="Questions and answers"
-    paragraph="Here we try to answer some questions you might have, in simple to understand."/>
+      paragraph="Here we try to answer some questions you might have, in simple to understand." />
     <block>
       <chat-link to="/questions/how-does-it-work?">
         How does it work?
@@ -30,22 +30,7 @@
   })
 </script>
 <style scoped lang="scss">
-  main{padding-top:0;}
-  main a{
-    display:block;
-    text-decoration:none;
-    font-size:150%;
-    width:100%;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    margin-bottom: sizer(2);
-    padding-bottom: sizer(1);
-    border-bottom:1px solid black;
+  main {
+    padding-top: 0;
   }
-  /*
-  .dark-mode main a{
-    border-color: $light;
-  }
-  */
 </style>
