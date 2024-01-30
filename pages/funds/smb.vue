26fc4dac45d1dd4e04b433ef9433859140dfa833
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:39 +0100

    Chore: Fix formatting

--- pages/funds/smb.vue
+++ pages/funds/smb.vue
@@ -1,7 +1,8 @@
 <template>
   <main>
     <block margin="half">
-      <intro ticker="ffe" text="The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by "/>
+      <intro ticker="ffe"
+        text="The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by " />
     </block>
     <block>
       <criteria-list>
@@ -33,13 +34,4 @@
       content: 'Invest in the future, today.'
     }]
   })
-  
 </script>
-<style scoped lang="scss">
-.button-group{
-  display:grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: sizer(1);
-}
-
-</style>
