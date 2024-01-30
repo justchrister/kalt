55ca801719e045a3e98512581ad8a7d9d35ab9a3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:12:57 +0100

    Chore: Fix formatting

--- pages/vision.vue
+++ pages/vision.vue
@@ -6,11 +6,15 @@
     </frame>
     <block>
       <h1> Introduction </h1>
-      <p> 
-        The only way to have actual lasting impact is to use market forces to drive a change. The issues we are facing require a lot of funding, and the only way to allocate the adequate capital, is if that capital actually gives a return on investment.
+      <p>
+        The only way to have actual lasting impact is to use market forces to drive a change. The issues we are facing
+        require a lot of funding, and the only way to allocate the adequate capital, is if that capital actually gives a
+        return on investment.
       </p>
-      <p> 
-        To guide us towards this improved future, we work in a methodical and structured way. We have defined three goals that we believe are the most important to achieve our shared mission and broader vision for a more sustainable future.
+      <p>
+        To guide us towards this improved future, we work in a methodical and structured way. We have defined three goals
+        that we believe are the most important to achieve our shared mission and broader vision for a more sustainable
+        future.
       </p>
     </block>
     <block>
@@ -20,35 +24,44 @@
     <block>
       <strong> Mission </strong>
       <h2> Redefine impact investing </h2>
-      <p> 
-        We want to empower individuals to maximize their positive impact on the world. By providing accessible investment tools and meaningful investment opportunities. Through innovation and technology, we are building the tools for anyone to build a purpose-driven portfolio.
+      <p>
+        We want to empower individuals to maximize their positive impact on the world. By providing accessible investment
+        tools and meaningful investment opportunities. Through innovation and technology, we are building the tools for
+        anyone to build a purpose-driven portfolio.
       </p>
     </block>
     <block>
       <strong> Goal 01 </strong>
       <h2> 2x market returns </h2>
       <p>
-        Portfolio performance driving impact is our guiding principle. To make investing in a sustainable future lucrative, we aim to double the returns of a global index fund. 
+        Portfolio performance driving impact is our guiding principle. To make investing in a sustainable future
+        lucrative, we aim to double the returns of a global index fund.
       </p>
       <p>
-        Our strategy is designed to create a powerful synergy between financial gain and impactful change. Through investing in real assets, you can build real value — we call it wealth with values.
+        Our strategy is designed to create a powerful synergy between financial gain and impactful change. Through
+        investing in real assets, you can build real value — we call it wealth with values.
       </p>
     </block>
     <block>
       <strong> Goal 02 </strong>
       <h2> Impact that grows </h2>
       <p>
-        The positive environmental and societal impact has to grow over time. The true measure of an investment lies not just in its financial returns but in its capacity to effect positive change. Our approach is to enhance the impact of every dollar, euro or yen invested. Planting seeds, not just harvesting returns.
+        The positive environmental and societal impact has to grow over time. The true measure of an investment lies not
+        just in its financial returns but in its capacity to effect positive change. Our approach is to enhance the impact
+        of every dollar, euro or yen invested. Planting seeds, not just harvesting returns.
       </p>
     </block>
     <block>
       <strong> Goal 03 </strong>
       <h2> Make it transparent </h2>
       <p>
-        Transparency is fundamental when making huge promises. It is vital that you both see and understand the impact of your investments. Our commitment is to provide comprehensive and clear updates that detail both financial performance and the real-world impact. 
+        Transparency is fundamental when making huge promises. It is vital that you both see and understand the impact of
+        your investments. Our commitment is to provide comprehensive and clear updates that detail both financial
+        performance and the real-world impact.
       </p>
       <p>
-        This way, you are not only informed but can also appreciate the broader significance of your financial decisions. This is the biggest difference between Kalt and traditional ESG funds.
+        This way, you are not only informed but can also appreciate the broader significance of your financial decisions.
+        This is the biggest difference between Kalt and traditional ESG funds.
       </p>
     </block>
   </main>
@@ -64,8 +77,4 @@
       content: 'Invest in the future, today.'
     }]
   })
-  
 </script>
-<style scoped lang="scss">
-  
-</style>
