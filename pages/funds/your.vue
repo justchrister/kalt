2d752f89eec3b6d70406800e1fbef6485335ba96
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:45 +0100

    Chore: Fix formatting

--- pages/funds/your.vue
+++ pages/funds/your.vue
@@ -1,12 +1,13 @@
 <template>
   <main>
-    <intro title="Build your own fund" paragraph="Choose what you want to invest in both through your subscription and when completing single investments."/>
+    <intro title="Build your own fund"
+      paragraph="Choose what you want to invest in both through your subscription and when completing single investments." />
     <block>
-      <fund-your ticker="ffe"/>
-      <fund-your ticker="art"/>
-      <fund-your ticker="ah" :beta="true"/>
-      <fund-your ticker="smb" :beta="true"/>
-      <fund-your ticker="vc" :beta="true"/>
+      <fund-your ticker="ffe" />
+      <fund-your ticker="art" />
+      <fund-your ticker="ah" :beta="true" />
+      <fund-your ticker="smb" :beta="true" />
+      <fund-your ticker="vc" :beta="true" />
       <block>
         <input-button link="/invest">invest now -></input-button>
       </block>
@@ -27,12 +28,7 @@
   })
 </script>
 <style scoped lang="scss">
-  .button-group{
-    display:grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: sizer(1);
-  }
-  main{
-    padding-top:0;
+  main {
+    padding-top: 0;
   }
 </style>
