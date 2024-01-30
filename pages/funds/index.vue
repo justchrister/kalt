e8b07b3888ad45de18bc2051edf51aba18d63be1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:35 +0100

    Chore: Fix formatting

--- pages/funds/index.vue
+++ pages/funds/index.vue
@@ -1,8 +1,9 @@
 <template>
   <main>
-    <intro title="Our direct dividend funds." paragraph="We have a few speciailized funds that let you invest in a variety of different asset classes. By building your own diversified fund you can invest in all of these."/>
+    <intro title="Our direct dividend funds."
+      paragraph="We have a few speciailized funds that let you invest in a variety of different asset classes. By building your own diversified fund you can invest in all of these." />
     <block v-for="fund of funds" :key="fund.ticker" margin="2">
-      <fund :ticker="fund.ticker"/>
+      <fund :ticker="fund.ticker" />
     </block>
     <block>
       <input-button link="/invest">invest now -></input-button>
@@ -24,12 +25,7 @@
   const funds = await get(supabase).funds() as fund[];
 </script>
 <style scoped lang="scss">
-.button-group{
-  display:grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: sizer(1);
-}
-  main{
-    padding-top:0;
+  main {
+    padding-top: 0;
   }
 </style>
