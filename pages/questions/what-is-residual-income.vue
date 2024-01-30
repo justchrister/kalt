d231ea458d4fa865cf1d369b7ac4e8bdad9b9da9
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:12:42 +0100

    Chore: Fix formatting

--- pages/questions/what-is-residual-income.vue
+++ pages/questions/what-is-residual-income.vue
@@ -5,16 +5,17 @@
         What is residual income?
       </chat-question>
       <chat-answer>
-        Residual income is money that keeps coming in even after you've done the initial work. 
+        Residual income is money that keeps coming in even after you've done the initial work.
       </chat-answer>
       <chat-answer>
-        Investing in solar energy for instance is a great way to get some of that sweet residual income. You earn money from the electricity it generates.
+        Investing in solar energy for instance is a great way to get some of that sweet residual income. You earn money
+        from the electricity it generates.
       </chat-answer>
       <chat-answer>
         And the best part?
       </chat-answer>
       <chat-answer>
-        This income keeps coming in for years, giving you a long-term and predictable stream of cash flow. 
+        This income keeps coming in for years, giving you a long-term and predictable stream of cash flow.
       </chat-answer>
       <chat-link to="/invest">
         invest now
@@ -33,8 +34,4 @@
       content: 'Invest in the future, today.'
     }]
   })
-  
 </script>
-<style scoped lang="scss">
-  
-</style>
