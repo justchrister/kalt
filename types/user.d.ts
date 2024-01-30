254a6eb85eca4c777d26b545923e055e6011ebc1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:13:14 +0100

    Chore: Fix formatting

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,24 +1,24 @@
 export type { user };
 
 declare global {
-    interface user {
-        id?: string;
-        firstName?: string;
-        lastName?: string;
-        email?: string;
-        country?: string;
-        city?: string;
-        postalCode?: string;
-        birthdate?: Date;
-        addressLine1?: string;
-        addressLine2?: string;
-        autoVest?: number;
-        newsletters?: boolean;
-        termsOfService?: boolean;
-        performanceUpdates?: boolean;
-        profilePicture?: string;
-        language?: string;
-        paymentProviderId?: string;
-        currency?: string;
-    }
+  interface user {
+    id?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    country?: string;
+    city?: string;
+    postalCode?: string;
+    birthdate?: Date;
+    addressLine1?: string;
+    addressLine2?: string;
+    autoVest?: number;
+    newsletters?: boolean;
+    termsOfService?: boolean;
+    performanceUpdates?: boolean;
+    profilePicture?: string;
+    language?: string;
+    paymentProviderId?: string;
+    currency?: string;
+  }
 }
