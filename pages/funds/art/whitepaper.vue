b9329fda516fa9cc2ce89408ee4388922a87ea98
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:22 +0100

    Chore: Fix formatting

--- pages/funds/art/whitepaper.vue
+++ pages/funds/art/whitepaper.vue
@@ -3,17 +3,22 @@
     <block>
       <h1>Whitepaper: The comprehensive methodology for predicting the future value of art</h1>
     </block>
-
     <block margin="half">
       <h2>Abstract</h2>
       <p>
-        Art investment is a multifaceted domain where the profound meets financial pragmatism. In a world marked by increasing affluence, art has evolved beyond a mere financial asset. It has become a symbol of social status and a source of emotional enrichment. This paper explores the intricate relationship between art, investment, and emotions.
+        Art investment is a multifaceted domain where the profound meets financial pragmatism. In a world marked by
+        increasing affluence, art has evolved beyond a mere financial asset. It has become a symbol of social status and a
+        source of emotional enrichment. This paper explores the intricate relationship between art, investment, and
+        emotions.
       </p>
       <p>
-        The surge in demand for tangible, meaningful assets among discerning individuals underscores art's unique appeal as an investment. Art transcends conventional notions of assets; it becomes a gateway to prestige and a source of profound emotional connection, making it a compelling choice for astute investors.
+        The surge in demand for tangible, meaningful assets among discerning individuals underscores art's unique appeal
+        as an investment. Art transcends conventional notions of assets; it becomes a gateway to prestige and a source of
+        profound emotional connection, making it a compelling choice for astute investors.
       </p>
       <p>
-        This research delves into the intrinsic qualities of art that bridge the gap between financial and emotional dimensions. To own art is to forge a tangible link and immerse oneself in an unparalleled emotional journey.
+        This research delves into the intrinsic qualities of art that bridge the gap between financial and emotional
+        dimensions. To own art is to forge a tangible link and immerse oneself in an unparalleled emotional journey.
       </p>
     </block>
 
@@ -22,50 +27,79 @@
 
       <h3>Quantitative assessment of art's intrinsic value</h3>
       <p>
-        Art valuation demands a rigorous and objective approach that transcends subjective opinions. Our methodology is deeply rooted in data-driven analysis and statistical models, ensuring a systematic and transparent approach to valuation. It draws strength from empirical evidence, statistical rigor, and data-driven insights, all working in concert to assess art's value within the dynamic art market landscape while respecting the unique qualities inherent in each artwork.
+        Art valuation demands a rigorous and objective approach that transcends subjective opinions. Our methodology is
+        deeply rooted in data-driven analysis and statistical models, ensuring a systematic and transparent approach to
+        valuation. It draws strength from empirical evidence, statistical rigor, and data-driven insights, all working in
+        concert to assess art's value within the dynamic art market landscape while respecting the unique qualities
+        inherent in each artwork.
       </p>
       <p>
-        At the core of our methodology lies Comparative Analysis—a process grounded in empirical examination that facilitates a nuanced comprehension of an artwork's essence. By incorporating non-obvious traits and content analysis into our objective framework, we unearth the intricate layers contributing to art's intrinsic worth.
+        At the core of our methodology lies Comparative Analysis—a process grounded in empirical examination that
+        facilitates a nuanced comprehension of an artwork's essence. By incorporating non-obvious traits and content
+        analysis into our objective framework, we unearth the intricate layers contributing to art's intrinsic worth.
       </p>
 
       <h3>Historical Data Analysis</h3>
       <p>
-        Our valuation process begins with an extensive scrutiny of historical sales data, applying statistical models, including k-means clustering. Factors such as an artist's career trajectory, artwork rarity, and past sales performance are methodically considered. This quantitative foundation establishes a dependable baseline for valuation.
+        Our valuation process begins with an extensive scrutiny of historical sales data, applying statistical models,
+        including k-means clustering. Factors such as an artist's career trajectory, artwork rarity, and past sales
+        performance are methodically considered. This quantitative foundation establishes a dependable baseline for
+        valuation.
       </p>
 
       <h3>Objective Comparative Analysis</h3>
       <p>
-        Each artwork is meticulously placed within its pertinent artistic movement, genre, context, theme, and period. Our objective comparative analysis is powered by reinforced learning algorithms, enabling an impartial assessment of its position within the art world, free from subjective biases.
+        Each artwork is meticulously placed within its pertinent artistic movement, genre, context, theme, and period. Our
+        objective comparative analysis is powered by reinforced learning algorithms, enabling an impartial assessment of
+        its position within the art world, free from subjective biases.
       </p>
       <p>
-        Machine learning-driven algorithms, featuring neural networks, identify artworks sharing thematic or contextual similarities. We construct a comparative framework that enriches valuation. This objective comparison transcends mere visual resemblances, delving into the profound layers of artistic significance.
+        Machine learning-driven algorithms, featuring neural networks, identify artworks sharing thematic or contextual
+        similarities. We construct a comparative framework that enriches valuation. This objective comparison transcends
+        mere visual resemblances, delving into the profound layers of artistic significance.
       </p>
 
       <h3>Data-Driven Content Analysis</h3>
       <p>
-        A meticulous examination of each artwork's content explores elements like subject matter, composition, symbolism, and artistic techniques. Employing machine learning algorithms, including natural language processing, we analyze these elements, unveiling concealed narratives and thematic connections that underlie an artwork's value.
+        A meticulous examination of each artwork's content explores elements like subject matter, composition, symbolism,
+        and artistic techniques. Employing machine learning algorithms, including natural language processing, we analyze
+        these elements, unveiling concealed narratives and thematic connections that underlie an artwork's value.
       </p>
     </block>
     <block>
       <h2>Conclusion</h2>
       <p>
-        In this research paper, we have presented a comprehensive methodology for predicting the future value of art, bridging the gap between the emotional and financial dimensions of art investment. Our methodology is firmly grounded in data-driven analysis and statistical models, providing a systematic and transparent approach to art valuation.
+        In this research paper, we have presented a comprehensive methodology for predicting the future value of art,
+        bridging the gap between the emotional and financial dimensions of art investment. Our methodology is firmly
+        grounded in data-driven analysis and statistical models, providing a systematic and transparent approach to art
+        valuation.
       </p>
 
       <p>
-        Through the utilization of historical data analysis, objective comparative analysis, and data-driven content analysis, we have demonstrated the versatility and objectivity of our approach. This methodology has been successfully applied to various case studies, showcasing its effectiveness in assessing the investment potential of artworks across different genres and periods.
+        Through the utilization of historical data analysis, objective comparative analysis, and data-driven content
+        analysis, we have demonstrated the versatility and objectivity of our approach. This methodology has been
+        successfully applied to various case studies, showcasing its effectiveness in assessing the investment potential
+        of artworks across different genres and periods.
       </p>
 
       <p>
-        Art investment, as explored in this paper, represents a unique opportunity for investors to engage with tangible, meaningful assets that offer both financial returns and emotional enrichment. Our methodology seeks to provide a robust framework for making informed art investment decisions.
+        Art investment, as explored in this paper, represents a unique opportunity for investors to engage with tangible,
+        meaningful assets that offer both financial returns and emotional enrichment. Our methodology seeks to provide a
+        robust framework for making informed art investment decisions.
       </p>
 
       <p>
-        As the art market continues to evolve, our methodology offers a valuable tool for art investors, collectors, and institutions, enabling them to navigate the complexities of art valuation with confidence. It bridges the gap between subjective opinions and objective analysis, ensuring a more reliable assessment of an artwork's intrinsic value and future potential.
+        As the art market continues to evolve, our methodology offers a valuable tool for art investors, collectors, and
+        institutions, enabling them to navigate the complexities of art valuation with confidence. It bridges the gap
+        between subjective opinions and objective analysis, ensuring a more reliable assessment of an artwork's intrinsic
+        value and future potential.
       </p>
 
       <p>
-        In conclusion, our research paves the way for a more data-driven and objective approach to art investment, aligning the worlds of finance and artistry. As we continue to refine and expand our methodology, we anticipate further advancements in the field of art valuation, ultimately benefiting both investors and the art community at large.
+        In conclusion, our research paves the way for a more data-driven and objective approach to art investment,
+        aligning the worlds of finance and artistry. As we continue to refine and expand our methodology, we anticipate
+        further advancements in the field of art valuation, ultimately benefiting both investors and the art community at
+        large.
       </p>
     </block>
   </main>
