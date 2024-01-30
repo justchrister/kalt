3e4747643a485086d4f3aca33884ec9ad91ad411
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:13:52 +0100

    Chore: Fix formatting

--- server/api/exchange/autoOrder.ts
+++ server/api/exchange/autoOrder.ts
@@ -18,19 +18,19 @@ import { serverSupabaseServiceRole } from '#supabase/server';
 export default defineEventHandler(async (event) => {
 
   const keyPair = await ok.verifyKeyPair(event)
-  if(!keyPair) return 'unauthorized'
+  if (!keyPair) return 'unauthorized'
 
   const supabase = serverSupabaseServiceRole(event);
   const service = 'autoOrder';
   const topic = 'transactions';
   const body = await readBody(event);
-  
+
   if (body.record.read) return 'message already read';
-  
+
   const message = await sub(supabase, topic).entity(body.record.id);
   await sub(supabase, topic).read(service, body.record.event);
 
-  if(message.status!=='complete') {
+  if (message.status !== 'complete') {
     return 'wrong payment status'
   }
   if (message.type !== 'deposit') {
@@ -44,7 +44,7 @@ export default defineEventHandler(async (event) => {
   }
 
   const createExchangeOrder = async (userId: string, quantity: number, tickerx: tickers) => {
-    const json ={
+    const json = {
       ticker: tickerx,
       type: 'buy',
       status: 'open',
@@ -52,13 +52,13 @@ export default defineEventHandler(async (event) => {
       quantity: quantity
     } as exchangeOrder;
     const error = await pub(supabase, {
-      sender:'server/api/exchange/autoOrder.ts'
+      sender: 'server/api/exchange/autoOrder.ts'
     }).exchangeOrders(json);
-    if(error) {
+    if (error) {
       ok.log('error', error)
       return error
     } else {
-      return {json}
+      return { json }
     }
   };
   const createWithdrawTransaction = async (userId: string, amount: number, currency: string) => {
@@ -72,9 +72,9 @@ export default defineEventHandler(async (event) => {
       'status': 'complete'
     } as transaction;
     const error = await pub(supabase, {
-      sender:'server/api/exchange/autoOrder.ts'
+      sender: 'server/api/exchange/autoOrder.ts'
     }).transactions(json);
-    if(error) {
+    if (error) {
       return
     } else {
       return json
@@ -88,10 +88,10 @@ export default defineEventHandler(async (event) => {
       'autoVest': 0
     } as transaction;
     const error = await pub(supabase, {
-      sender:'server/api/exchange/autoOrder.ts',
+      sender: 'server/api/exchange/autoOrder.ts',
       id: id,
     }).transactions(json);
-    if(error) {
+    if (error) {
       return
     } else {
       return json
@@ -107,18 +107,18 @@ export default defineEventHandler(async (event) => {
   ok.log('', 'assetPrices: ', assetPrices)
   const convertedCurrency = await get(supabase).exchangeRates('EUR', message.currency)
   const user = await get(supabase).user(message.userId);
-  if(!user) return 'user not found'
+  if (!user) return 'user not found'
   const autoVestRate = user.autoVest || 1;
-  const withdrawAmount = message.amount - (message.amount*(1 - autoVestRate));
-  
+  const withdrawAmount = message.amount - (message.amount * (1 - autoVestRate));
+
 
   const withdrawTransaction = await createWithdrawTransaction(user.id, withdrawAmount, message.currency);
   response.withdrawTransaction = withdrawTransaction || {};
   const updatedTransaction = await updateTransaction(user.id, message.id);
   response.updatedTransaction = updatedTransaction || {};
 
-  const calculateAllocationPercentage = (userFund:any) => {
-    const total = userFund.reduce((total:any, { rate }) => total + rate, 0);
+  const calculateAllocationPercentage = (userFund: any) => {
+    const total = userFund.reduce((total: any, { rate }) => total + rate, 0);
     let userDefinedFundPercentage = []
     for (let i = 0; i < userFund.length; i++) {
       const ticker = userFund[i].ticker;
@@ -131,17 +131,17 @@ export default defineEventHandler(async (event) => {
     return userDefinedFundPercentage
   }
 
-  const userDefinedFund =  await get(supabase).userDefinedFund(user.id)
-  if(!userDefinedFund) return 'userDefinedFund not found'
+  const userDefinedFund = await get(supabase).userDefinedFund(user.id)
+  if (!userDefinedFund) return 'userDefinedFund not found'
 
   const allocationPercentage = calculateAllocationPercentage(userDefinedFund)
 
   for (let i = 0; i < allocationPercentage.length; i++) {
     const entry = allocationPercentage[i];
-    if(entry.allocation===0 || !entry.allocation) continue;
+    if (entry.allocation === 0 || !entry.allocation) continue;
     const currencyConvertedSharePrice = assetPrices[entry.ticker] * convertedCurrency;
-    const investQuantity =  (message.amount * autoVestRate * entry.allocation) / currencyConvertedSharePrice;
-    ok.log('', 'investQuantity '+entry.ticker+': ', investQuantity)
+    const investQuantity = (message.amount * autoVestRate * entry.allocation) / currencyConvertedSharePrice;
+    ok.log('', 'investQuantity ' + entry.ticker + ': ', investQuantity)
     const exchangeOrder = await createExchangeOrder(user.id, investQuantity, entry.ticker);
     response.exchangeOrders.push(exchangeOrder);
   }
