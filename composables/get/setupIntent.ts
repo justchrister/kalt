a1218ea6a0a7c5f3145624ac1d1c3c965bec42f1
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 21:21:22 +0100

    functioning fetch

--- composables/get/setupIntent.ts
+++ composables/get/setupIntent.ts
@@ -1,21 +1,10 @@
-export const getSetupIntent = async (user: user) => {
-  try {
-    const response = await fetch('/api/paymentMethods/generateSetupIntent', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ user })
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    return { data, error: null };
-  } catch (error) {
-    return { data: null, error };
-  }
+export const getSetupIntent = async (user) => {
+  const response = await $fetch('/api/paymentMethods/generateSetupIntent', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user })
+  });
+  return await response;
 };
