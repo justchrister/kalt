accf439afebee13206cc9a3b41c48a91b34f469b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:12:24 +0100

    Chore: Fix formatting

--- composables/get/sharePrices.ts
+++ composables/get/sharePrices.ts
@@ -1,18 +1,18 @@
 import { ok } from '~/composables/ok'
 
 export const getSharePrices = async (client) => {
-  const { data:originOrders } = await client
+  const { data: originOrders } = await client
     .from('topic_exchange')
     .select()
     .order('timestamp', { ascending: true })
     .eq('origin', true)
-  
+
   const { data: assetList } = await client
     .from('topic_assets')
     .select()
     .order('timestamp', { ascending: true })
   const assets = ok.combineJsonByKeys(assetList, 'ticker');
-  
+
   const shares = {};
   originOrders.forEach(message => {
     if (shares[message.ticker]) {
