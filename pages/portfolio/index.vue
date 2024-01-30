60ec7dc813c98fddaba6aaab75cb75ec189330a7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:11:39 +0100

    Chore: Fix formatting

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -3,7 +3,7 @@
   <main>
     <navbar-tabs />
     <frame>
-      <chart-portfolio :days="days"/>
+      <chart-portfolio :days="days" />
     </frame>
     <block margin="none">
       <nav class="filters">
@@ -12,28 +12,22 @@
             <li @click="setDays(1)">
               <span class="live-bullet">•</span> live
             </li>-->
-            <li @click="days=7" 
-                :class="{ active: days === 7 }">
+            <li @click="days = 7" :class="{ active: days === 7 }">
               7 days
             </li>
-            <li @click="days=30"
-                :class="{ active: days === 30 }">
+            <li @click="days = 30" :class="{ active: days === 30 }">
               1 month
             </li>
-            <li @click="days=90"
-                :class="{ active: days === 90 }">
+            <li @click="days = 90" :class="{ active: days === 90 }">
               3 months
             </li>
-            <li @click="days=180"
-                :class="{ active: days === 180 }">
+            <li @click="days = 180" :class="{ active: days === 180 }">
               6 months
             </li>
-            <li @click="days=365"
-                :class="{ active: days === 365 }">
+            <li @click="days = 365" :class="{ active: days === 365 }">
               1 year
             </li>
-            <li @click="days=3650"
-                :class="{ active: days === 3650 }">
+            <li @click="days = 3650" :class="{ active: days === 3650 }">
               max
             </li>
           </ul>
@@ -41,17 +35,8 @@
       </nav>
     </block>
     <block margin="1">
-      <cta :showDivest="true"/>
+      <cta :showDivest="true" />
     </block>
-    <!--
-    <block margin="1">
-      <label>you</label>
-      <profile-card />
-    </block>
-    <block margin="1">
-      <label>your fund</label>
-      <select-fund />
-    </block>-->
   </main>
 </template>
 <script setup lang="ts">
@@ -65,59 +50,54 @@
   const days = ref(30)
 </script>
 <style scoped lang="scss">
+  .filters {
+    margin-top: sizer(5);
+    height: sizer(5);
+    overflow-x: scroll;
+    overflow-y: hidden;
 
-.filters{
-  margin-top: sizer(5);
-  height: sizer(5);
-  overflow-x:scroll;
-  overflow-y:hidden;
-  &::-webkit-scrollbar {
-    width: 0px;
-    background: transparent; /* make scrollbar transparent */
+    &::-webkit-scrollbar {
+      width: 0px;
+      background: transparent;
+      /* make scrollbar transparent */
+    }
   }
-}
-.overflow-wrap{
-  position: relative;
-  width: sizer(70);
-  height: sizer(6);
-}
+  .overflow-wrap {
+    position: relative;
+    width: sizer(70);
+    height: sizer(6);
+  }
 
-ul{
-  position: absolute;
-  margin-left:0;
-  width:auto;
-}
-.live-bullet{
-  color: red(100%);
-}
-li{
-  display:inline-block;
-  width:sizer(7.5);
-  text-align:center;
-  border: $border;
-  padding:0;
-  font-size:85%;
-  margin-right: sizer(1);
-  border-radius: $border-radius;
-  position:relative;
-  &:before{
-    display: none;
+  ul {
+    position: absolute;
+    margin-left: 0;
+    width: auto;
   }
-  &.active,
-  &:hover{
-    border-radius:100%;
-    background:white;
-    cursor:   pointer;
+
+  .live-bullet {
+    color: red(100%);
   }
-  
-}
-/*
-  .dark-mode li{
-    border-color: $light;
+
+  li {
+    display: inline-block;
+    width: sizer(7.5);
+    text-align: center;
+    border: $border;
+    padding: 0;
+    font-size: 85%;
+    margin-right: sizer(1);
+    border-radius: $border-radius;
+    position: relative;
+
+    &:before {
+      display: none;
+    }
+
+    &.active,
+    &:hover {
+      border-radius: 100%;
+      background: white;
+      cursor: pointer;
+    }
   }
-  .dark-mode li.active,
-  .dark-mode li:hover{
-    background: dark(100%);
-  }
-  */
 </style>
