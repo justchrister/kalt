354fda30315198b91879e661a73c05baeae9a00b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:12:00 +0100

    Chore: Fix formatting

--- pages/questions/compound-interest.vue
+++ pages/questions/compound-interest.vue
@@ -5,10 +5,14 @@
         How does compound interest work?
       </chat-question>
       <chat-answer>
-        Compound interest is a powerful financial concept that allows your money to grow more and more over time. When you invest, what you earn on that investment is added to the original investment, making it larger. This larger investment then earns even more in the next period, and so on. 
+        Compound interest is a powerful financial concept that allows your money to grow more and more over time. When you
+        invest, what you earn on that investment is added to the original investment, making it larger. This larger
+        investment then earns even more in the next period, and so on.
       </chat-answer>
       <chat-answer>
-        This process repeats itself, causing the interest earned to become larger and larger, faster and faster. The longer the investment is left to grow, the greater the impact of compound interest will be. This is why we automatically re-invest for you.
+        This process repeats itself, causing the interest earned to become larger and larger, faster and faster. The
+        longer the investment is left to grow, the greater the impact of compound interest will be. This is why we
+        automatically re-invest for you.
       </chat-answer>
       <chat-link to="/invest">
         invest now
@@ -27,8 +31,4 @@
       content: 'Invest in the future, today.'
     }]
   })
-  
 </script>
-<style scoped lang="scss">
-  
-</style>
