e32c23ab4f3bc2c2761544b4d9761448a139ae56
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:15 +0100

    Chore: Fix formatting

--- pages/funds/ah.vue
+++ pages/funds/ah.vue
@@ -1,13 +1,15 @@
 <template>
   <main>
-    <intro title="Combat housing inequality, through our fund."/>
+    <intro title="Combat housing inequality, through our fund." />
     <block margin="half" :border="true">
-      <h3>The fund strategically allocates capital in development of affordable housing around the world. In locations that yield a stable return on the investments, but also contributes to social good by combating housing inequality. All based on a set of criteria. </h3>
+      <h3>The fund strategically allocates capital in development of affordable housing around the world. In locations
+        that yield a stable return on the investments, but also contributes to social good by combating housing
+        inequality. All based on a set of criteria. </h3>
     </block>
     <block margin="half">
       <criteria-list>
         <criteria :nr="1">
-          In a location with demand for affordable housing. 
+          In a location with demand for affordable housing.
         </criteria>
         <criteria :nr="2">
           The project must hacve clear indicators of future returns.
@@ -26,16 +28,29 @@
     <block margin="half">
       <h2>The thesis</h2>
       <p>
-        In the cities of tomorrow, affordable and quality housing will be critical. As more people move into urban areas, the need for accessible housing is set to rise sharply. This kind of development is key to a city's growth, ensuring that everyone, regardless of income, has a place to call home. This isn't just about planning for numbers, but nurturing community spirit and diversity that make cities special and liveable.
+        In the cities of tomorrow, affordable and quality housing will be critical. As more people move into urban areas,
+        the need for accessible housing is set to rise sharply. This kind of development is key to a city's growth,
+        ensuring that everyone, regardless of income, has a place to call home. This isn't just about planning for
+        numbers, but nurturing community spirit and diversity that make cities special and liveable.
       </p>
       <p>
-        Affordable housing also means thinking about the environment and the economy. It encourages energy-efficient living, lessens the impact on the environment, and keeps key workers living close to their jobs. Creating spaces where neighbors can connect and support each other turns housing into communities, laying the groundwork for cities that can face the future with confidence.
+        Affordable housing also means thinking about the environment and the economy. It encourages energy-efficient
+        living, lessens the impact on the environment, and keeps key workers living close to their jobs. Creating spaces
+        where neighbors can connect and support each other turns housing into communities, laying the groundwork for
+        cities that can face the future with confidence.
       </p>
       <p>
-        Affordable housing stands as a solid investment opportunity. Its essence as a fundamental need provides a constant demand, offering a buffer against market volatility and ensuring a stable return on investment. In an economic landscape that is often unpredictable, the affordable housing market offers a unique combination of social impact and financial prudence. The demand increases during volatile times. 
+        Affordable housing stands as a solid investment opportunity. Its essence as a fundamental need provides a constant
+        demand, offering a buffer against market volatility and ensuring a stable return on investment. In an economic
+        landscape that is often unpredictable, the affordable housing market offers a unique combination of social impact
+        and financial prudence. The demand increases during volatile times.
       </p>
       <p>
-        As we build these homes, we must blend innovation with compassion. Tomorrow's affordable housing should be synonymous with lasting quality and respect for the environment, embracing smart technology and sustainable design. This isn't just building for shelter, but carefully crafting the backdrop of everyday life for a thriving city. It's a commitment to a future where everyone has a stake in their community and the urban environment as a whole.
+        As we build these homes, we must blend innovation with compassion. Tomorrow's affordable housing should be
+        synonymous with lasting quality and respect for the environment, embracing smart technology and sustainable
+        design. This isn't just building for shelter, but carefully crafting the backdrop of everyday life for a thriving
+        city. It's a commitment to a future where everyone has a stake in their community and the urban environment as a
+        whole.
       </p>
       <input-button link="/invest">invest now -></input-button>
     </block>
@@ -43,25 +58,20 @@
   </main>
 </template>
 <script lang="ts" setup>
-  definePageMeta({
-    pagename: 'Affordable housing'
-  })
-  useHead({
-    title: 'Affordable housing',
-    meta: [{
-      name: 'description',
-      content: 'Invest in the future, today.'
-    }]
-  })
-  
+definePageMeta({
+  pagename: 'Affordable housing'
+})
+useHead({
+  title: 'Affordable housing',
+  meta: [{
+    name: 'description',
+    content: 'Invest in the future, today.'
+  }]
+})
+
 </script>
 <style scoped lang="scss">
-  .button-group{
-    display:grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: sizer(1);
-  }
-  main{
-    padding-top:0;
+  main {
+    padding-top: 0;
   }
 </style>
