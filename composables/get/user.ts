13de511f113f2338bd658f7377b786deedc6512b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:37 +0100

    Chore: Fix formatting

--- composables/get/user.ts
+++ composables/get/user.ts
@@ -3,28 +3,28 @@ import { ok } from '~/composables/ok'
 export const getUser = async (client, auth) => {
   let userId = auth.id;
   let email = auth.email;
-  if(!auth.id){
+  if (!auth.id) {
     userId = auth;
   }
-  if(!auth.email){
+  if (!auth.email) {
     email = null;
   }
-  const { data } = await client 
+  const { data } = await client
     .from('topic_users')
     .select()
     .eq('id', userId)
     .order('timestamp', { ascending: true })
   const merged = ok.merge(data, 'id')[0] as user;
-  if(!merged) {
+  if (!merged) {
     return {
       "id": userId,
-      email, 
+      email,
       auth
     } as user
   } else {
     return {
-      ...merged, 
-      email, 
+      ...merged,
+      email,
       auth
     } as user
   }
