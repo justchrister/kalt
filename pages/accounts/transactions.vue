1be4293c92da778f72b4a37c8a1d6506043ccdc7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:10:02 +0100

    Chore: Fix formatting

--- pages/accounts/transactions.vue
+++ pages/accounts/transactions.vue
@@ -1,17 +1,11 @@
 
 <template>
   <main>
-    <navbar-breadcrumbs parent="Accounts"/>
+    <navbar-breadcrumbs parent="Accounts" />
 
     <block margin="none" v-if="transactions">
-      <transaction 
-        v-for="transaction of transactions" 
-        :key="transaction.id" 
-        :type="transaction.type"
-        :amount="transaction.amount"
-        :currency="transaction.currency"
-        :date="transaction.timestamp"
-      />
+      <transaction v-for="transaction of transactions" :key="transaction.id" :type="transaction.type"
+        :amount="transaction.amount" :currency="transaction.currency" :date="transaction.timestamp" />
     </block>
   </main>
 </template>
@@ -26,6 +20,6 @@
   const supabase = useSupabaseClient()
   const auth = useSupabaseUser()
   const user = await get(supabase).user(auth.value) as user;
-  
+
   const transactions = await get(supabase).transactions(user);
 </script>
