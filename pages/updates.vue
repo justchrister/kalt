506a0b6704011bf0e2e2db91e131ed31b5d03bbb
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 20:36:54 +0100

    Update updates.vue

--- pages/updates.vue
+++ pages/updates.vue
@@ -2,9 +2,10 @@
   <main>
     <navbar-tabs />
     <block>
-      <update-single id="id" title="Welcome to Kalt!" ingress="We are super excited to have you on board, welcome to the club!" category="performance" date="01.01.2024"/>
-      <update-single id="id" title="title" ingress="ingress" category="majorEvent" date="01.01.2024"/>
-      <update-single id="id" title="title" ingress="ingress" category="performance" date="01.01.2024"/>
+      <update-single id="id" title="Welcome" ingress="We are super excited to have you on board, welcome to the club!" category="general" date="01.01.2024" :read="false"/>
+      <update-single id="id" title="You can now invite 10 friends to join Kalt" ingress="ingress" category="general" date="01.01.2024" :read="false"/>
+      <update-single id="id" title="So far this year you've earned 10 000$" ingress="At this rate you will reach your savings goal of 100,000 by march next year" category="performance" date="01.01.2024"/>
+      <update-single id="id" title="You have to confirm some info" ingress="On a regular basis we are required to ask you to confirm some information" category="kyc" date="01.01.2024" :read="false"/>
     </block>
   </main>
 </template>
