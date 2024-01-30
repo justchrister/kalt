159b8e335906602bb2cb46e1c161dba307701397
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-30 15:14:10 +0100

    Chore: Fix formatting

--- types/linkedBankAccount.d.ts
+++ types/linkedBankAccount.d.ts
@@ -1,12 +1,12 @@
 export { linkedBankAccount };
 
 declare global {
-    interface linkedBankAccount {
-        id?: string;
-        name?: string;
-        email?: string;
-        iban?: string;
-        bankCode?: string;
-        reference?: string; 
-    }
+  interface linkedBankAccount {
+    id?: string;
+    name?: string;
+    email?: string;
+    iban?: string;
+    bankCode?: string;
+    reference?: string;
+  }
 }
