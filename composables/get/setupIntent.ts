e4f1bba0bf2b76ae45ff5f2668530b0b6da0f63e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-31 19:05:36 +0100

    added try+catch

--- composables/get/setupIntent.ts
+++ composables/get/setupIntent.ts
@@ -1,10 +1,20 @@
 export const getSetupIntent = async (user) => {
-  const response = await $fetch('/api/paymentMethods/generateSetupIntent', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ user })
-  });
-  return await response;
+  try{
+    const response = await $fetch('/api/paymentMethods/generateSetupIntent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user })
+    });
+    return await response;
+  } catch (error) {
+    return {
+      data: null,
+      error: {
+        status: 501,
+        message: 'failed getting default payment method'
+      }
+    }
+  }
 };
