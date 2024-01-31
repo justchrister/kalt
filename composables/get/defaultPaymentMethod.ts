d0444a45c6c41aa877402b818c61526c185ef514
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-01-31 18:51:08 +0100

    Update defaultPaymentMethod.ts

--- composables/get/defaultPaymentMethod.ts
+++ composables/get/defaultPaymentMethod.ts
@@ -1,10 +1,20 @@
 export const getDefaultPaymentMethod = async (user) => {
-  const response = await $fetch('/api/paymentMethods/default', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ user })
-  });
-  return await response;
+  try{
+    const response = await $fetch('/api/paymentMethods/default', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user })
+    });
+    return await response;
+  } catch (error) {
+    return {
+      data: null,
+      error: {
+        status: 501,
+        message: 'failed getting default payment method'
+      }
+    }
+  }
 };
