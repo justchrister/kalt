f0c72bedff5f32ba75156637292c6b4993219e4d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-04 16:55:25 +0100

    KALT-567: Optimize on-boarding flow

---
+++ pages/invite/accept/payment-method.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <block>
+      <h1> How would you like to invest?</h1>
+      <payment-method-add :user="user" button-label="build your fund ->" submit-redirect="/funds/your"/>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Payment method',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Payment method',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value) as user;
+</script>
