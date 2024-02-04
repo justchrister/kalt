53f33474266e88146c8496dcd254a58084c3dd56
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-04 19:01:35 +0100

    KALT-458: Add custom date picker

---
+++ pages/profile/edit/birthdate/day.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <block>
+      <input-birthdate-day :user="user" />
+      <input-button link="/profile/"> done </input-button>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Birthdate',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'Birthdate',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+  const supabase = useSupabaseClient();
+  const auth = useSupabaseUser();
+  const user = await get(supabase).user(auth.value);
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
