395697c148f79c960efb71ae1702263098b79b77
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-04 19:02:02 +0100

    KALT-458: Add custom date picker

---
+++ components/select/birthdate.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <label>Country: </label>
+  <nuxt-link to="/profile/edit/birthdate/year">
+    <span>Set your birthday</span>
+    <span>→</span>
+  </nuxt-link></div>
+</template>
+<script setup lang="ts">
+  const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value) as user;
+</script>
+<style scoped lang="scss">
+  div{
+    margin: sizer(1) 0 0 0 ;
+  }
+  a{
+    padding: sizer(1) sizer(2);
+    display:grid;
+    grid-template-columns: 4fr sizer(1);
+    text-decoration:none;
+    @include border;
+    @include hoverable;
+    &:hover{
+      @include hovering;
+    }
+  }
+  .iso{
+    font-family:"Kalt Monospace", monospace;
+    font-size:75%;
+  }
+</style>
