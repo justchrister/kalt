12e041671ce9a75b0b0626947a54e2a9d14e058b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-04 23:18:08 +0100

    summing function instead of combineJsonByKeys (should be removed)

--- composables/get/sharePrices.ts
+++ composables/get/sharePrices.ts
@@ -1,6 +1,12 @@
 import { ok } from '~/composables/ok'
 
 export const getSharePrices = async (client) => {
+  if (process.client) {
+    return {
+      data: null,
+      error: 'cannot be used unless server side. instead fetch from /api/exchange/sharePrices'
+    }
+  }
   const { data: originOrders, error: errorOriginOrders } = await client
     .from('topic_exchange')
     .select()
@@ -24,8 +30,11 @@ export const getSharePrices = async (client) => {
     .from('topic_assets')
     .select()
     .order('timestamp', { ascending: true })
-  const assets = ok.combineJsonByKeys(assetList, 'ticker');
+  
+  const assets = ok.sum('value', assetList, 'ticker');
+  
   if (errorAssetList) {
+    ok.log('error', 'errorAssetList', errorAssetList)
     return {
       data: null,
       error: errorAssetList
@@ -53,7 +62,6 @@ export const getSharePrices = async (client) => {
       result[ticker] = asset.value / shares[ticker];
     }
   });
-
   return {
     error: null,
     data: result
