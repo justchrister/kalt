d244fdddabe1e7b79db2b1bff8eece7f6a1fe098
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-04 23:41:52 +0100

    Update portfolio.ts

--- composables/get/portfolio.ts
+++ composables/get/portfolio.ts
@@ -38,7 +38,7 @@ export const getPortfolio = async (client, user) => {
   for (let i = 0; i < fulfilledOrders.length; i++) {
     const fulfilledOrder = fulfilledOrders[i];
     const ticker = fulfilledOrder.ticker;
-    const currencyConvertedSharePrice = assetPrices[ticker] * convertedCurrency;
+    const currencyConvertedSharePrice = sharePrices[ticker] * convertedCurrency;
     const todaysValueChange = fulfilledOrder.quantity * currencyConvertedSharePrice;
     const dateObject = new Date(fulfilledOrder.timestamp);
     const date = dateObject.toISOString().split('T')[0];
@@ -54,30 +54,32 @@ export const getPortfolio = async (client, user) => {
 
   // Sort the dates to go from earliest to latest
   const sortedDates = Object.keys(dateValueMap).sort();
-
+  ok.log('', 'sortedDates:', sortedDates)
   const latestDate = new Date();
   latestDate.setHours(0, 0, 0, 0);
+  ok.log('', 'latestDate:'+latestDate)
 
+  const numOfDays = 365;
 
-  // Ensure it goes back at least 365 days
+  // Set the starting date to numOfDays days before the latestDate
   let firstDate = new Date(latestDate);
-  firstDate.setDate(latestDate.getDate() - 365);
-
-  let runningValue = 0;  
-
-  while (firstDate <= latestDate) {
-    const dateStr = firstDate.toISOString().split('T')[0];
+  firstDate.setDate(latestDate.getDate() - (numOfDays-1));
+  
+  let runningValue = 0;
+  for (let i = 0; i <= numOfDays; i++) {
+    // Create a new date for each iteration, adding 'i' days to firstDate
+    let currentDate = new Date(firstDate);
+    currentDate.setDate(firstDate.getDate() + i);
+  
+    const dateStr = currentDate.toISOString().split('T')[0];
     let todaysValueChange = dateValueMap[dateStr] || 0;
-
+  
     runningValue += todaysValueChange;
-
     portfolio.push({
       date: dateStr,
-      value: runningValue, 
+      value: runningValue,
     });
-
-    firstDate.setDate(firstDate.getDate() + 1);
   }
-  ok.log('', portfolio)
+  ok.log('', 'portfolio', portfolio)
   return portfolio;
 }
