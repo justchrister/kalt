9becaa28a35a89ddd1518c100f3a10ed2b8aea9d
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-06 00:18:20 +0100

    Update test.ts

--- server/api/invites/test.ts
+++ server/api/invites/test.ts
@@ -2,12 +2,13 @@ export default defineEventHandler(async (event) => {
 
   const secretCronKey = 'Bearer ' + process.env.CRON_SECRET;
   const secretWebhookKey = 'Bearer ' + process.env.WEBHOOK_SECRET;
-  const incomingKey = event.node.req.headers['authorization'];
+  const headers = event.node.req.headers
+  const incomingKey = headers['authorization'];
 
   return {
     incomingKey,
     secretCronKey,
-    secretWebhookKey, 
-    event
+    secretWebhookKey,
+    headers
   }
 });
