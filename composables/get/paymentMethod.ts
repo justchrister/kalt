0fe87b97ea6f817e997912b7a6653240a274a3bc
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-07 00:29:09 +0100

    KALT-592: Review and adjust ok.log() logs

--- composables/get/paymentMethod.ts
+++ composables/get/paymentMethod.ts
@@ -1,17 +1,17 @@
 import { ok } from '~/composables/ok'
 
 export const getPaymentMethod = async (client, user) => {
-  ok.log('', user.id)
   const { data, error } = await client
     .from('topic_paymentMethods')
     .select()
     .eq('id', user.id)
     .order('timestamp', { ascending: true })
   if (error) {
-    ok.log('error', 'failed getting payment method: ', error)
+    ok.log('error', 'failed getting payment method: '+error.message)
     return null
   } else {
     const combined = ok.merge(data, 'id')[0]
+    ok.log('', 'got payment method')
     return ok.cleanMessage(combined)
   }
 };
