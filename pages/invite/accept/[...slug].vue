c5959ab2c96c48dd9f85784c0bb60030218fb947
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-07 00:27:56 +0100

    KALT-592: Review and adjust ok.log() logs

--- pages/invite/accept/[...slug].vue
+++ pages/invite/accept/[...slug].vue
@@ -21,20 +21,16 @@
   })
   const route = useRoute()
   const inviteCode = ref(route.params.slug[0] || null)
-  ok.log('', inviteCode.value)
   const startInvite = async () => {
     const { data, error } = await useFetch('/api/invites/validate?code=' + inviteCode.value, {
       method: 'POST'
     })
     const status = data?.value.status
     if (error.value) {
-      ok.log('', 'im here')
       return
     } else if (status === 'valid') {
-      ok.log('', status)
       navigateTo('/invite/accept/auth')
     } else {
-      ok.log('', status)
       return status
     }
   }
