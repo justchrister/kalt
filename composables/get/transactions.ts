dbf404b71b39a2ad431777ce6406bf1e3f6232b9
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-07 00:27:15 +0100

    KALT-592: Review and adjust ok.log() logs

--- composables/get/transactions.ts
+++ composables/get/transactions.ts
@@ -7,9 +7,10 @@ export const getTransactions = async (client, user) => {
     .eq('userId', user.id)
     .order('timestamp', { ascending: true })
   if (error) {
-    ok.log('error', error)
+    ok.log('error', 'could not getTransactions'+error.message)
     return null
   } else {
+    ok.log('', 'got transactions')
     return ok.merge(data, 'entity');
   }
 };
