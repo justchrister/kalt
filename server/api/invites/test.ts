6edef82b59d5bb6a817c95b011cec933f86f95cf
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-06 18:30:39 +0100

    Update test.ts

--- server/api/invites/test.ts
+++ server/api/invites/test.ts
@@ -1,3 +1,5 @@
+import { ok } from '~/composables/ok'
+
 export default defineEventHandler(async (event) => {
 
   const secretCronKey = 'Bearer ' + process.env.CRON_SECRET;
@@ -5,7 +7,9 @@ export default defineEventHandler(async (event) => {
   const headers = event.node.req.headers
   const incomingKey = headers['authorization'];
 
+  const keyPair = await ok.verifyKeyPair(event)
   return {
+    keyPair,
     incomingKey,
     secretCronKey,
     secretWebhookKey,
