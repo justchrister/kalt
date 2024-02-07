3c731b8766292f8c2ec9da28c594959830e98462
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-08 00:17:39 +0100

    KALT-469: KYC

--- pages/profile/kyc/index.vue
+++ pages/profile/kyc/index.vue
@@ -3,8 +3,7 @@
     <block>
       <h1> Legally required </h1>
       <p> We are legally required to collect some info about you and where your money is coming from. We've made it as easy as possible and it only takes a couple of minutes to do. </p>
-      <div class="bar one">
-      </div>
+      <input-button link="/profile/kyc/1">start -></input-button>
     </block>
   </main>
 </template>
@@ -22,15 +21,3 @@
   })
   
 </script>
-<style scoped lang="scss">
-  .bar{
-    background: linear-gradient(90deg, green(10%) 0%, green(10%) 45%, green(10%) 45%, green(10%) 100%);
-    border-radius:$border-radius;
-    width:100%;
-    height: sizer(1);
-    display:block;
-    &.one{
-      background: linear-gradient(90deg, green(80%) 0%, green(80%) 20%, green(10%) 20%, green(10%) 100%);
-    }
-  }
-</style>
