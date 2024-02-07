2407abfaf58dd83defc08256e50f9fe936761582
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-07 22:26:50 +0100

    KALT-579: Add a good flow to the /jobs application part

---
+++ pages/jobs/success.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <main>
+    <frame>
+      <frame-image src="/images/audun-alvestad/waiting-room.png" />
+      <frame-image src="/images/apollo-celebration.jpg" />
+    </frame>
+    <block margin="half">
+      <h1> Thank you! </h1>
+      <p>
+        We will be in touch very soon <omoji emoji="✨"/>. Looking forward to getting to know you better! 
+      </p>
+    </block>
+    <block margin="half">
+      <input-button link="/vision">our vision →</input-button>
+    </block>
+  </main>
+</template>
+<script setup lang="ts">
+  definePageMeta({
+    pagename: 'Lobby'
+  })
+  useHead({
+    title: 'Lobby'
+  })
+</script>
