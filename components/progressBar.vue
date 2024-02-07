d088fc10391f5c1bdca3089184389d4d2bf84bf3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-08 00:17:30 +0100

    KALT-469: KYC

---
+++ components/progressBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bar" :style="'background: linear-gradient(90deg, #a2ecd4 '+props.percentage+', #f3fbf5 '+props.percentage+');'"></div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    percentage: {
+      type: String,
+      required: true
+    }
+  })
+  ok.log('', props.percentage)
+</script>
+<style scoped lang="scss">
+  
+  .bar{
+    border-radius:$border-radius;
+    color:green(10%);
+    width:100%;
+    height: sizer(1);
+    display:block;
+  }
+</style>
