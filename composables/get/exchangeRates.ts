ef21fec7d6d0142b2c246114fdc7e27b18505c23
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-07 01:12:28 +0100

    Update exchangeRates.ts

--- composables/get/exchangeRates.ts
+++ composables/get/exchangeRates.ts
@@ -11,7 +11,7 @@ export const getExchangeRates = async (client, from, to) => {
     .limit(1)
     .single()
   if (error) {
-    ok.log('warn', 'could not get exchange rates'+from+' → '+to)
+    ok.log('warn', 'could not get exchange rates '+from+' → '+to)
     return null
   } else {
     ok.log('', 'got exchange rate '+from+' → '+to+' = '+data.rate)
