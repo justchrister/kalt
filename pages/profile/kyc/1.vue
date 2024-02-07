b2161f7b4f18f68b80549b7bf9200597469e5a33
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-08 00:17:35 +0100

    KALT-469: KYC

---
+++ pages/profile/kyc/1.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <block>
+      What is the source of the money you are planning on investing?
+      <progress-bar percentage="20%" />
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'source of funds',
+    middleware: 'auth'
+  })
+  useHead({
+    title: 'source of funds',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
