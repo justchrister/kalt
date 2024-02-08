356f664a0fcaeed91fc3bb871faf431032722cd7
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-08 08:37:12 +0100

    KALT-624: add request more invites button

--- components/invites.vue
+++ components/invites.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="invites">
     <p>Invite some friends</p>
-    <invite v-for="invite in invites" :key="invite.code" :code="invite.code" />
-    <!-- 
-      If there are no invites, there should be a button to request more. 
-    -->
+    <invite v-for="invite in invites" :key="invite.code" :code="invite.code" v-if="invites.length"/>
+    <input-button @click="requestInvities()" ><loading-icon v-if="loading"/> request more invites</input-button>
   </div>
 </template>
 <script setup lang="ts">
@@ -14,11 +12,28 @@
       required: true
     }
   })
+  const loading = ref(false)
   const supabase = useSupabaseClient()
+  const auth = useSupabaseUser()
+  const user = await get(supabase).user(auth.value)
   const { data: invites, error } = await supabase
     .from('topic_invites')
     .select()
     .eq('issuedTo', props.user.id)
+    .limit(3)
+  const requestInvities = async () => {
+    loading.value = true
+    const { data, error } = await supabase
+      .from('topic_inviteRequests')
+      .insert({
+        id: user.id,
+        sender: 'components/invites.vue'
+      })
+      .select()
+    if(error) ok.log('error', 'error in requesting more invites: '+error.message)
+    await ok.sleep(200);
+    loading.value = false
+  }
 </script>
 <style scoped lang="scss">
   .invite{
