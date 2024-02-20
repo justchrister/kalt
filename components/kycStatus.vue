11cace4c5035938ad1ecbf43e5077761302c7def
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-20 22:57:04 +0100

    KALT-469: KYC

--- components/kycStatus.vue
+++ components/kycStatus.vue
@@ -6,21 +6,18 @@
     user: {
       type: Object,
       required: true
-    }, 
-    initialValue: {
-      type: String,
-      required: false,
-      default: null
     }
   })
-  const verified = false;
+  const supabase = useSupabaseClient()
+  const verified = await get(supabase).kycStatus(props.user)
+  ok.log('', verified)
 </script>
 <style scoped lang="scss">
   .icon{
     display:inline-block;
     width: sizer(1.5);
     height: sizer(1.5);
-    background-image: url('/omoji/kyc/verified.svg');
+    background-image: url('/icons/kyc/verified.svg');
     background-repeat: no-repeat;
     background-size: 80%;
     background-position: bottom;
