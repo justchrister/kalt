45aed8c13a04ac85d43c178adedeb649e2199493
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-22 19:54:31 +0100

    KALT-628: switch vc for smb

--- supabase/sys/funds.sql
+++ supabase/sys/funds.sql
@@ -1,19 +1,21 @@
-create table public."sys_funds" (
-    "ticker"               "tickers"                 not null   primary key,
-    "name"                 text,
-    "description"          text
-);
 
+drop table public."sys_funds";
+create table public."sys_funds" (
+    "ticker"                "tickers"                 not null   primary key,
+    "name"                  text,
+    "description"           text, 
+    "state"                 "funds_state"             not null  default "inactive"
+);
 
 --- row level security
 ALTER TABLE "sys_funds" ENABLE ROW LEVEL SECURITY;
 
-insert into public."sys_funds" ("ticker", "name", "description") values 
-  ('art', 'Art Fund', 'The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with clear set of criteria for picking the artworks and artists we invest in.'),
-  ('ffe', 'Fossil-free energy', 'The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by'),
-  ('ah', 'Affordable housing', ''),
---  ('smb', 'Small and medium sized businesses', 'The small and medium sized businesses fund invests in small and medium sized businesses with high growth potential, and a clear path to profitability.'),
-  ('vc', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.');
+insert into public."sys_funds" ("ticker", "name", "description", "state") values 
+  ('art', 'Art Fund', 'The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with clear set of criteria for picking the artworks and artists we invest in.', 'alfa'),
+  ('ffe', 'Fossil-free energy', 'The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by', 'alfa'),
+  ('ah', 'Affordable housing', '', 'beta'),
+  ('smb', 'Small business', 'The small business fund invests in or lends money to businesses that are designed for profitability with a potential for stable growth.', 'beta'),
+  ('vc', 'Venture capital', 'The venture capital fund invests in early stage startups with high growth potential, and a clear path to profitability.', 'inactive');
 
 CREATE POLICY "PUBLIC — Select"
   ON public.sys_funds
