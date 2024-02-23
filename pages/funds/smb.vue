fc719a58c197872fdb39b0a81b2f5cc66d1f5509
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-23 18:51:38 +0100

    Added the criteria and thesis

--- pages/funds/smb.vue
+++ pages/funds/smb.vue
@@ -1,22 +1,45 @@
 <template>
   <main>
-    <block margin="half">
-      <intro ticker="ffe"
-        text="The fossil-free energy fund invests in energy generating infrastructure, in high impact geographies, low current supply, and high projected growth-by " />
+    <intro title="Invest in the fundament of all economies and communities." />
+    <block margin="half" :border="true">
+      <h3>The small business fund offers a unique opportunity to invest in the backbone of global economies and local communities — small businesses. We apply a rigorous selection process, identifying companies with business models designed for profitability and a committed leadership. </h3>
     </block>
-    <block>
+    <block margin="half">
       <criteria-list>
         <criteria :nr="1">
-          The geographical area must have high GDP growth potential
+          A business model designed for profitability.
         </criteria>
         <criteria :nr="2">
-          The geographical area must have high energy generation potential
+          Strong financial track record with effective management
         </criteria>
         <criteria :nr="3">
-          The country must have moderate to low political risk
+          Must have been operational for at least 3 years
+        </criteria>
+        <criteria :nr="4">
+          A committed and diverse leadership team
+        </criteria>
+        <criteria :nr="5">
+          Clear understanding of their market niche and customer base
         </criteria>
       </criteria-list>
     </block>
+    <block margin="half">
+      <h2> The thesis </h2>
+      <p>
+        The cozy café where you grab your morning coffee, the electrician where your cousin was an apprentice, or the family-owned restaurant where you celebrate special occasions. These aren't just businesses; they're the cornerstones of our communities, and the bedrock of the global economy. These small businesses adapt to any crisis and keep our local communities vibrant and thriving.
+      </p>
+      <p>
+        As AI and automation make efficiency in large corporations more important, they will end up consolidating further and further. Small businesses will not reap the benefits of these technologies as their scale is too small, but they compete on service and experience. This is where the fund comes in. We make it more efficient for small businesses to run and act as a safety net for the resilient.
+      </p>
+      <p>
+        We either lend small businesses money, offer convertible loans, or buy parts of the business; we do not dictate the strategy but enable the business owners to focus on running and growing their business.
+      </p>
+      <p>
+        When we pick businesses to invest in, we look for ones that have been doing well for a while and really understand the people they serve and what makes them special. This is not just about making money. It's about putting our money into the businesses that make our towns and cities better places to live.
+      </p>
+      <input-button link="/invest">invest now -></input-button>
+    </block>
+    <!-- insert map with locations -->
     <block>
       <input-button link="/invest">invest now -></input-button>
       <select-fund />
@@ -25,13 +48,19 @@
 </template>
 <script lang="ts" setup>
   definePageMeta({
-    pagename: 'Fossil-free energy'
+    pagename: 'Small business'
   })
   useHead({
-    title: 'Fossil-free energy',
+    title: 'Small business',
     meta: [{
       name: 'description',
       content: 'Invest in the future, today.'
     }]
   })
+
 </script>
+<style scoped lang="scss">
+  main {
+    padding-top: 0;
+  }
+</style>
