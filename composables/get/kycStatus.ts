8aaab54d981c53cc91745277877ed9b964190628
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-28 19:43:50 +0100

    KALT-469: KYC

--- composables/get/kycStatus.ts
+++ composables/get/kycStatus.ts
@@ -9,26 +9,28 @@ export const getKycStatus = async (client: any, auth: any) => {
   if(!data.length>1 || !data) return false
   
   const merged = ok.merge(data, 'id') as kyc
-  
+
   if(merged.politicallyExposed !== null && merged.sourceOfFunds !== null ) {
-    const { data:proofOfAddress, error: proofOfAddressError } = await client
-      .storage
-      .from('proofOfAddress')
-      .list(auth.id, {
-        limit: 1,
-        offset: 0,
-        sortBy: { column: 'name', order: 'asc' },
-      })
-    
-    const { data:userIdentification, error:userIdentificationError } = await client
-      .storage
-      .from('userIdentification')
-      .list(auth.id, {
-        limit: 1,
-        offset: 0,
-        sortBy: { column: 'name', order: 'asc' },
-      })
-    if(proofOfAddress.length>0 && userIdentification.length>0) return true
+    if(merged.incomeRangeFrom|| merged.incomeRangeTo){
+      const { data:proofOfAddress, error: proofOfAddressError } = await client
+        .storage
+        .from('proofOfAddress')
+        .list(auth.id, {
+          limit: 1,
+          offset: 0,
+          sortBy: { column: 'name', order: 'asc' },
+        })
+      
+      const { data:userIdentification, error:userIdentificationError } = await client
+        .storage
+        .from('userIdentification')
+        .list(auth.id, {
+          limit: 1,
+          offset: 0,
+          sortBy: { column: 'name', order: 'asc' },
+        })
+      if(proofOfAddress.length>0 && userIdentification.length>0) return true
+    }
   }
 
   return false
