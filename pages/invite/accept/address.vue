ff33b3ac331bd23243ff9e6f4f6ad1035828089e
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-02-28 22:51:35 +0100

    KALT-627: switch useHead with useSeoMeta

--- pages/invite/accept/address.vue
+++ pages/invite/accept/address.vue
@@ -14,12 +14,12 @@
     pagename: 'City',
     middleware: 'auth'
   })
-  useHead({
-    title: 'City',
-    meta: [{
-      name: 'description',
-      content: 'Invest in the future, today.'
-    }]
+  useSeoMeta({
+    title: 'Accept invite',
+    ogTitle: 'Kalt - Accept invite',
+    description: 'Real assets, real impact.',
+    ogDescription: 'Real assets, real impact.',
+    ogImage: 'https://ka.lt/images/meta.png'
   })
 const supabase = useSupabaseClient()
 const auth = useSupabaseUser()
