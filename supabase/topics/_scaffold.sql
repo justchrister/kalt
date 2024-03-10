290e29fea6fde266da3fe7519341c251cdf12aa9
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-03-10 17:24:46 +0100

    KALT-637: Cohorts

--- supabase/topics/_scaffold.sql
+++ supabase/topics/_scaffold.sql
@@ -1,14 +1,15 @@
--- create a topic, by simply renaming all instances of <<topic_name>
--- version 28.7.23
+-- create a topic/ledger, by simply renaming all instances of <<topic_name>
+-- version 10.03.24
 -- topic   <<topic_name>>
 
 --- create the table, with default values
 CREATE TABLE "topic_<<topic_name>>" (
 -- meta information used for processing
-    "event"       uuid        NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
-    "id"   uuid        NOT NULL  DEFAULT uuid_generate_v4(),
-    "timestamp"     timestamptz NOT NULL  DEFAULT (now() at time zone 'utc'),
-    "sender"   text        NOT NULL
+    "event"         uuid            NOT NULL  DEFAULT uuid_generate_v4()         PRIMARY KEY,
+    "id"            uuid            NOT NULL  DEFAULT uuid_generate_v4(),
+    "timestamp"     timestamptz     NOT NULL  DEFAULT (now() at time zone 'utc'),
+    "sender"        text            NOT NULL,
+-- columns
 );
 
 --- row level security
