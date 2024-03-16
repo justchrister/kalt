1ca95ff3a4646d3a7586a7220c612c0690ed4d79
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-03-16 12:54:59 +0100

    KALT-637: Cohorts

--- server/api/updatesEngine/cohorts/index.ts
+++ server/api/updatesEngine/cohorts/index.ts
@@ -2,7 +2,6 @@ import { ok } from '~/composables/ok'
 import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler( async (event) => {
-  // call the api/updateEngine/cohorts/new using fetch
   const cohortsNew = await fetch('http://localhost:3000/api/updatesEngine/cohorts/new', {
     method: 'POST',
     headers: {
