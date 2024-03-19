d38f4fa98e688d62bf132c7472b12e09d5763b70
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-03-19 02:38:50 +0100

    KALT-607: Adjust font sizes

--- pages/funds/art/index.vue
+++ pages/funds/art/index.vue
@@ -2,8 +2,8 @@
   <main>
     <intro title="Invest in art from the most famous artists in the world." />
     <block margin="half" :border="true">
-      <h3>The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with
-        clear set of criteria for picking the artworks and artists we invest in. </h3>
+      The art fund invests in high quality blue- and red-chip art from a variety of artists around the world — with
+        clear set of criteria for picking the artworks and artists we invest in. 
     </block>
     <block margin="half">
       <criteria-list>
