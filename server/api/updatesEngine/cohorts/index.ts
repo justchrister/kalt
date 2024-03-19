0c020cf71c79ec1e47545f7f7e43ab01ec5904e8
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-03-19 02:29:55 +0100

    KALT-643: sending updates

--- server/api/updatesEngine/cohorts/index.ts
+++ server/api/updatesEngine/cohorts/index.ts
@@ -1,8 +1,7 @@
 import { ok } from '~/composables/ok'
-import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler( async (event) => {
-  const cohortsNew = await fetch('http://localhost:3000/api/updatesEngine/cohorts/new', {
+  const cohortsNew = await fetch('/api/updatesEngine/cohorts/new', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
