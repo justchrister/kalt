513b81672bbacf12cc8b2ca94edffe34578d1203
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-03-19 01:36:56 +0100

    KALT-645: show updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,33 +10,31 @@
 <script lang="ts" setup>
   import { SpeedInsights } from '@vercel/speed-insights/vue';
   const supabase = useSupabaseClient()
-  const auth = useSupabaseUser()
+  const auth = useSupabaseUser();
+  
   const updateCount = ref(0)
   
-  const fetchUpdates = async (user: user) => {
+  const fetchUpdates = async (userId: string) => {
+    if(!userId) return
     const { data, error } = await supabase
       .from('topic_updates')
       .select()
-      .eq('userId', user.id)
+      .eq('userId', userId)
     if (error) {
       updateCount.value = 0 
       return
     }
     const merged = ok.merge(data, 'id')
     const filtered = merged.filter((update: any) => !update.read);
-    
+
     if(filtered && filtered.length>=1){
       updateCount.value = filtered.length
     } else {
       updateCount.value = 0 
     }
   }
-
-  if(auth.value){
-    const user = await get(supabase).user(auth.value);
-    fetchUpdates(user)
-    setInterval(fetchUpdates, 200)
+  let intervalId = '';
+  if(auth.value.id) {
+    intervalId = setInterval(() => fetchUpdates(auth.value.id), 500)
   }
-  
-
 </script>
