e686d4554df52146e116e75af1e695b9bdb1d202
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-04-01 17:05:39 +0200

    send it up

---
+++ pages/jobs/cio.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <block>
+      <p> hello</p>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'founder'
+  })
+  useHead({
+    title: 'founder',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
