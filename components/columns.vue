2d65d69f2c5c0d782a30227636d7b6c6c89f0557
christerwaters <42318403+christerwaters@users.noreply.github.com>
2024-04-18 00:11:31 +0200

    KALT-652: Only make the art fund selectable under funds/your

---
+++ components/columns.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="'columns col-'+number">
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    n: {
+      type: Number,
+      required: true
+    }
+  })
+  //convert to string
+  const number = props.n.toString()
+</script>
+<style scoped lang="scss">
+  .columns{
+    display:grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: sizer(1);
+
+    &.col-2{
+      grid-template-columns: repeat(2, 1fr);
+    }
+    &.col-3{
+      grid-template-columns: repeat(3, 1fr);
+    }
+    &.col-4{
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+</style>
