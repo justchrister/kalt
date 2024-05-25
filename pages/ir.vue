4102890d5944870b782f9515220214b27a9f2b97
christerwaters <christer@kalt.co>
2024-05-25 10:33:09 +0200

    Create ir.vue

---
+++ pages/ir.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <block>
+      <h1> Interested in becoming an owner of Kalt? </h1>
+      <form>
+        <p> We are currently raising a round of funding. If you are interested in becoming an owner of Kalt. </p>
+        <input type="phonenumber" placeholder="Phone number"><input-button>Request more information</input-button>
+      </form>
+    </block>
+    <block>
+      <h2>Updates</h2>
+      <update-single subject="Kalt raises $1M in seed funding" date="2022-01-01" category="performance" ingress="xxxx."/>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Investor relations'
+  })
+  useHead({
+    title: 'Investor relations',
+    meta: [{
+      name: 'description',
+      content: 'Invest in the future, today.'
+    }]
+  })
+  
+</script>
+<style scoped lang="scss">
+  
+</style>
