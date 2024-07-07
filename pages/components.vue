ed4f106babfa68fd296d589b1d5fb7c941e02663
christer <christer@ka.lt>
2024-07-07 22:21:18 +0200

    Create components.vue

---
+++ pages/components.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <block>
+      <chat-note>
+        {{ text }}
+      </chat-note>
+    </block>
+  </main>
+</template>
+<script lang="ts" setup>
+  definePageMeta({
+    pagename: 'Components'
+  })
+  useHead({
+    title: 'Components',
+    meta: [{
+    name: 'description',
+    content: 'Invest in the future, today.'
+    }]
+  })
+  const text = ` 
+hello number two`
+
+</script>
+<style scoped lang="scss">
+
+</style>
