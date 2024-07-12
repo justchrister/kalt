cf7dd03565d821c53ac8e82e9df649aee51ce191
christer <christer@ka.lt>
2024-07-12 08:22:39 +0200

    Create success.vue

---
+++ pages/careers/success.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <main>
+    <frame>
+      <frame-image src="/images/audun-alvestad/waiting-room.png" />
+      <frame-image src="/images/apollo-celebration.jpg" />
+    </frame>
+    <block margin="half">
+      <h1> Thank you! </h1>
+      <p>
+        We will be in touch very soon <omoji emoji="✨"/>. Looking forward to getting to know you better! 
+      </p>
+    </block>
+    <block margin="half">
+      <input-button link="/vision">our vision →</input-button>
+    </block>
+  </main>
+</template>
+<script setup lang="ts">
+  definePageMeta({
+    pagename: 'Lobby'
+  })
+  
+  useSeoMeta({
+    title: 'Lobby',
+    ogTitle: 'Kalt - Lobby',
+    description: 'Real assets, real impact.',
+    ogDescription: 'Real assets, real impact.',
+    ogImage: 'https://ka.lt/images/meta.png'
+  })
+</script>
