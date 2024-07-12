a1ff4e419e198c3cf34f3ffb6f6559bcffbc8fdc
christer <christer@ka.lt>
2024-07-12 17:29:01 +0200

    Create yourCard.vue

---
+++ components/fund/yourCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps({
+    energy: {
+      type: Number,
+      required: true
+    },
+    art: {
+      type: Number,
+      required: true
+    },
+    smallBusiness: {
+      type: Number,
+      required: true
+    },
+    accessibleHousing: {
+      type: Number,
+      required: true
+    },
+  })
+</script>
+<style scoped lang="scss">
+  
+</style>
