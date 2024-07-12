4b428ca0fecdedb901838260500bf02fc893932a
christer <christer@ka.lt>
2024-07-12 08:43:37 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,24 @@
   <main>
     <div class="video-container" @click="navigateTo('/invite/request/amount')">
       <h1>
-        Invest in the <strong>future</strong>, today. <br/> <nuxt-link to="/invite/request/amount"> start → </nuxt-link>
+        <span>Invest</span> <span>in</span> <span>the</span> <span><strong>future</strong>,</span> <span>today.</span> <br/> <span><nuxt-link to="/invite/request/amount"> start → </nuxt-link></span>
       </h1>
       <video src="/videos/film-3.mp4" class="background-video" autoplay muted loop></video>
     </div>
     <block>
       <h2>Real impact</h2>
       <p>
-        We use cutting-edge technology to make your money work for you, while contributing to a sustainable future for everyone.
-      </p>
+        Our innovative technology lets you to grow your wealth while building a more sustainable future for everyone.</p>
       <p>
         By investing in what you believe in, you can be sure that your money is aligned with your values.
       </p>
       <p>
-        <u>Our funds:</u> 
+        Our funds:
+      </p>
       <pill text="energy" to="funds/ffe" />
       <pill text="art" to="funds/art" />
       <pill text="housing" to="funds/ah" />
       <pill text="small business" to="funds/smb" />
-    </p>
     </block>
     <block>
       <h2>Want to work with us?</h2>
@@ -68,15 +67,33 @@
     width: 100%;
     height: 100%;
     object-fit: cover;
-    border-radius: sizer(2); 
+    border-radius: sizer(0.8); 
     z-index: -1;
+  }
+  @keyframes moveUp {
+  0% {
+    transform: translateY(0);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(-10px);
+    opacity: 1;
+  }
+}
+
+  h1 span {
+    display: inline-block;
+    opacity: 0;
+    animation: moveUp 0.5s forwards;
+  }
+
+  // Use SCSS loop to apply animation delay
+  @for $i from 1 through 6 {
+    h1 span:nth-child(#{$i}) {
+      animation-delay: 0.1s * $i;
+    }
   }
   main{
     padding-top:0;
-  }
-</style>
-<style lang="scss">
-  body{
-    animation: backgroundColorAnimation  5s infinite;
   }
 </style>
