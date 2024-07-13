2e36f12415c6ec0f99ac9d20b3989104162ddcb5
christer <christer@ka.lt>
2024-07-13 12:29:00 +0200

    Update next.vue

--- pages/portfolio/next.vue
+++ pages/portfolio/next.vue
@@ -1,31 +1,10 @@
 
 <template>
   <main>
+    <frame-next width="wide">
+      <chart-next/>
+    </frame-next>
     <block margin="none">
-      {{ user.firstName }} {{ user.lastName }}
-    </block>
-    <frame>
-      <chart :days="days" />
-    </frame>
-    <block margin="none">
-      <nav class="filters">
-        <div class="overflow-wrap">
-          <ul>
-            <li @click="setDays('thisMonth')" :class="{ active: active === 'thisMonth' }">
-              this month
-            </li>
-            <li @click="setDays('91Days')" :class="{ active: active === '91Days' }">
-              3 months
-            </li>
-            <li @click="setDays('thisYear')" :class="{ active: active === 'thisYear' }">
-              this year
-            </li>
-            <li @click="setDays('fromStart')" :class="{ active: active === 'fromStart' }">
-              from start
-            </li>
-          </ul>
-        </div>
-      </nav>
     </block>
     <block margin="4">
       <cta :showDivest="true" />
@@ -48,30 +27,9 @@
     ogDescription: 'Real assets, real impact.',
     ogImage: 'https://ka.lt/images/meta.png'
   })
-  const days = ref(30)
   const supabase = useSupabaseClient()
   const auth = useSupabaseUser()
   const user = await get(supabase).user(auth.value);
-  const daysSoFarThisYear = () => {
-    return Math.floor((new Date() - new Date(new Date().getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-  }
-  const daysSoFarThisMonth = () => {
-    return Math.floor((new Date() - new Date(new Date().getFullYear(), new Date().getMonth(), 0)) / 1000 / 60 / 60 / 24);
-  }
-  const active = ref('thisMonth');
-  const setDays = (range) => {
-    active.value = range;
-    if (range === 'thisMonth') {
-      days.value = daysSoFarThisMonth();
-    } else if(range==='91Days'){
-      days.value = 91
-    } else if(range==='thisYear'){
-      days.value = daysSoFarThisYear();
-    } else if(range==='fromStart'){
-      days.value = 99999999999
-    } 
-  }
-  setDays('thisMonth')
 </script>
 <style scoped lang="scss">
   .filters {
