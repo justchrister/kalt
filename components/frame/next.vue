0fcef1518cd3ea1066f163ec0785df332d9e99ef
christer <christer@ka.lt>
2024-07-13 12:28:49 +0200

    Create next.vue

---
+++ components/frame/next.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    width: {
+      type: String,
+      required: false
+    }
+  })
+
+  const classes = computed(() => {
+    let classes = ['frame']
+    if (props.width) {
+      classes.push(props.width)
+    }
+    return classes
+  })
+</script>
+<style scoped lang="scss">
+.frame {
+  width: $sitewidth;
+  max-width: $maxsitewidth;
+  margin:0 auto sizer(3) auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: flex-end;
+  align-content: flex-start;
+  height: 50vw;
+  max-height: 418px;
+  &.wide{
+    max-width: $maxsitewidth*1.3;
+  }
+}
+</style>
