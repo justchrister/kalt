90c0c5cd962c38b80e09a05e30c0a5c6388954dd
christer <christer@ka.lt>
2024-07-15 00:15:13 +0200

    Update index.vue

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,7 +1,21 @@
 <template>
   <main>
-    <block margin="2">
-      <profile-card/>
+    <block margin="4">
+      <h2>Details</h2>
+      <div class="col-2">
+        <input-user id="firstName" />
+        <input-user id="lastName" />
+      </div>
+      <select-birthdate />
+      <select-country />
+      <div class="col-1-4">
+        <input-user id="city" />
+        <input-user id="postalCode" />
+      </div>
+      <input-user id="addressLine1" />
+    </block>
+    <block margin="none">
+      <h2>Preferences</h2>
     </block>
     <block margin="2">
       <select-currency />
@@ -33,3 +47,15 @@
     ogImage: 'https://ka.lt/images/meta.png'
   })
 </script>
+<style lang="scss" scoped>
+  .col-2{
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    gap: sizer(1);
+  }
+  .col-1-4{
+    display:grid;
+    grid-template-columns: 5fr 2fr;
+    gap: sizer(1);
+  }
+</style>
